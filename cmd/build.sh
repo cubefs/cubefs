0ff79e8dfb5495224df7cac38a9625081e8051e9
bjguoweilong <guowL$0917>
2018-09-22 15:27:22 +0800

    recory blob file

--- cmd/build.sh
+++ cmd/build.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
-export GOPATH=/root/work/baud
+export GOPATH=/home/guowl/cbfs
 cd $GOPATH/src/github.com/tiglabs/containerfs/cmd
 export LD_LIBRARY_PATH=/usr/local/lib:$LD_LIBRARY_PATH
 CGO_CFLAGS="-I/usr/local/include" CGO_LDFLAGS="-L/usr/local/lib -lrocksdb -lstdc++ -lm -lz -lbz2 -lsnappy " go build -v cmd.go
