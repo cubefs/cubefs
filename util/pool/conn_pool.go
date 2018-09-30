e53711c6a3a1b706e3a68dff830299c7ea32493a
guoweilong <bjguoweilong>
2018-10-01 00:16:39 +0800

    add

--- util/pool/conn_pool.go
+++ util/pool/conn_pool.go
@@ -1,5 +1,6 @@
 // Copyright 2018 silenceper
 // from https://github.com/silenceper/pool.git,thanks
+//
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
