a8d516f1e28b3c33bd5cf79c488e8765df862ec8
zhuhyc <zzhniy.163.niy@163.com>
2018-12-20 16:50:33 +0800

    init meta partitions

--- master/const.go
+++ master/const.go
@@ -46,7 +46,7 @@ const (
 )
 
 const (
-	defaultInitMetaPartitionCount               = 10
+	defaultInitMetaPartitionCount           = 3
 	defaultMaxMetaPartitionInodeID  uint64  = 1<<63 - 1
 	defaultMetaPartitionInodeIDStep uint64  = 1 << 24
 	defaultMetaNodeReservedMem      uint64  = 1 << 32
