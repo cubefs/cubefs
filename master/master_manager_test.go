c7eec17a7b76100c51b8a76d78437ba633a12add
zhuhyc <zzhniy.163.niy@163.com>
2019-08-02 11:16:20 +0800

    enhancement: support config raft heartbeat port and add test case for raft related operational
    Signed-off-by: zhuhyc <zzhniy.163.niy@163.com>

--- master/master_manager_test.go
+++ master/master_manager_test.go
@@ -1,6 +1,14 @@
 package master
 
-import "testing"
+import (
+	"testing"
+	"fmt"
+	"github.com/chubaofs/chubaofs/proto"
+	"github.com/chubaofs/chubaofs/raftstore"
+	rproto "github.com/tiglabs/raft/proto"
+	"net/http"
+	"io/ioutil"
+)
 
 func TestHandleLeaderChange(t *testing.T) {
 	leaderID := server.id
@@ -15,4 +23,96 @@ func TestHandleLeaderChange(t *testing.T) {
 		t.Errorf("logic error,metaReady should be true,metaReady[%v]", server.metaReady)
 		return
 	}
+}
+
+func TestHandlerPeerChange(t *testing.T) {
+	addPeerTest(t)
+	removePeerTest(t)
+}
+
+func addPeerTest(t *testing.T) {
+	confChange := &rproto.ConfChange{
+		Type:    rproto.ConfAddNode,
+		Peer:    rproto.Peer{ID: 2},
+		Context: []byte("127.0.0.2:9090"),
+	}
+	if err := server.handlePeerChange(confChange); err != nil {
+		t.Error(err)
+		return
+	}
+}
+
+func removePeerTest(t *testing.T) {
+	confChange := &rproto.ConfChange{
+		Type:    rproto.ConfRemoveNode,
+		Peer:    rproto.Peer{ID: 2},
+		Context: []byte("127.0.0.2:9090"),
+	}
+	if err := server.handlePeerChange(confChange); err != nil {
+		t.Error(err)
+		return
+	}
+}
+
+func TestRaft(t *testing.T) {
+	addRaftServerTest("127.0.0.1:9001", 2, t)
+	removeRaftServerTest("127.0.0.1:9001", 2, t)
+	snapshotTest(t)
+}
+
+func snapshotTest(t *testing.T) {
+	mdSnapshot, err := server.cluster.fsm.Snapshot()
+	if err != nil {
+		t.Error(err)
+		return
+	}
+	t.Logf("snapshot apply index[%v]\n", mdSnapshot.ApplyIndex())
+	s := &Server{}
+	dbStore := raftstore.NewRocksDBStore("/export/chubaofs/raft2", LRUCacheSize, WriteBufferSize)
+	fsm := &MetadataFsm{
+		rs:    server.fsm.rs,
+		store: dbStore,
+	}
+	fsm.registerApplySnapshotHandler(func() {
+		fsm.restore()
+	})
+	s.fsm = fsm
+	peers := make([]rproto.Peer, 0, len(server.config.peers))
+	for _, peer := range server.config.peers {
+		peers = append(peers, peer.Peer)
+	}
+	if err = fsm.ApplySnapshot(peers, mdSnapshot); err != nil {
+		t.Error(err)
+		return
+	}
+	if fsm.applied != mdSnapshot.ApplyIndex() {
+		t.Errorf("applied not equal,applied[%v],snapshot applied[%v]\n", fsm.applied, mdSnapshot.ApplyIndex())
+		return
+	}
+	mdSnapshot.Close()
+}
+
+func addRaftServerTest(addRaftAddr string, id uint64, t *testing.T) {
+	//don't pass id test
+	reqURL := fmt.Sprintf("%v%v?id=&addr=%v", hostAddr, proto.AddRaftNode, addRaftAddr)
+	fmt.Println(reqURL)
+	resp, err := http.Get(reqURL)
+	if err != nil {
+		t.Errorf("err is %v", err)
+		return
+	}
+	fmt.Println(resp.StatusCode)
+	defer resp.Body.Close()
+	body, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		t.Errorf("err is %v", err)
+		return
+	}
+	fmt.Println(string(body))
+}
+
+func removeRaftServerTest(removeRaftAddr string, id uint64, t *testing.T) {
+	reqURL := fmt.Sprintf("%v%v?id=%v&addr=%v", hostAddr, proto.RemoveRaftNode, id, removeRaftAddr)
+	fmt.Println(reqURL)
+	process(reqURL, t)
 }
