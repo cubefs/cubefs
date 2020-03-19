53909e831f71dab6bafbd906e64a00107f3c1ac8
Mofei Zhang <mofei2816@gmail.com>
2020-03-19 12:00:10 +0800

    Fix: empty result on list_objects
    Fix issue that list objects interface respond empty result when no
    content in contents element.
    
    Signed-off-by: Mofei Zhang <mofei2816@gmail.com>

--- objectnode/fs_volume.go
+++ objectnode/fs_volume.go
@@ -246,9 +246,6 @@ func (v *volume) ListFilesV1(request *ListBucketRequestV1) ([]*FSFileInfo, strin
 	if err != nil {
 		return nil, "", false, nil, err
 	}
-	if len(infos) <= 0 {
-		return nil, "", false, nil, nil
-	}
 
 	var nextMarker string
 	var isTruncated bool
@@ -276,9 +273,6 @@ func (v *volume) ListFilesV2(request *ListBucketRequestV2) ([]*FSFileInfo, uint6
 
 	infos, prefixes, err = v.listFilesV2(prefix, startAfter, contToken, delimiter, maxKeys)
 	if err != nil {
-		return nil, 0, "", false, nil, err
-	}
-	if len(infos) <= 0 {
 		return nil, 0, "", false, nil, err
 	}
 
