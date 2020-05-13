e6c469f1c66efc96982f5ae2e215886b0a0ced7f
xuxihao <xuxihao3@jd.com>
2020-05-13 15:39:11 +0800

    refactor: decouple the client container and ltptest container,upgrade base image from `cfs-base:1.0` to `cfs-base:1.1` in dockerfile; fix: docker build failing when running `build_docker.sh`
    Signed-off-by: xuxihao <xuxihao3@jd.com>

--- docker/script/start_meta.sh
+++ docker/script/start_meta.sh
@@ -2,5 +2,5 @@
 rm -rf /cfs/data/* /cfs/log/*
 mkdir -p /cfs/bin /cfs/log /cfs/data/meta /cfs/data/raft
 echo "start metanode"
-/cfs/bin/cfs-server -f -c /cfs/conf/metanode.json
+/cfs/bin/cfs-server -f -c /cfs/conf/metanode.json && sleep 99999999d
 
