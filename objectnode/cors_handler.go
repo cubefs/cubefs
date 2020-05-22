wenjia322/chubaofs

feature: add api 'getBucketCors/deleteBucketCors/putBucketCors'
Signed-off-by: wenjia322 <buaa1214wwj@126.com>

objectnode/cors_handler.go
@@ -1,0 +1,129 @@
+package objectnode
+
+// https://docs.aws.amazon.com/zh_cn/AmazonS3/latest/dev/EnableCorsUsingREST.html
+
+import (
+	"encoding/json"
+	"encoding/xml"
+	"github.com/chubaofs/chubaofs/util/log"
+	"io"
+	"io/ioutil"
+	"net/http"
+)
+
+// https://docs.aws.amazon.com/AmazonS3/latest/API/API_GetBucketCors.html
+func (o *ObjectNode) getBucketCorsHandler(w http.ResponseWriter, r *http.Request) {
+	log.LogInfof("Get bucket cors")
+	var (
+		err error
+		ec  *ErrorCode
+	)
+	defer o.errorResponse(w, r, err, ec)
+
+	var param *RequestParam
+	param = ParseRequestParam(r)
+	if param.Bucket() == "" {
+		ec = NoSuchBucket
+		return
+	}
+
+	var vol *Volume
+	if vol, err = o.vm.Volume(param.Bucket()); err != nil {
+		ec = NoSuchBucket
+		return
+	}
+
+	corsOutput := &GetBucketCorsOutput{}
+	cors := vol.loadCors()
+	if cors != nil {
+		corsOutput.corsRules = cors.CORSRule
+	}
+	var corsData []byte
+	if corsData, err = xml.Marshal(corsOutput); err != nil {
+		ec = InternalErrorCode(err)
+		return
+	}
+
+	w.Write(corsData)
+
+	return
+}
+
+// https://docs.aws.amazon.com/AmazonS3/latest/API/API_PutBucketCors.html
+func (o *ObjectNode) putBucketCorsHandler(w http.ResponseWriter, r *http.Request) {
+	log.LogInfof("Put bucket cors")
+
+	var (
+		err error
+		ec  *ErrorCode
+	)
+	defer o.errorResponse(w, r, err, ec)
+
+	var param *RequestParam
+	param = ParseRequestParam(r)
+	if param.Bucket() == "" {
+		ec = NoSuchBucket
+		return
+	}
+	var vol *Volume
+	if vol, err = o.vm.Volume(param.Bucket()); err != nil {
+		ec = NoSuchBucket
+		return
+	}
+
+	var bytes []byte
+	if bytes, err = ioutil.ReadAll(r.Body); err != nil && err != io.EOF {
+		return
+	}
+
+	var corsConfig *CORSConfiguration
+	if corsConfig, err = ParseCorsConfig(bytes); err != nil {
+		return
+	}
+	if corsConfig == nil {
+		return
+	}
+
+	var newBytes []byte
+	if newBytes, err = json.Marshal(corsConfig); err != nil {
+		return
+	}
+	if err = storeBucketCors(newBytes, vol); err != nil {
+		return
+	}
+	vol.storeCors(corsConfig)
+
+	return
+}
+
+// https://docs.aws.amazon.com/AmazonS3/latest/API/API_DeleteBucketCors.html
+func (o *ObjectNode) deleteBucketCorsHandler(w http.ResponseWriter, r *http.Request) {
+	log.LogInfof("Delete bucket cors")
+
+	var (
+		err error
+		ec  *ErrorCode
+	)
+	defer o.errorResponse(w, r, err, ec)
+
+	var param *RequestParam
+	param = ParseRequestParam(r)
+	if param.Bucket() == "" {
+		ec = NoSuchBucket
+		return
+	}
+	var vol *Volume
+	if vol, err = o.vm.Volume(param.Bucket()); err != nil {
+		ec = NoSuchBucket
+		return
+	}
+
+	if err = deleteBucketCors(vol); err != nil {
+		return
+	}
+	vol.storeCors(nil)
+
+	w.WriteHeader(http.StatusNoContent)
+
+	return
+}

proto/perm_action.go
@@ -77,6 +77,10 @@ const (
 	OSSGetBucketTaggingAction           Action = OSSActionPrefix + "GetBucketTagging"
 	OSSPutBucketTaggingAction           Action = OSSActionPrefix + "PutBucketTagging"
 	OSSDeleteBucketTaggingAction        Action = OSSActionPrefix + "DeleteBucketTagging"
+	OSSGetBucketCorsAction              Action = OSSActionPrefix + "GetBucketCORS"
+	OSSPutBucketCorsAction              Action = OSSActionPrefix + "PutBucketCORS"
+	OSSDeleteBucketCorsAction           Action = OSSActionPrefix + "DeleteBucketCORS"
+	//todo cors in acl?
 
 	// constants for POSIX file system interface
 	POSIXReadAction  Action = POSIXActionPrefix + "Read"
@@ -130,6 +134,9 @@ var (
 		OSSGetBucketTaggingAction,
 		OSSPutBucketTaggingAction,
 		OSSDeleteBucketTaggingAction,
+		OSSGetBucketCorsAction,
+		OSSPutBucketCorsAction,
+		OSSDeleteBucketCorsAction,
 
 		// posix file system interface actions
 		POSIXReadAction,
@@ -263,6 +270,7 @@ var (
 			OSSListObjectXAttrsAction,
 			OSSGetObjectTaggingAction,
 			OSSGetBucketTaggingAction,
+			OSSGetBucketCorsAction,
 
 			// file system interface
 			POSIXReadAction,
@@ -304,6 +312,9 @@ var (
 			OSSPutObjectTaggingAction,
 			OSSDeleteObjectTaggingAction,
 			OSSGetBucketTaggingAction,
+			OSSGetBucketCorsAction,
+			OSSPutBucketCorsAction,
+			OSSDeleteBucketCorsAction,
 
 			// file system interface
 			POSIXReadAction,

cubefs/cubefs

objectnode/cors_handler.go
@@ -1,0 +1,129 @@
+package objectnode
+
+// https://docs.aws.amazon.com/zh_cn/AmazonS3/latest/dev/EnableCorsUsingREST.html
+
+import (
+	"encoding/json"
+	"encoding/xml"
+	"io"
+	"io/ioutil"
+	"net/http"
+
+	"github.com/chubaofs/chubaofs/util/log"
+)
+
+// https://docs.aws.amazon.com/AmazonS3/latest/API/API_GetBucketCors.html
+func (o *ObjectNode) getBucketCorsHandler(w http.ResponseWriter, r *http.Request) {
+	log.LogInfof("Get bucket cors")
+	var (
+		err error
+		ec  *ErrorCode
+	)
+	defer o.errorResponse(w, r, err, ec)
+
+	var param *RequestParam
+	param = ParseRequestParam(r)
+	if param.Bucket() == "" {
+		ec = NoSuchBucket
+		return
+	}
+
+	var vol *Volume
+	if vol, err = o.vm.Volume(param.Bucket()); err != nil {
+		ec = NoSuchBucket
+		return
+	}
+
+	var output = CORSConfiguration{}
+
+	cors := vol.loadCors()
+	if cors != nil {
+		output.CORSRule = cors.CORSRule
+	}
+	var corsData []byte
+	if corsData, err = xml.Marshal(output); err != nil {
+		ec = InternalErrorCode(err)
+		return
+	}
+
+	_, _ = w.Write(corsData)
+	return
+}
+
+// https://docs.aws.amazon.com/AmazonS3/latest/API/API_PutBucketCors.html
+func (o *ObjectNode) putBucketCorsHandler(w http.ResponseWriter, r *http.Request) {
+	log.LogInfof("Put bucket cors")
+
+	var (
+		err error
+		ec  *ErrorCode
+	)
+	defer o.errorResponse(w, r, err, ec)
+
+	var param *RequestParam
+	param = ParseRequestParam(r)
+	if param.Bucket() == "" {
+		ec = NoSuchBucket
+		return
+	}
+	var vol *Volume
+	if vol, err = o.vm.Volume(param.Bucket()); err != nil {
+		ec = NoSuchBucket
+		return
+	}
+
+	var bytes []byte
+	if bytes, err = ioutil.ReadAll(r.Body); err != nil && err != io.EOF {
+		return
+	}
+
+	var corsConfig *CORSConfiguration
+	if corsConfig, err = ParseCorsConfig(bytes); err != nil {
+		return
+	}
+	if corsConfig == nil {
+		return
+	}
+
+	var newBytes []byte
+	if newBytes, err = json.Marshal(corsConfig); err != nil {
+		return
+	}
+	if err = storeBucketCors(newBytes, vol); err != nil {
+		return
+	}
+	vol.storeCors(corsConfig)
+
+	return
+}
+
+// https://docs.aws.amazon.com/AmazonS3/latest/API/API_DeleteBucketCors.html
+func (o *ObjectNode) deleteBucketCorsHandler(w http.ResponseWriter, r *http.Request) {
+	log.LogInfof("Delete bucket cors")
+
+	var (
+		err error
+		ec  *ErrorCode
+	)
+	defer o.errorResponse(w, r, err, ec)
+
+	var param *RequestParam
+	param = ParseRequestParam(r)
+	if param.Bucket() == "" {
+		ec = NoSuchBucket
+		return
+	}
+	var vol *Volume
+	if vol, err = o.vm.Volume(param.Bucket()); err != nil {
+		ec = NoSuchBucket
+		return
+	}
+
+	if err = deleteBucketCors(vol); err != nil {
+		return
+	}
+	vol.storeCors(nil)
+
+	w.WriteHeader(http.StatusNoContent)
+	return
+}

proto/perm_action.go
@@ -71,6 +71,11 @@ const (
 	OSSGetBucketAclAction Action = OSSActionPrefix + "GetBucketAcl"
 	OSSPutBucketAclAction Action = OSSActionPrefix + "PutBucketAcl"
 
+	// Bucket CORS actions
+	OSSGetBucketCorsAction    Action = OSSActionPrefix + "GetBucketCors"
+	OSSPutBucketCorsAction    Action = OSSActionPrefix + "PutBucketCors"
+	OSSDeleteBucketCorsAction Action = OSSActionPrefix + "DeleteBucketCors"
+
 	// Object torrent actions
 	OSSGetObjectTorrentAction Action = OSSActionPrefix + "GetObjectTorrent" // unsupported
 
@@ -128,11 +133,6 @@ const (
 	OSSGetBucketEncryptionAction    Action = OSSActionPrefix + "GetBucketEncryption"    // unsupported
 	OSSPutBucketEncryptionAction    Action = OSSActionPrefix + "PutBucketEncryption"    // unsupported
 	OSSDeleteBucketEncryptionAction Action = OSSActionPrefix + "DeleteBucketEncryption" // unsupported
-
-	// Bucket CORS actions
-	OSSGetBucketCorsAction    Action = OSSActionPrefix + "GetBucketCors"    // unsupported
-	OSSPutBucketCorsAction    Action = OSSActionPrefix + "PutBucketCors"    // unsupported
-	OSSDeleteBucketCorsAction Action = OSSActionPrefix + "DeleteBucketCors" // unsupported
 
 	// Bucket website actions
 	OSSGetBucketWebsiteAction    Action = OSSActionPrefix + "GetBucketWebsite"    // unsupported