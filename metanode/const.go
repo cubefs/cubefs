0dfc0af064d6995dd20efa51aa26596101b9bec6
awzhgw <guowl18702995996@gmail.com>
2020-06-19 13:36:40 +0800

    Enhancment:Improve metanode startup loading speed
    Signed-off-by: awzhgw <guowl18702995996@gmail.com>

--- metanode/const.go
+++ metanode/const.go
@@ -157,7 +157,7 @@ const (
 	intervalToSyncCursor  = time.Minute * 1
 
 	// The minimum gap of the last ApplyID that triggered the current ApplyID of the persistent data.
-	gapToPersistData = 1000
+	gapToPersistData = 100
 )
 
 const (
