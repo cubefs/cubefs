122b5b37fb5b0c0b28c6fcd6e4ce6b48c83a851e
zhuzhengyi1 <zhuzhengyi@jd.com>
2020-07-30 17:40:54 +0800

    Fix: add EXTENT_DEL file extentKey header and checksum verify
    Signed-off-by: zhuzhengyi1 <zhuzhengyi@jd.com>
    Signed-off-by: hooklee2000 <hooklee2000@gmail.com>

--- metanode/partition_item.go
+++ metanode/partition_item.go
@@ -163,6 +163,9 @@ func newMetaItemIterator(mp *metaPartition) (si *MetaItemIterator, err error) {
 		if !fileInfo.IsDir() && strings.HasPrefix(fileInfo.Name(), prefixDelExtent) {
 			filenames = append(filenames, fileInfo.Name())
 		}
+		if !fileInfo.IsDir() && strings.HasPrefix(fileInfo.Name(), prefixDelExtentV2) {
+			filenames = append(filenames, fileInfo.Name())
+		}
 	}
 	si.filenames = filenames
 
