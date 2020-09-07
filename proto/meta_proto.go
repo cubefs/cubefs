ZhuZhengyi/cfs

add vol mp storeType arg for createVol api
Signed-off-by: zhuzhengyi1 <zhuzhengyi@jd.com>

proto/meta_proto.go
@@ -35,6 +35,7 @@ type Peer struct {
 type CreateMetaPartitionRequest struct {
 	MetaId      string
 	VolName     string
+	StoreType   uint8
 	Start       uint64
 	End         uint64
 	PartitionID uint64

cubefs/cubefs

proto/meta_proto.go
@@ -35,6 +35,7 @@ type Peer struct {
 type CreateMetaPartitionRequest struct {
 	MetaId      string
 	VolName     string
+	StoreType   StoreType
 	Start       uint64
 	End         uint64
 	PartitionID uint64