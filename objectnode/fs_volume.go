73c5cbbb90012111f86710b201b6f34f28dc5643
yuboLee <pangbolee@gmail.com>
2020-09-09 15:51:18 +0800

    Resolve conflict
    Signed-off-by: yuboLee <pangbolee@gmail.com>

--- objectnode/fs_volume.go
+++ objectnode/fs_volume.go
@@ -1427,11 +1427,12 @@ func (v *Volume) recursiveMakeDirectory(path string) (ino uint64, err error) {
 			var info *proto.InodeInfo
 			info, err = v.mw.Create_ll(ino, pathItem.Name, uint32(DefaultDirMode), 0, 0, nil)
 			if err != nil && err == syscall.EEXIST {
-				_, mode, e := v.mw.Lookup_ll(ino, pathItem.Name)
+				existInode, mode, e := v.mw.Lookup_ll(ino, pathItem.Name)
 				if e != nil {
 					return
 				}
 				if os.FileMode(mode).IsDir() {
+					ino, err = existInode, nil
 					continue
 				}
 			}
