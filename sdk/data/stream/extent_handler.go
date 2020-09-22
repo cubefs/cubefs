08054c157ab7836647d89aac3ddc8c0e373085a5
yinlei-jinan <297155992@qq.com>
2020-09-22 14:52:42 +0800

    fix: remove failed dp when create extent and fix RemoveDP bug for DefaultRandomSelector
    Signed-off-by: yinlei-jinan <297155992@qq.com>

--- sdk/data/stream/extent_handler.go
+++ sdk/data/stream/extent_handler.go
@@ -17,7 +17,6 @@ package stream
 import (
 	"fmt"
 	"net"
-	"strings"
 	"sync/atomic"
 	"time"
 
@@ -498,13 +497,9 @@ func (eh *ExtentHandler) allocateExtent() (err error) {
 			extID, err = eh.createExtent(dp)
 		}
 		if err != nil {
-			if strings.Contains(err.Error(), "DiskNoSpaceErr") {
-				log.LogWarnf("allocateExtent: delete dp[%v] caused by create extent failed, eh(%v) err(%v) exclude(%v)",
-					dp, eh, err, exclude)
-				eh.stream.client.dataWrapper.RemoveDataPartitionForWrite(dp.PartitionID)
-				continue
-			}
-			log.LogWarnf("allocateExtent: failed to create extent, eh(%v) err(%v) exclude(%v)", eh, err, exclude)
+			log.LogWarnf("allocateExtent: delete dp[%v] caused by create extent failed, eh(%v) err(%v) exclude(%v)",
+				dp, eh, err, exclude)
+			eh.stream.client.dataWrapper.RemoveDataPartitionForWrite(dp.PartitionID)
 			dp.CheckAllHostsIsAvail(exclude)
 			continue
 		}
