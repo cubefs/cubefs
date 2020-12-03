fc7c4b01e3ca05685a4d4a7a8208d1a3e263c09d
awzhgw <guowl18702995996@gmail.com>
2020-12-03 14:05:31 +0800

    fix: ci failed
    Signed-off-by: awzhgw <guowl18702995996@gmail.com>

--- master/cluster_test.go
+++ master/cluster_test.go
@@ -21,7 +21,8 @@ func buildPanicVol() *Vol {
 	}
 	var createTime = time.Now().Unix() // record create time of this volume
 	vol := newVol(id, commonVol.Name, commonVol.Owner, testZone1+","+testZone2, commonVol.dataPartitionSize, commonVol.Capacity,
-		defaultReplicaNum, defaultReplicaNum, false, false, true, true, createTime, "")
+		defaultReplicaNum, defaultReplicaNum, false, false, true,
+		true, createTime, "","","")
 
 	vol.dataPartitions = nil
 	return vol
