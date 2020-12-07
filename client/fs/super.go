29fbeb997b63f56eec3d72e43b108f773d42dbfd
sunshijie8 <sunshijie@jd.com>
2020-12-07 12:42:25 +0800

    fix:remove act type from ump alarm key

--- client/fs/super.go
+++ client/fs/super.go
@@ -192,23 +192,24 @@ func (s *Super) exporterKey(act string) string {
 	return fmt.Sprintf("%v_fuseclient_%v", s.cluster, act)
 }
 
-func (s *Super) umpKey(act string) string {
-	return fmt.Sprintf("%v_fuseclient_%v", s.cluster, act)
+func (s *Super) umpKey() string {
+	return fmt.Sprintf("%v_client_warning", s.cluster)
 }
 
 func (s *Super) umpFunctionKey(act string) string {
 	return fmt.Sprintf("%s_%s_%s", s.cluster, s.volname, act)
 }
 
-func (s *Super) umpAlarmKey(act string) string {
-	return fmt.Sprintf("%s_%s_%s_warning", s.cluster, s.volname, act)
+func (s *Super) umpAlarmKey() string {
+	return fmt.Sprintf("%s_%s_warning", s.cluster, s.volname)
 }
 
 func (s *Super) handleError(op, msg string) {
 	log.LogError(msg)
 
-	ump.Alarm(s.umpAlarmKey(op), msg)
+	errmsg1 := fmt.Sprintf("act(%v) - %v", op, msg)
+	ump.Alarm(s.umpAlarmKey(), errmsg1)
 
-	errmsg := fmt.Sprintf("volume(%s) - %s", s.volname, msg)
-	ump.Alarm(s.umpKey(op), errmsg)
+	errmsg2 := fmt.Sprintf("volume(%v) %v", s.volname, errmsg1)
+	ump.Alarm(s.umpKey(), errmsg2)
 }
