f9ccc36173032f99382fa983cb45ace1242aed6d
xuxihao1 <xuxihao3@jd.com>
2020-12-09 17:00:30 +0800

    Fix: docker ltptest failed
    Signed-off-by: xuxihao1 <xuxihao3@jd.com>

--- docker/script/run_test.sh
+++ docker/script/run_test.sh
@@ -97,7 +97,7 @@ create_volume() {
         echo -e "\033[32mdone\033[0m"
         return
     fi
-    ${cli} volume create ${VolName} ${Owner} --zonename=zone-01 --capacity=30 -y > /dev/null
+    ${cli} volume create ${VolName} ${Owner} --capacity=30 -y > /dev/null
     if [[ $? -ne 0 ]]; then
         echo -e "\033[31mfail\033[0m"
         exit 1
