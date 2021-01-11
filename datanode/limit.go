0bbc1d33b1c7c31c1a15fab90947623e2771ab53
xrefft <cserxiao@qq.com>
2021-01-11 11:31:41 +0800

    fix: fix repair limit bug for func setDoExtentRepair
    Signed-off-by: xrefft <cserxiao@qq.com>

--- datanode/limit.go
+++ datanode/limit.go
@@ -10,11 +10,13 @@ var (
 	deleteLimiteRater       = rate.NewLimiter(rate.Inf, defaultMarkDeleteLimitBurst)
 	MaxExtentRepairLimit    = 20000
 	MinExtentRepairLimit    = 5
+	CurExtentRepairLimit    = -1
 	extentRepairLimiteRater = make(chan struct{}, MaxExtentRepairLimit)
 )
 
 func requestDoExtentRepair() (err error) {
-	err = fmt.Errorf("cannot do extentRepair")
+	err = fmt.Errorf("repair limit, cannot do extentRepair")
+
 	select {
 	case <-extentRepairLimiteRater:
 		return nil
@@ -38,14 +40,23 @@ func setDoExtentRepair(value int) {
 	if value <= 0 {
 		value = MaxExtentRepairLimit
 	}
-	close(extentRepairLimiteRater)
+
 	if value > MaxExtentRepairLimit {
 		value = MaxExtentRepairLimit
 	}
+
 	if value < MinExtentRepairLimit {
 		value = MinExtentRepairLimit
 	}
-	extentRepairLimiteRater = make(chan struct{}, value)
+
+	if CurExtentRepairLimit != value {
+		CurExtentRepairLimit = value
+		close(extentRepairLimiteRater)
+		extentRepairLimiteRater = make(chan struct{}, CurExtentRepairLimit)
+		for i := 0; i < CurExtentRepairLimit; i++ {
+			extentRepairLimiteRater <- struct{}{}
+		}
+	}
 }
 
 func DeleteLimiterWait() {
