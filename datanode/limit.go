8f5bbf485013c5226d30b8c3542c9d28555f83d1
jadewang198510 <741773046@qq.com>
2021-01-12 10:23:56 +0800

    fix: modify repair limit issue
    Signed-off-by: jadewang198510 <741773046@qq.com>

--- datanode/limit.go
+++ datanode/limit.go
@@ -10,15 +10,22 @@ var (
 	deleteLimiteRater       = rate.NewLimiter(rate.Inf, defaultMarkDeleteLimitBurst)
 	MaxExtentRepairLimit    = 20000
 	MinExtentRepairLimit    = 5
-	CurExtentRepairLimit    = -1
-	extentRepairLimiteRater = make(chan struct{}, MaxExtentRepairLimit)
+	CurExtentRepairLimit    = MaxExtentRepairLimit
+	extentRepairLimitRater chan struct{}
 )
+
+func initRepairLimit() {
+	extentRepairLimitRater = make(chan struct{}, MaxExtentRepairLimit)
+	for i := 0; i < MaxExtentRepairLimit; i++ {
+		extentRepairLimitRater <- struct{}{}
+	}
+}
 
 func requestDoExtentRepair() (err error) {
 	err = fmt.Errorf("repair limit, cannot do extentRepair")
 
 	select {
-	case <-extentRepairLimiteRater:
+	case <-extentRepairLimitRater:
 		return nil
 	default:
 		return
@@ -29,7 +36,7 @@ func requestDoExtentRepair() (err error) {
 
 func fininshDoExtentRepair() {
 	select {
-	case extentRepairLimiteRater <- struct{}{}:
+	case extentRepairLimitRater <- struct{}{}:
 		return
 	default:
 		return
@@ -51,10 +58,10 @@ func setDoExtentRepair(value int) {
 
 	if CurExtentRepairLimit != value {
 		CurExtentRepairLimit = value
-		close(extentRepairLimiteRater)
-		extentRepairLimiteRater = make(chan struct{}, CurExtentRepairLimit)
+		close(extentRepairLimitRater)
+		extentRepairLimitRater = make(chan struct{}, CurExtentRepairLimit)
 		for i := 0; i < CurExtentRepairLimit; i++ {
-			extentRepairLimiteRater <- struct{}{}
+			extentRepairLimitRater <- struct{}{}
 		}
 	}
 }
