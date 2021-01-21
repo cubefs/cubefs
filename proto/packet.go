wenjia322/chubaofs

feature: add raft learner on datanode
Signed-off-by: wenjia322 <buaa1214wwj@126.com>

proto/packet.go
@@ -113,16 +113,18 @@ const (
 	OpPromoteMetaPartitionRaftLearner uint8 = 0x4D
 
 	// Operations: Master -> DataNode
-	OpCreateDataPartition           uint8 = 0x60
-	OpDeleteDataPartition           uint8 = 0x61
-	OpLoadDataPartition             uint8 = 0x62
-	OpDataNodeHeartbeat             uint8 = 0x63
-	OpReplicateFile                 uint8 = 0x64
-	OpDeleteFile                    uint8 = 0x65
-	OpDecommissionDataPartition     uint8 = 0x66
-	OpAddDataPartitionRaftMember    uint8 = 0x67
-	OpRemoveDataPartitionRaftMember uint8 = 0x68
-	OpDataPartitionTryToLeader      uint8 = 0x69
+	OpCreateDataPartition             uint8 = 0x60
+	OpDeleteDataPartition             uint8 = 0x61
+	OpLoadDataPartition               uint8 = 0x62
+	OpDataNodeHeartbeat               uint8 = 0x63
+	OpReplicateFile                   uint8 = 0x64
+	OpDeleteFile                      uint8 = 0x65
+	OpDecommissionDataPartition       uint8 = 0x66
+	OpAddDataPartitionRaftMember      uint8 = 0x67
+	OpRemoveDataPartitionRaftMember   uint8 = 0x68
+	OpDataPartitionTryToLeader        uint8 = 0x69
+	OpAddDataPartitionRaftLearner     uint8 = 0x6A
+	OpPromoteDataPartitionRaftLearner uint8 = 0x6B
 
 	// Operations: MultipartInfo
 	OpCreateMultipart  uint8 = 0x70
@@ -347,6 +349,10 @@ func (p *Packet) GetOpMsg() (m string) {
 		m = "OpRemoveDataPartitionRaftMember"
 	case OpAddDataPartitionRaftMember:
 		m = "OpAddDataPartitionRaftMember"
+	case OpAddDataPartitionRaftLearner:
+		m = "OpAddDataPartitionRaftLearner"
+	case OpPromoteDataPartitionRaftLearner:
+		m = "OpPromoteDataPartitionRaftLearner"
 	case OpAddMetaPartitionRaftMember:
 		m = "OpAddMetaPartitionRaftMember"
 	case OpRemoveMetaPartitionRaftMember:

cubefs/cubefs

proto/packet.go
@@ -113,17 +113,19 @@ const (
 	OpPromoteMetaPartitionRaftLearner uint8 = 0x4D
 
 	// Operations: Master -> DataNode
-	OpCreateDataPartition           uint8 = 0x60
-	OpDeleteDataPartition           uint8 = 0x61
-	OpLoadDataPartition             uint8 = 0x62
-	OpDataNodeHeartbeat             uint8 = 0x63
-	OpReplicateFile                 uint8 = 0x64
-	OpDeleteFile                    uint8 = 0x65
-	OpDecommissionDataPartition     uint8 = 0x66
-	OpAddDataPartitionRaftMember    uint8 = 0x67
-	OpRemoveDataPartitionRaftMember uint8 = 0x68
-	OpDataPartitionTryToLeader      uint8 = 0x69
-	OpSyncDataPartitionReplicas     uint8 = 0x6A
+	OpCreateDataPartition           	uint8 = 0x60
+	OpDeleteDataPartition           	uint8 = 0x61
+	OpLoadDataPartition             	uint8 = 0x62
+	OpDataNodeHeartbeat             	uint8 = 0x63
+	OpReplicateFile                 	uint8 = 0x64
+	OpDeleteFile                    	uint8 = 0x65
+	OpDecommissionDataPartition     	uint8 = 0x66
+	OpAddDataPartitionRaftMember    	uint8 = 0x67
+	OpRemoveDataPartitionRaftMember 	uint8 = 0x68
+	OpDataPartitionTryToLeader      	uint8 = 0x69
+	OpSyncDataPartitionReplicas     	uint8 = 0x6A
+	OpAddDataPartitionRaftLearner     	uint8 = 0x6B
+	OpPromoteDataPartitionRaftLearner	uint8 = 0x6C
 
 	// Operations: MultipartInfo
 	OpCreateMultipart  uint8 = 0x70
@@ -356,6 +358,10 @@ func (p *Packet) GetOpMsg() (m string) {
 		m = "OpRemoveDataPartitionRaftMember"
 	case OpAddDataPartitionRaftMember:
 		m = "OpAddDataPartitionRaftMember"
+	case OpAddDataPartitionRaftLearner:
+		m = "OpAddDataPartitionRaftLearner"
+	case OpPromoteDataPartitionRaftLearner:
+		m = "OpPromoteDataPartitionRaftLearner"
 	case OpAddMetaPartitionRaftMember:
 		m = "OpAddMetaPartitionRaftMember"
 	case OpRemoveMetaPartitionRaftMember: