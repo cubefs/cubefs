d8e0bef4e3ecd35b68db854a3e75ba3ea1de52ae
leonrayang <chl696@sina.com>
2021-03-25 00:36:29 -0700

    bug fix. partition be split can be write if not full and less then on step
    online fix.partition already build with large scope need be set readonly

--- master/meta_partition.go
+++ master/meta_partition.go
@@ -260,12 +260,17 @@ func (mp *MetaPartition) checkStatus(clusterID string, writeLog bool, replicaNum
 				continue
 			}
 			if mp.PartitionID == maxPartitionID {
-				msg := fmt.Sprintf("split[checkStatus],id:%v,status:%v,replicaNum:%v,InodeCount:%v",
+				msg := fmt.Sprintf("split[checkStatus] need split,id:%v,status:%v,replicaNum:%v,InodeCount:%v",
 					mp.PartitionID, mp.Status, mp.ReplicaNum, mp.InodeCount)
 				log.LogInfo(msg)
 				doSplit = true
 			} else {
-				mp.Status = proto.ReadOnly
+				if(mr.metaNode.reachesThreshold() || mp.End - mp.MaxInodeID > 2 * defaultMetaPartitionInodeIDStep) {
+					msg := fmt.Sprintf("split[checkStatus],change state,id:%v,status:%v,replicaNum:%v,replicas:%v,persistenceHosts:%v, inodeCount:%v, MaxInodeID:%v, start:%v, end:%v",
+						mp.PartitionID, mp.Status, mp.ReplicaNum, len(liveReplicas), mp.Hosts, mp.InodeCount, mp.MaxInodeID, mp.Start, mp.End)
+						log.LogInfo(msg)
+					mp.Status = proto.ReadOnly
+				}
 			}
 		}
 	}
