04504647052f16fbedc70301622da34e171f25a4
xrefft <cserxiao@qq.com>
2021-04-01 17:03:06 +0800

    fix: fix datanode repair log print error 2
    Signed-off-by: xrefft <cserxiao@qq.com>
    Signed-off-by: mingwei <gongwilliam@163.com>

--- datanode/data_partition_repair.go
+++ datanode/data_partition_repair.go
@@ -412,7 +412,7 @@ func (dp *DataPartition) notifyFollower(wg *sync.WaitGroup, index int, members [
 		if err == nil {
 			log.LogInfof(ActionNotifyFollowerToRepair+" to host(%v) Partition(%v) done", target, dp.partitionID)
 		} else {
-			log.LogErrorf(ActionNotifyFollowerToRepair+" to host(%v) Partition(%v) failed", target, dp.partitionID, err)
+			log.LogErrorf(ActionNotifyFollowerToRepair+" to host(%v) Partition(%v) failed, err(%v)", target, dp.partitionID, err)
 		}
 	}()
 	if err != nil {
