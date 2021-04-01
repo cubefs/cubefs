764d1e496df91fe32e38dd721a12e358fbeef2a5
xrefft <cserxiao@qq.com>
2021-04-01 19:49:01 +0800

    fix bug when packet opCode=OpExtentRepairRead and network is unstable
    Signed-off-by: mingwei <gongwilliam@163.com>

--- datanode/data_partition_repair.go
+++ datanode/data_partition_repair.go
@@ -534,7 +534,7 @@ func (dp *DataPartition) streamRepairExtent(remoteExtentInfo *storage.ExtentInfo
 
 		if reply.ResultCode != proto.OpOk {
 			err = errors.Trace(fmt.Errorf("unknow result code"),
-				"streamRepairExtent receive opcode error(%v) ,localExtentSize(%v) remoteExtentSize(%v)", string(reply.Data[:reply.Size]), currFixOffset, remoteExtentInfo.Size)
+				"streamRepairExtent receive opcode error(%v) ,localExtentSize(%v) remoteExtentSize(%v)", string(reply.Data[:intMin(len(reply.Data), int(reply.Size))]), currFixOffset, remoteExtentInfo.Size)
 			return
 		}
 
@@ -601,4 +601,12 @@ func (dp *DataPartition) streamRepairExtent(remoteExtentInfo *storage.ExtentInfo
 	}
 	return
 
+}
+
+func intMin(a, b int) int {
+	if a < b {
+		return a
+	} else {
+		return b
+	}
 }
