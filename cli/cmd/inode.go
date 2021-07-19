d0844eaf28d49177741242c6a7397e79c6fb22ca
xiaopeng42 <xiaopeng42@jd.com>
2021-07-19 11:09:24 +0800

    enhancement: add inode replicas consistency check

--- cli/cmd/inode.go
+++ cli/cmd/inode.go
@@ -3,29 +3,33 @@ package cmd
 import (
 	"encoding/json"
 	"fmt"
-	"github.com/chubaofs/chubaofs/proto"
-	sdk "github.com/chubaofs/chubaofs/sdk/master"
-	"github.com/spf13/cobra"
 	"io/ioutil"
 	"net/http"
 	"strconv"
 	"strings"
+
+	"github.com/chubaofs/chubaofs/proto"
+	sdk "github.com/chubaofs/chubaofs/sdk/master"
+	"github.com/spf13/cobra"
 )
 
 const (
-	cmdInodeUse   = "inode  [COMMAND]"
-	cmdInodeInfoUse   = "info  volumeName inodeId"
-	cmdInodeShort = "Show inode information"
+	cmdInodeUse          = "inode [COMMAND]"
+	cmdInodeInfoUse      = "info volumeName inodeId"
+	cmdInodeShort        = "Show inode information"
+	cmdCheckReplicaUse   = "check-replica volumeName inodeId(comma separated list)"
+	cmdCheckReplicaShort = "Check inode replica consistency"
 )
 
 func newInodeCmd(client *sdk.MasterClient) *cobra.Command {
 	var cmd = &cobra.Command{
-		Use:     cmdInodeUse,
-		Short:   cmdInodeShort,
-		Args:    cobra.MinimumNArgs(1),
+		Use:   cmdInodeUse,
+		Short: cmdInodeShort,
+		Args:  cobra.MinimumNArgs(1),
 	}
 	cmd.AddCommand(
 		newInodeInfoCmd(client),
+		newCheckReplicaCmd(client),
 	)
 	return cmd
 }
@@ -54,13 +58,13 @@ func newInodeInfoCmd(client *sdk.MasterClient) *cobra.Command {
 			}
 			for _, view := range views {
 				inodeStartNumber := view.Start
-				inodeEndNumber  := view.End
+				inodeEndNumber := view.End
 				metaPartitionNumber := view.PartitionID
-				if inodeStartNumber < inodeUnint64Number  && inodeUnint64Number <  inodeEndNumber{
-					for _, addr := range view.Members{
+				if inodeStartNumber < inodeUnint64Number && inodeUnint64Number < inodeEndNumber {
+					for _, addr := range view.Members {
 						addr := strings.Split(addr, ":")[0]
 						metaNodeProfPort := client.MetaNodeProfPort
-						if metaNodeProfPort == 0{
+						if metaNodeProfPort == 0 {
 							metaNodeProfPort = 17220
 						}
 						resp, err := http.Get(fmt.Sprintf("http://%s:%d/getInode?pid=%d&ino=%d", addr, metaNodeProfPort, metaPartitionNumber, inodeUnint64Number))
@@ -80,7 +84,7 @@ func newInodeInfoCmd(client *sdk.MasterClient) *cobra.Command {
 							errout("get partition info failed:\n%v\n", err)
 							return
 						}
-						if value["msg"] != "Ok"{
+						if value["msg"] != "Ok" {
 							errout("get inode information failed: %v\n", value["msg"])
 							return
 						}
@@ -99,7 +103,7 @@ func newInodeInfoCmd(client *sdk.MasterClient) *cobra.Command {
 							Uid:         uint64(dataInfo["uid"].(float64)),
 							Mode:        uint64(dataInfo["mode"].(float64)),
 						}
-						stdout("Summary of inode  :\n%s\n",  formatInodeInfoView(inodeInfoView))
+						stdout("Summary of inode  :\n%s\n", formatInodeInfoView(inodeInfoView))
 
 						// getExtentsByInode
 						resp, err = http.Get(fmt.Sprintf("http://%s:%d/getExtentsByInode?pid=%d&ino=%d", addr, metaNodeProfPort, metaPartitionNumber, inodeUnint64Number))
@@ -119,15 +123,15 @@ func newInodeInfoCmd(client *sdk.MasterClient) *cobra.Command {
 							errout("get partition info failed:\n%v\n", err)
 							return
 						}
-						if value["msg"] != "Ok"{
+						if value["msg"] != "Ok" {
 							errout("get inode information failed: %v\n", value["msg"])
 							return
 						}
 						data = value["data"].(map[string]interface{})
 						if data["eks"] != nil {
 							dataEks := data["eks"].([]interface{})
-							stdout("Summary of inodeExtentInfo  :\n%s\n",  inodeExtentInfoTableHeader)
-							for _, ek := range dataEks{
+							stdout("Summary of inodeExtentInfo  :\n%s\n", inodeExtentInfoTableHeader)
+							for _, ek := range dataEks {
 								inodeExtentInfo := ek.(map[string]interface{})
 								var inodeExtentInfoView *proto.InodeExtentInfoView
 								inodeExtentInfoView = &proto.InodeExtentInfoView{
@@ -157,3 +161,147 @@ func newInodeInfoCmd(client *sdk.MasterClient) *cobra.Command {
 	return cmd
 }
 
+type ExtentMd5 struct {
+	PartitionID uint64 `json:"PartitionID"`
+	ExtentID    uint64 `json:"ExtentID"`
+	Md5         string `json:"md5"`
+}
+
+func newCheckReplicaCmd(client *sdk.MasterClient) *cobra.Command {
+	var cmd = &cobra.Command{
+		Use:   cmdCheckReplicaUse,
+		Short: cmdCheckReplicaShort,
+		Args:  cobra.MinimumNArgs(2),
+		Run: func(cmd *cobra.Command, args []string) {
+			var volumeName = args[0]
+			var inodeStr = args[1]
+			inodeSlice := strings.Split(inodeStr, ",")
+			var inodes []uint64
+			for _, inode := range inodeSlice {
+				ino, err := strconv.Atoi(inode)
+				if err != nil {
+					continue
+				}
+				inodes = append(inodes, uint64(ino))
+			}
+			for _, inode := range inodes {
+				checkInode(client, volumeName, inode)
+			}
+			return
+		},
+		ValidArgsFunction: func(cmd *cobra.Command, args []string, toComplete string) ([]string, cobra.ShellCompDirective) {
+			if len(args) != 0 {
+				return nil, cobra.ShellCompDirectiveNoFileComp
+			}
+			return validVols(client, toComplete), cobra.ShellCompDirectiveNoFileComp
+		},
+	}
+	return cmd
+}
+
+func checkInode(client *sdk.MasterClient, vol string, inode uint64) {
+	var err error
+	defer func() {
+		if err != nil {
+			errout("checkInode failed: %v", err)
+		}
+	}()
+	var (
+		extentsResp *proto.GetExtentsResponse
+		errCount    int = 0
+		partition   *proto.DataPartitionInfo
+		extentMd5   *ExtentMd5
+	)
+	extentsResp, err = getExtentsByInode(client, vol, inode)
+	if err != nil {
+		return
+	}
+	stdout("begin check, inode: %d, extent count: %d\n", inode, len(extentsResp.Extents))
+	for _, ek := range extentsResp.Extents {
+		partition, err = client.AdminAPI().GetDataPartition("", ek.PartitionId)
+		if err != nil {
+			return
+		}
+		var preDatanode string
+		var preMd5 string
+		for idx, replica := range partition.Replicas {
+			datanode := strings.ReplaceAll(replica.Addr, "17030", "17031")
+			extentMd5, err = getExtentMd5(datanode, ek.PartitionId, ek.ExtentId)
+			if err != nil {
+				return
+			}
+			msg := fmt.Sprintf("dp: %d, extent: %d, datanode: %s, md5: %s\n", ek.PartitionId, ek.ExtentId, datanode, extentMd5.Md5)
+			if idx == 0 || extentMd5.Md5 == preMd5 {
+				stdout(msg)
+			} else {
+				errout("ERROR %s, preDatanode: %s, preMd5: %s\n", msg, preDatanode, preMd5)
+				errCount++
+			}
+			preDatanode = datanode
+			preMd5 = extentMd5.Md5
+		}
+	}
+	stdout("finish check, inode: %d, err count: %d\n", inode, errCount)
+}
+
+func getExtentsByInode(client *sdk.MasterClient, vol string, inode uint64) (re *proto.GetExtentsResponse, err error) {
+	mps, err := client.ClientAPI().GetMetaPartitions(vol)
+	if err != nil {
+		return
+	}
+	var metanode string
+	var mpId uint64
+	for _, mp := range mps {
+		if inode >= mp.Start && inode < mp.End {
+			metanode = mp.LeaderAddr
+			mpId = mp.PartitionID
+			break
+		}
+	}
+	addressInfo := strings.Split(metanode, ":")
+	metanode = fmt.Sprintf("%s:%d", addressInfo[0], client.MetaNodeProfPort)
+	resp, err := http.Get(fmt.Sprintf("http://%s/getExtentsByInode?pid=%d&ino=%d", metanode, mpId, inode))
+	if err != nil {
+		return
+	}
+	respData, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		return
+	}
+	var data []byte
+	if data, err = parseResp(respData); err != nil {
+		return
+	}
+	re = &proto.GetExtentsResponse{}
+	if err = json.Unmarshal(data, &re); err != nil {
+		return
+	}
+	return
+}
+
+func getExtentMd5(datanode string, dpId uint64, extentId uint64) (re *ExtentMd5, err error) {
+	resp, _ := http.Get(fmt.Sprintf("http://%s/computeExtentMd5?id=%d&extent=%d", datanode, dpId, extentId))
+	respData, _ := ioutil.ReadAll(resp.Body)
+	var data []byte
+	if data, err = parseResp(respData); err != nil {
+		return
+	}
+	re = &ExtentMd5{}
+	if err = json.Unmarshal(data, &re); err != nil {
+		return
+	}
+	return
+}
+
+func parseResp(resp []byte) (data []byte, err error) {
+	var body = &struct {
+		Code int32           `json:"code"`
+		Msg  string          `json:"msg"`
+		Data json.RawMessage `json:"data"`
+	}{}
+	if err = json.Unmarshal(resp, &body); err != nil {
+		return
+	}
+	data = body.Data
+	return
+}
