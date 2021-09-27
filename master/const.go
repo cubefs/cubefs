ffafad44d37f6fd12f63405ade3540ee7e8881bd
zhuzhengyi001 <zhuzhengyi001@ke.com>
2021-09-27 15:36:36 +0800

    Enhancement: Add master node stat metrics
    Signed-off-by: zhuzhengyi001 <zhuzhengyi001@ke.com>

--- master/const.go
+++ master/const.go
@@ -126,6 +126,13 @@ const (
 )
 
 const (
+	MetricRoleMaster     = "master"
+	MetricRoleMetaNode   = "metaNode"
+	MetricRoleDataNode   = "dataNode"
+	MetricRoleObjectNode = "objectNode"
+)
+
+const (
 	deleteIllegalReplicaErr       = "deleteIllegalReplicaErr "
 	addMissingReplicaErr          = "addMissingReplicaErr "
 	checkDataPartitionDiskErr     = "checkDataPartitionDiskErr  "
