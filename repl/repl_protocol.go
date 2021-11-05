54caf085a1c08c1b4ef1d7b22a6799fe61611a27
bjguoweilong <guoweilong@jd.com>
2021-11-05 12:39:00 +0800

    fix: datanode may be painc
    Signed-off-by: bjguoweilong <guoweilong@jd.com>

--- repl/repl_protocol.go
+++ repl/repl_protocol.go
@@ -457,8 +457,8 @@ func (rp *ReplProtocol) checkLocalResultAndReciveAllFollowerResponse() {
 	}
 	for index := 0; index < len(request.followersAddrs); index++ {
 		followerPacket := request.followerPackets[index]
-		followerPacket.Data=nil
 		err := <-followerPacket.respCh
+		followerPacket.Data=nil
 		if err != nil {
 			request.PackErrorBody(ActionReceiveFromFollower, err.Error())
 			return
