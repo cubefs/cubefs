84b1162cbf45ad13c2aa51331f99443b764e0a0d
Victor1319 <834863182@qq.com>
2021-11-16 11:36:03 +0800

    support delete miss replicas
    Signed-off-by: Victor1319 <834863182@qq.com>

--- datanode/wrap_operator.go
+++ datanode/wrap_operator.go
@@ -997,9 +997,8 @@ func (s *DataNode) handlePacketToRemoveDataPartitionRaftMember(p *repl.Packet) {
 	}
 	p.PartitionID = req.PartitionId
 
-	if !dp.IsExsitReplica(req.RemovePeer.Addr) {
-		log.LogInfof("recive MasterCommand: %v "+
-			"RemoveRaftPeer(%v) has not exsit", string(reqData), req.RemovePeer.Addr)
+	if !dp.needDeleteReplica(req.RemovePeer.Addr) {
+		log.LogWarnf("handlePacketToRemoveDataPartitionRaftMember, req(%s) RemoveRaftPeer(%s) is not exist", string(reqData), req.RemovePeer.Addr)
 		return
 	}
 
