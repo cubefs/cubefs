f0d35128bdef6df2686b83c0102baedf48519988
leonrayang <chl696@sina.com>
2022-01-25 16:52:40 +0800

    update. docker image path change from chubaofs to cubefs
    Signed-off-by: leonrayang <chl696@sina.com>

--- docker/build_docker.sh
+++ docker/build_docker.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
 RootPath=$(cd $(dirname $0) ; pwd)
-CfsBase="ghcr.io/chubaofs/cbfs-base:1.0-golang-1.16.12"
+CfsBase="ghcr.io/cubefs/cbfs-base:1.0-golang-1.16.12"
 
 docker build -t ${CfsBase} -f ${RootPath}/Dockerfile ${RootPath}
