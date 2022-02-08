f9f6d8bd1216a3151c0062373c17343352952583
Sheng Yong <shengyong2021@gmail.com>
2022-02-08 12:05:59 +0800

    fix: Avoid race syncing node's info when setting rdonly
    Hold cluster lock to avoid race syncUpdate[Meta|Data]Node when setting
    node as read only.
    
    Signed-off-by: Sheng Yong <shengyong2021@gmail.com>

--- master/api_service.go
+++ master/api_service.go
@@ -975,6 +975,9 @@ const (
 
 func (m *Server) setNodeRdOnly(addr string, nodeType uint32, rdOnly bool) (err error) {
 	if nodeType == TypeDataPartion {
+		m.cluster.dnMutex.Lock()
+		defer m.cluster.dnMutex.Unlock()
+
 		value, ok := m.cluster.dataNodes.Load(addr)
 		if !ok {
 			return fmt.Errorf("[setNodeRdOnly] data node %s is not exist", addr)
@@ -991,6 +994,9 @@ func (m *Server) setNodeRdOnly(addr string, nodeType uint32, rdOnly bool) (err e
 
 		return
 	}
+
+	m.cluster.mnMutex.Lock()
+	defer m.cluster.mnMutex.Unlock()
 
 	value, ok := m.cluster.metaNodes.Load(addr)
 	if !ok {
