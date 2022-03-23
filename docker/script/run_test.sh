1f4c384937acac050d4d8d63459633fcfb8fcf53
Shuoran Liu <shuoranliu@gmail.com>
2022-03-23 14:13:42 +0800

    ci-fix: disable follower read for LTP test.
    Enabling follower read option of a volume improves the availability at
    the expense of overwrites' read consistency. This does not affect the
    sequential writes' consistency.
    
    But in LTP test, we demand strict consistency for both sequential writes
    and overwrites. So let's disable follower read option for ltp test
    volumes.
    
    Signed-off-by: Shuoran Liu <shuoranliu@gmail.com>

--- docker/script/run_test.sh
+++ docker/script/run_test.sh
@@ -96,7 +96,7 @@ create_volumes() {
             echo -e "\033[32mdone\033[0m"
             continue
         fi
-        ${cli} volume create ${VolName} ${Owner} --capacity=15 -y >/dev/null
+        ${cli} volume create ${VolName} ${Owner} --capacity=15 --follower-read=false -y >/dev/null
         if [[ $? -ne 0 ]]; then
             echo -e "\033[31mfail\033[0m"
             exit 1
