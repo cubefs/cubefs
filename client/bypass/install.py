3f5464eb3387946d699830e65c5fc96b210503ca
xiaopeng42 <xiaopeng42@jd.com>
2022-04-13 17:50:14 +0800

    fix: bypass client renameat compatibility

--- client/bypass/install.py
+++ client/bypass/install.py
@@ -38,6 +38,7 @@ parser.add_argument('--tracingSamplerType', default='const', help='(const | prob
 parser.add_argument('--tracingSamplerParam', default='1', help='(default: 1)')
 parser.add_argument('--tracingReportAddr', default='jaegermysqlcfs.jd.local:6831', help='(default: jaegermysqlcfs.jd.local:6831)')
 parser.add_argument('--libDir', default='/usr/lib64', help='(default: /usr/lib64)')
+parser.add_argument('--env', default='', help='(test for test package, default: online package)')
 args = parser.parse_args()
 if args.ignorePath is None:
     args.ignorePath = ''
@@ -46,4 +47,8 @@ os.system('mkdir -p {0}'.format(args.configDir))
 with open('{0}/cfs_client.ini'.format(args.configDir), 'w') as f:
     f.write('mountPoint={0}\nignorePath={1}\nmasterAddr={2}\nvolName={3}\nowner={4}\nfollowerRead={5}\nlogDir={6}\nlogLevel={7}\napp={8}\nprofPort={9}\nmasterClient={10}\ntracingSamplerType={11}\ntracingSamplerParam={12}\ntracingReportAddr={13}\n'.format(args.mountPoint, args.ignorePath, args.masterAddr, args.volName, args.owner, args.followerRead, args.logDir, args.logLevel, args.app, args.profPort, args.masterClient, args.tracingSamplerType, args.tracingSamplerParam, args.tracingReportAddr))
 
-os.system('wget --quiet -O libcfs.tar.gz http://storage.jd.local/dpgimage/libcfs_mysql/libcfs.tar.gz && tar xzf libcfs.tar.gz && chmod 755 libcfssdk.so libcfsclient.so && mv libcfssdk.so libcfsclient.so {0} && rm -f libcfs.tar.gz argparse.py*'.format(args.libDir))
+package = "http://storage.jd.local/dpgimage/libcfs_mysql/libcfs.tar.gz"
+if args.env == "test":
+    package = "http://storage.jd.local/dpgimage/libcfs_mysql/libcfs_test.tar.gz"
+
+os.system('wget --quiet -O libcfs.tar.gz {0} && tar xzf libcfs.tar.gz && chmod 755 libcfssdk.so libcfsclient.so && mv libcfssdk.so libcfsclient.so {1} && rm -f libcfs.tar.gz argparse.py*'.format(package, args.libDir))
