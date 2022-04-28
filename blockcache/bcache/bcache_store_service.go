49c5f98da0a293c6d09b0407849048c4d592559e
tianjiongzhang <236556116@qq.com>
2022-04-28 11:43:54 +0800

    refactor:update unix domain socket path
    Signed-off-by: zhangtianjiong <hanhanztj@sina.com>

--- blockcache/bcache/bcache_store_service.go
+++ blockcache/bcache/bcache_store_service.go
@@ -40,7 +40,7 @@ import (
 )
 
 const (
-	UnixSocketPath         = "/var/lib/adls/bcache.socket"
+	UnixSocketPath         = "/var/lib/kubelet/bcache.socket"
 	CacheKey               = "cachekey"
 	CheckSum               = "md5sum"
 	OffSet                 = "offset"
