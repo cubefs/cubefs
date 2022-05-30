cff71b7bea4fdd9a06f46962f99f45b1a2b76b06
dailiwang <dailiwang@jd.com>
2022-05-30 11:36:11 +0800

    add extent_map_slice unit test

--- storage/extent_map_slice_test.go
+++ storage/extent_map_slice_test.go
@@ -11,45 +11,96 @@ import (
 	"testing"
 )
 
+var nilExtentPtr *ExtentInfoBlock = nil
+
 func initExtentInfo(id uint64) ExtentInfoBlock {
 	return ExtentInfoBlock{
 		FileID: id,
-		Size:   id,
+		Size: id+1,
 	}
 }
 
 func TestBaseFunc(t *testing.T) {
 	ms := NewMapSlice(1)
-	var id uint64 = 1
-	ms.Store(id, initExtentInfo(id))
+	var tinyExtentID uint64 = 1
+	expectExtent := initExtentInfo(tinyExtentID)
+	ms.Store(tinyExtentID, expectExtent)
 	assertEqual(t, 1, ms.Len())
-
-	ms.Delete(id)
-	assertEqual(t, 1, ms.Len())
-
-	var id1 uint64 = 65
-	ms.Store(id1, initExtentInfo(id1))
-	assertEqual(t, 2, ms.Len())
-	var id2 uint64 = 1e8
-	ms.Store(id2, initExtentInfo(id2))
-	assertEqual(t, 3, ms.Len())
-	dObj, ok := ms.Load(id2)
+	actualExtent, ok := ms.Load(tinyExtentID)
 	assertEqual(t, true, ok)
-	assertEqual(t, id2, dObj[FileID])
-	assertEqual(t, id2, dObj[Size])
+	assertEqual(t, expectExtent, *actualExtent)
+
+	ms.Delete(tinyExtentID)
+	// Do not delete tinyExtent, the length remains unchanged
+	assertEqual(t, 1, ms.Len())
+	actualExtent, ok = ms.Load(tinyExtentID)
+	assertEqual(t, true, ok)
+	assertEqual(t, expectExtent, *actualExtent)
+
+	var normalExtentId1 uint64 = 65
+	ms.Store(normalExtentId1, initExtentInfo(normalExtentId1))
+	assertEqual(t, 2, ms.Len())
+	dObj, ok := ms.Load(normalExtentId1)
+	assertEqual(t, true, ok)
+	assertEqual(t, normalExtentId1, dObj[FileID])
+	assertEqual(t, normalExtentId1+1, dObj[Size])
+
+	var normalExtentId2 uint64 = 1e8
+	ms.Store(normalExtentId2, initExtentInfo(normalExtentId2))
+	assertEqual(t, 3, ms.Len())
+	dObj, ok = ms.Load(normalExtentId2)
+	assertEqual(t, true, ok)
+	assertEqual(t, normalExtentId2, dObj[FileID])
+	assertEqual(t, normalExtentId2+1, dObj[Size])
+
+	ms.Delete(normalExtentId1)
+	assertEqual(t, uint64(1), ms.objDeletedCnt)
+	assertEqual(t, 2, ms.Len())
+	actualExtent, ok = ms.Load(normalExtentId1)
+	assertEqual(t, false, ok)
+	assertEqual(t, nilExtentPtr, actualExtent)
+
+	ms.Delete(normalExtentId2)
+	assertEqual(t, uint64(2), ms.objDeletedCnt)
+	assertEqual(t, 1, ms.Len())
+	actualExtent, ok = ms.Load(normalExtentId2)
+	assertEqual(t, false, ok)
+	assertEqual(t, nilExtentPtr, actualExtent)
 }
 
 func TestTinyExtent(t *testing.T) {
 	ms := NewMapSlice(1)
 	for i := 1; i < TinyExtentCount+1; i++ {
-		ms.Store(uint64(i), initExtentInfo(uint64(i)))
+		ei := initExtentInfo(uint64(i))
+		ms.Store(uint64(i), ei)
+		assertEqual(t, ms.tinyExtents[i], ei)
+		assertEqual(t, ms.tinyExtents[i], initExtentInfo(uint64(i)))
 	}
+	// before delete
+	assertEqual(t, uint64(0), ms.objDeletedCnt)
 	assertEqual(t, TinyExtentCount, ms.Len())
 	ms.RangeTinyExtent(func(extentID uint64, ei *ExtentInfoBlock) {
 		if extentID == 0 {
 			t.Errorf("extentID:%v should not exist.", extentID)
 		} else {
 			assertEqual(t, extentID, (*ei)[FileID])
+			assertEqual(t, extentID+1, (*ei)[Size])
+		}
+	})
+
+	var deleteExtentId = [5]uint64{1, 3, 4, 8, 60}
+	for _, deid := range deleteExtentId {
+		ms.Delete(deid)
+	}
+	assertEqual(t, uint64(0), ms.objDeletedCnt)
+	// after delete
+	assertEqual(t, TinyExtentCount, ms.Len())
+	ms.RangeTinyExtent(func(extentID uint64, ei *ExtentInfoBlock) {
+		if extentID == 0 {
+			t.Errorf("extentID:%v should not exist.", extentID)
+		} else {
+			assertEqual(t, extentID, (*ei)[FileID])
+			assertEqual(t, extentID+1, (*ei)[Size])
 		}
 	})
 }
@@ -60,16 +111,24 @@ func TestNormalExtent(t *testing.T) {
 	for i := TinyExtentCount + 1; i < normalCount+TinyExtentCount+1; i++ {
 		ms.Store(uint64(i), initExtentInfo(uint64(i)))
 	}
+	// before delete
 	assertEqual(t, normalCount, ms.Len())
-
+	assertEqual(t, uint64(0), ms.objDeletedCnt)
 	ms.RangeTinyExtent(func(extentID uint64, ei *ExtentInfoBlock) {
 		assertEqual(t, EmptyExtentBlock, *ei)
 	})
-	var deleteExtentId = [5]uint64{66, 68, 69, 100, 225}
+	ms.RangeNormalExtent(func(extentID uint64, ei *ExtentInfoBlock) {
+		assertEqual(t, extentID, (*ei)[FileID])
+		assertEqual(t, extentID+1, (*ei)[Size])
+	})
+
+	var deleteExtentId = []uint64{65, 66, 68, 69, 100, 225, 500, 1064}
 	for _, deid := range deleteExtentId {
 		ms.Delete(deid)
 	}
+	// after delete
 	assertEqual(t, normalCount-len(deleteExtentId), ms.Len())
+	assertEqual(t, uint64(len(deleteExtentId)), ms.objDeletedCnt)
 	ms.RangeNormalExtent(func(extentID uint64, ei *ExtentInfoBlock) {
 		for _, deid := range deleteExtentId {
 			if extentID == deid {
@@ -77,6 +136,7 @@ func TestNormalExtent(t *testing.T) {
 			}
 		}
 		assertEqual(t, extentID, (*ei)[FileID])
+		assertEqual(t, extentID+1, (*ei)[Size])
 	})
 }
 
@@ -85,8 +145,9 @@ func TestDelete(t *testing.T) {
 	var extentId uint64 = 999
 	ms.Store(extentId, initExtentInfo(extentId))
 	ms.Delete(extentId)
-	_, ok := ms.Load(extentId)
+	extent, ok := ms.Load(extentId)
 	assertEqual(t, false, ok)
+	assertEqual(t, nilExtentPtr, extent)
 }
 
 func TestReduce(t *testing.T) {
@@ -114,6 +175,8 @@ func TestReduce(t *testing.T) {
 	ms.Range(func(extentID uint64, ei *ExtentInfoBlock) {
 		eiLoad, _ := ms.Load(extentID)
 		assertEqual(t, extentID, (*eiLoad)[FileID])
+		assertEqual(t, extentID, (*ei)[FileID])
+		assertEqual(t, *eiLoad, *ei)
 	})
 	for i := maxSize - 1; i > minExtentID; i -= 5 {
 		_, ok := ms.Load(uint64(i))
@@ -126,6 +189,13 @@ func TestReduce(t *testing.T) {
 		}
 	}
 	assertEqual(t, sumCnt-deleteCnt, ms.Len())
+
+	ms.Range(func(extentID uint64, ei *ExtentInfoBlock) {
+		eiLoad, _ := ms.Load(extentID)
+		assertEqual(t, extentID, (*eiLoad)[FileID])
+		assertEqual(t, extentID, (*ei)[FileID])
+		assertEqual(t, *eiLoad, *ei)
+	})
 }
 
 func TestRandomReduce(t *testing.T) {
@@ -135,14 +205,21 @@ func TestRandomReduce(t *testing.T) {
 		ms.Store(extentId, initExtentInfo(extentId))
 	}
 	ms.Range(func(extentID uint64, ei *ExtentInfoBlock) {
+		eiLoad, _ := ms.Load(extentID)
 		assertEqual(t, extentID, ei[FileID])
+		assertEqual(t, extentID, eiLoad[FileID])
+		assertEqual(t, *eiLoad, *ei)
 	})
 	ms.Delete(89)
 	ms.Range(func(extentID uint64, ei *ExtentInfoBlock) {
+		eiLoad, _ := ms.Load(extentID)
 		assertEqual(t, extentID, ei[FileID])
+		assertEqual(t, extentID, eiLoad[FileID])
+		assertEqual(t, *eiLoad, *ei)
 	})
 
-	maxExtentId := 1000
+	// Reduce will be performed 5 times.
+	maxExtentId := reduceThresholdSize * 6 * 5
 	for i := 1; i <= maxExtentId; i++ {
 		ms.Store(uint64(i), initExtentInfo(uint64(i)))
 	}
@@ -156,9 +233,15 @@ func TestRandomReduce(t *testing.T) {
 			}
 		}
 	}
+	if deleteCnt < reduceThresholdSize {
+		t.Fatalf("delete count is less than %v", reduceThresholdSize)
+	}
 	assertEqual(t, maxExtentId - deleteCnt, ms.Len())
 	ms.Range(func(extentID uint64, ei *ExtentInfoBlock) {
+		eiLoad, _ := ms.Load(extentID)
 		assertEqual(t, extentID, ei[FileID])
+		assertEqual(t, extentID, eiLoad[FileID])
+		assertEqual(t, *eiLoad, *ei)
 	})
 }
 
@@ -169,51 +252,130 @@ func TestIter(t *testing.T) {
 		ms.Store(v, initExtentInfo(v))
 	}
 	count := 0
-	ms.Range(func(extentID uint64, obj *ExtentInfoBlock) {
-		assertEqual(t, extentID, obj[FileID])
+	ms.Range(func(extentID uint64, ei *ExtentInfoBlock) {
+		eiLoad, _ := ms.Load(extentID)
+		assertEqual(t, extentID, ei[FileID])
+		assertEqual(t, extentID, eiLoad[FileID])
+		assertEqual(t, *eiLoad, *ei)
 		count++
 	})
 	assertEqual(t, count, len(seq))
+	assertEqual(t, len(seq), ms.Len())
 	ms.Delete(654)
 	count = 0
-	ms.Range(func(extentID uint64, obj *ExtentInfoBlock) {
-		assertEqual(t, extentID, obj[FileID])
+	ms.Range(func(extentID uint64, ei *ExtentInfoBlock) {
+		eiLoad, _ := ms.Load(extentID)
+		assertEqual(t, extentID, ei[FileID])
+		assertEqual(t, extentID, eiLoad[FileID])
+		assertEqual(t, *eiLoad, *ei)
 		count++
 	})
-	assertEqual(t, count, len(seq)-1)
+	assertEqual(t, len(seq)-1, count)
+	assertEqual(t, len(seq)-1, ms.Len())
 
 	ms.Delete(1970)
 	count = 0
-	ms.Range(func(extentID uint64, obj *ExtentInfoBlock) {
-		assertEqual(t, extentID, obj[FileID])
+	ms.Range(func(extentID uint64, ei *ExtentInfoBlock) {
+		eiLoad, _ := ms.Load(extentID)
+		assertEqual(t, extentID, ei[FileID])
+		assertEqual(t, extentID, eiLoad[FileID])
+		assertEqual(t, *eiLoad, *ei)
 		count++
 	})
-	assertEqual(t, count, len(seq)-2)
-	assertEqual(t, ms.Len(), len(seq)-2)
+	assertEqual(t, len(seq)-2, count)
+	assertEqual(t, len(seq)-2, ms.Len())
 }
 
 func TestMapSlice_RangeDist(t *testing.T) {
 	ms := NewMapSlice(1)
-	for i := 1; i < 1000; i++ {
+	extentSize := 10000
+	for i := 1; i <= extentSize; i++ {
 		ms.Store(uint64(i), initExtentInfo(uint64(i)))
 	}
+	count := 0
 	ms.RangeDist(proto.AllExtentType, func(extentID uint64, ei *ExtentInfoBlock) {
-		if extentID == 0 || extentID >= 1000 {
+		if extentID == 0 || extentID > uint64(extentSize) {
 			t.Fail()
 		}
+		eiLoad, _ := ms.Load(extentID)
 		assertEqual(t, extentID, (*ei)[FileID])
+		assertEqual(t, extentID, (*eiLoad)[FileID])
+		assertEqual(t, *eiLoad, *ei)
+		count++
 	})
+	assertEqual(t, extentSize, count)
+	assertEqual(t, extentSize, ms.Len())
+	count = 0
 	ms.RangeDist(proto.TinyExtentType, func(extentID uint64, ei *ExtentInfoBlock) {
 		if extentID <= 0 || extentID >= TinyExtentCount+1 {
 			t.Fail()
 		}
+		eiLoad, _ := ms.Load(extentID)
 		assertEqual(t, extentID, (*ei)[FileID])
+		assertEqual(t, extentID, (*eiLoad)[FileID])
+		assertEqual(t, *eiLoad, *ei)
+		count++
 	})
+	assertEqual(t, TinyExtentCount, count)
+	assertEqual(t, TinyExtentCount, ms.Len() - (extentSize - TinyExtentCount))
+
+	count = 0
 	ms.RangeDist(proto.NormalExtentType, func(extentID uint64, ei *ExtentInfoBlock) {
 		if extentID < TinyExtentCount {
 			t.Fail()
 		}
+		eiLoad, _ := ms.Load(extentID)
 		assertEqual(t, extentID, (*ei)[FileID])
+		assertEqual(t, extentID, (*eiLoad)[FileID])
+		assertEqual(t, *eiLoad, *ei)
+		count++
+	})
+	assertEqual(t, extentSize - TinyExtentCount, count)
+	assertEqual(t, extentSize - TinyExtentCount, ms.Len() - TinyExtentCount)
+}
+
+func TestSpacedStore(t *testing.T) {
+	ms := NewMapSlice(1)
+	maxSize := 20000
+	count := 0
+	for i := 1; i <= maxSize; i++ {
+		if i%2 == 0 {
+			ms.Store(uint64(i), initExtentInfo(uint64(i)))
+			count++
+		}
+	}
+	assertEqual(t, count, ms.Len())
+	ms.Range(func(extentID uint64, ei *ExtentInfoBlock) {
+		if extentID <=0 || extentID > uint64(maxSize) {
+			t.Fail()
+		}
+		eiLoad, _ := ms.Load(extentID)
+		assertEqual(t, extentID, (*ei)[FileID])
+		assertEqual(t, extentID, (*eiLoad)[FileID])
+		assertEqual(t, *eiLoad, *ei)
+	})
+
+	deleteCnt := 0
+	for i := 1; i <=maxSize ; i++ {
+		if i%4 == 0 {
+			ms.Delete(uint64(i))
+			if i > TinyExtentCount {
+				deleteCnt++
+			}
+		}
+	}
+	if deleteCnt < reduceThresholdSize {
+		t.Fatalf("delete count is less than %v", reduceThresholdSize)
+	}
+	assertEqual(t, count - deleteCnt, ms.Len())
+	ms.Range(func(extentID uint64, ei *ExtentInfoBlock) {
+		if extentID <=0 || extentID > uint64(maxSize) {
+			t.Fail()
+		}
+		eiLoad, _ := ms.Load(extentID)
+		assertEqual(t, extentID, (*ei)[FileID])
+		assertEqual(t, extentID, (*eiLoad)[FileID])
+		assertEqual(t, *eiLoad, *ei)
 	})
 }
 
@@ -278,6 +440,23 @@ func Test_DeleteExtentData(t *testing.T) {
 	wg.Wait()
 }
 
+func Test_RangeAndReduceSametime(t *testing.T) {
+	InsertExtentData(t)
+	var wg sync.WaitGroup
+	concurrentDelete(t, &wg)
+	concurrentRange(t, &wg)
+	wg.Wait()
+	ems.Range(func(extentID uint64, ei *ExtentInfoBlock) {
+		if extentID <=0 || extentID >= MaxExtentID {
+			t.Fail()
+		}
+		eiLoad, _ := ems.Load(extentID)
+		assertEqual(t, extentID, (*ei)[FileID])
+		assertEqual(t, extentID, (*eiLoad)[FileID])
+		assertEqual(t, *eiLoad, *ei)
+	})
+}
+
 func InsertExtentData(t *testing.T) {
 	var wg sync.WaitGroup
 	ems=NewMapSlice(1)
@@ -287,6 +466,33 @@ func InsertExtentData(t *testing.T) {
 	}
 	wg.Wait()
 
+}
+
+func concurrentDelete(t *testing.T, wg *sync.WaitGroup) {
+	for i := 1; i <= 3; i++ {
+		wg.Add(1)
+		go func(i int) {
+			asyncDeleteExtentData(t, i, wg)
+		}(i)
+	}
+}
+
+func concurrentRange(t *testing.T, wg *sync.WaitGroup) {
+	for i := 0; i < 10; i++ {
+		wg.Add(1)
+		go func() {
+			defer wg.Done()
+			count := 0
+			ems.Range(func(extentID uint64, ei *ExtentInfoBlock) {
+				/*assertEqual(t, extentID, (*ei)[FileID])
+				assertEqual(t, extentID+1, (*ei)[Size])
+				assertEqual(t, extentID+2, (*ei)[Crc])
+				assertEqual(t, extentID+3, (*ei)[ModifyTime])*/
+				count++
+			})
+			t.Logf("Range count:%v, emsSumRecords:%v", count, ems.Len())
+		}()
+	}
 }
 
 func asyncInsertExtentData(t *testing.T,modNun int,wg *sync.WaitGroup) {
