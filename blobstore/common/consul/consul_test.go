99dc6bb80f4a824638d7ee1259eb92a9aab08117
JasonHu520 <hastyjsaon500@gmail.com>
2022-07-08 10:50:27 +0800

    build(proxy): add blobstore module proxy
    Signed-off-by: JasonHu520 <hastyjsaon500@gmail.com>

---
+++ blobstore/common/consul/consul_test.go
@@ -0,0 +1,71 @@
+// Copyright 2022 The CubeFS Authors.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or
+// implied. See the License for the specific language governing
+// permissions and limitations under the License.
+
+package consul
+
+import (
+	"fmt"
+	"net/http"
+	"testing"
+
+	"github.com/hashicorp/consul/api"
+	"github.com/stretchr/testify/assert"
+)
+
+func skipIfConsulNotRunning(t *testing.T, consulAddr string) {
+	defConfig := api.DefaultConfig()
+	defConfig.Address = consulAddr
+	client, err := api.NewClient(defConfig)
+	if err != nil {
+		t.Skip("consul not running")
+	}
+
+	if err = checkConsulAgentRunning(client); err != nil {
+		t.Skip(err)
+	}
+}
+
+func TestServiceRegister(t *testing.T) {
+	skipIfConsulNotRunning(t, "127.0.0.1:8500")
+	ast := assert.New(t)
+	{
+		_, err := ServiceRegister("", nil)
+		ast.Error(err)
+	}
+
+	{
+		r, err := ServiceRegister("0.0.0.0:10220", nil)
+		ast.NoError(err)
+		services, _, err := r.Health().Service(r.ServiceName, "", false, nil)
+		ast.NoError(err)
+		ast.NotNil(services)
+		ast.Equal(1, len(services))
+		service := services[0]
+		ast.Equal(r.ServiceName, service.Service.Service)
+		ast.Equal(r.getId(), service.Service.ID)
+		ast.Equal(r.ServiceIP, service.Service.Address)
+
+		r.Close()
+	}
+}
+
+func TestStartHealthCheckServ(t *testing.T) {
+	ast := assert.New(t)
+	_, port := StartHttpServerForHealthyCheck("", "/test")
+
+	resp, err := http.Get(fmt.Sprintf("http://127.0.0.1:%v/test", port))
+	ast.NoError(err)
+	resp.Body.Close()
+	ast.Equal(http.StatusOK, resp.StatusCode)
+}
---
+++ blobstore/proxy/mq/shard_repair_test.go
@@ -0,0 +1,99 @@
+// Copyright 2022 The CubeFS Authors.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or
+// implied. See the License for the specific language governing
+// permissions and limitations under the License.
+
+package mq
+
+import (
+	"context"
+	"errors"
+	"testing"
+
+	"github.com/stretchr/testify/require"
+
+	"github.com/cubefs/cubefs/blobstore/api/proxy"
+	"github.com/cubefs/cubefs/blobstore/common/kafka"
+)
+
+func TestShardRepairMgr_sendShardRepairMsg(t *testing.T) {
+	mockProducer := newProducer()
+
+	mgr := &ShardRepairMgr{
+		priorityTopic:        "priority",
+		topic:                "normal",
+		topicSelector:        defaultTopicSelector,
+		shardRepairMsgSender: mockProducer,
+	}
+	testCases := []struct {
+		args *proxy.ShardRepairArgs
+		err  error
+	}{
+		{
+			args: &proxy.ShardRepairArgs{
+				ClusterID: 0,
+				Bid:       1000,
+				Vid:       1,
+				BadIdxes:  []uint8{1},
+				Reason:    "",
+			},
+			err: nil,
+		},
+		{
+			args: &proxy.ShardRepairArgs{
+				ClusterID: 0,
+				Bid:       1000,
+				Vid:       100,
+				BadIdxes:  []uint8{1, 2, 3, 4},
+				Reason:    "",
+			},
+			err: ErrSendMessage,
+		},
+	}
+	for _, tc := range testCases {
+		err := mgr.SendShardRepairMsg(context.Background(), tc.args)
+		require.Equal(t, true, errors.Is(err, tc.err))
+	}
+}
+
+func TestNewShardRepairMgr(t *testing.T) {
+	_, err := NewShardRepairMgr(ShardRepairConfig{
+		Topic:        "",
+		MsgSenderCfg: kafka.ProducerCfg{},
+	})
+	require.Error(t, err)
+
+	seedBroker, leader := NewBrokers(t)
+
+	mgr, err := NewShardRepairMgr(ShardRepairConfig{
+		Topic:         "my_topic",
+		PriorityTopic: "my_topic",
+		MsgSenderCfg:  kafka.ProducerCfg{BrokerList: []string{seedBroker.Addr()}},
+	})
+	require.NoError(t, err)
+
+	info := &proxy.ShardRepairArgs{
+		ClusterID: 0,
+		Bid:       1000,
+		Vid:       1,
+		BadIdxes:  []uint8{1, 2},
+		Reason:    "",
+	}
+
+	for i := 0; i < 10; i++ {
+		err := mgr.SendShardRepairMsg(context.Background(), info)
+		require.NoError(t, err)
+	}
+
+	leader.Close()
+	seedBroker.Close()
+}
