6d28d585b6b34938b9cd975b6563b0cab78d1c8c
tianjiongzhang <236556116@qq.com>
2022-07-12 11:28:06 +0800

    fix:file parentPath dismatch.
    Signed-off-by: tianjiongzhang <236556116@qq.com>

--- client/fs/file.go
+++ client/fs/file.go
@@ -216,14 +216,16 @@ func (f *File) Open(ctx context.Context, req *fuse.OpenRequest, resp *fuse.OpenR
 	log.LogDebugf("TRACE open ino(%v) info(%v)", ino, f.info)
 	start := time.Now()
 
-	parentPath := f.getParentPath()
-	if parentPath != "" && !strings.HasSuffix(parentPath, "/") {
-		parentPath = parentPath + "/"
+	if f.super.bcacheDir != "" {
+		parentPath := f.getParentPath()
+		if parentPath != "" && !strings.HasSuffix(parentPath, "/") {
+			parentPath = parentPath + "/"
+		}
+		log.LogDebugf("TRACE open ino(%v) parentPath(%v)", ino, parentPath)
+		if strings.HasPrefix(parentPath, f.super.bcacheDir) {
+			needBCache = true
+		}
 	}
-	if f.super.bcacheDir != "" && strings.HasPrefix(parentPath, f.super.bcacheDir) {
-		needBCache = true
-	}
-	log.LogDebugf("TRACE open ino(%v) parentPath(%v) f.super.bcacheDir(%v) needBCache(%v)", ino, parentPath, f.super.bcacheDir, needBCache)
 	if needBCache {
 		f.super.ec.OpenStreamWithCache(ino, needBCache)
 	} else {
