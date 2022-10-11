3a8f9dd8fac2fe0a08f8a1e8cbcbdaa2c3ba47fc
Victor1319 <834863182@qq.com>
2022-10-11 11:05:09 +0800

    fix: remove redundant code
    Signed-off-by: Victor1319 <834863182@qq.com>

--- client/fuse.go
+++ client/fuse.go
@@ -23,7 +23,6 @@ package main
 import (
 	"flag"
 	"fmt"
-	"github.com/cubefs/cubefs/blockcache/bcache"
 	"io/ioutil"
 	syslog "log"
 	"net"
@@ -38,6 +37,8 @@ import (
 	"strings"
 	"syscall"
 	"time"
+
+	"github.com/cubefs/cubefs/blockcache/bcache"
 
 	"github.com/cubefs/cubefs/util/buf"
 
@@ -404,10 +405,6 @@ func main() {
 	defer fsConn.Close()
 
 	syslog.Printf("enable bcache %v", opt.EnableBcache)
-
-	if cfg.GetString(exporter.ConfigKeyPushAddr) == "" {
-		cfg.SetString(exporter.ConfigKeyPushAddr, "cfs-push.oppo.local")
-	}
 
 	exporter.Init(ModuleName, cfg)
 	exporter.RegistConsul(super.ClusterName(), ModuleName, cfg)
