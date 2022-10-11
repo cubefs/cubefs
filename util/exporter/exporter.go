3ec54232e986e2d6a10cdb33c54363294d6cc115
Victor1319 <834863182@qq.com>
2022-10-11 11:05:09 +0800

    fix: remove redundant code
    Signed-off-by: Victor1319 <834863182@qq.com>

--- util/exporter/exporter.go
+++ util/exporter/exporter.go
@@ -224,8 +224,6 @@ func autoPush(pushAddr, role, cluster, ip, mountPoint string) {
 		Grouping("cluster", cluster).
 		Grouping("pid", strconv.Itoa(pid)).
 		Grouping("commit", proto.CommitID).
-		Grouping("dataset", "custom").
-		Grouping("category", "custom").
 		Grouping("app", AppName).
 		Grouping("mountPoint", mountPoint).
 		Grouping("hostName", hostname)
