292f966388c1274451695cb4b329ae28aec13246
Patrick Wu <wuchunhuan@oppo.com>
2022-10-26 17:00:17 +0800

    fix: 1.add parameters for lcnode task routine num. 2.return registration err when max concurrent lcnode reached
    Signed-off-by: Patrick Wu <wuchunhuan@oppo.com>

--- lcnode/const.go
+++ lcnode/const.go
@@ -1,7 +1,9 @@
 package lcnode
 
 var (
-	defaultUChanInitCapacity int = 10000
-	defaultRoutingNumPerTask int = 100
-	defaultMaxChanUnm        int = 10000000 //worst case: may take up around 400MB of heap space
+	defaultUChanInitCapacity         int = 10000
+	defaultS3ScanRoutineNumPerTask   int = 100
+	defaultSnapShotRoutineNumPerTask int = 100
+	maxRoutineNumPerTask             int = 5000
+	defaultMaxChanUnm                int = 10000000 //worst case: may take up around 400MB of heap space
 )
