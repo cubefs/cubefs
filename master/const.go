1a777daa9309cb2f9e8869944f68ae1368ece9e8
lizhenzhen36 <lizhenzhen36@jd.com>
2022-12-21 14:19:12 +0800

    fix default children dentry max count(100W ==> 2000W)

--- master/const.go
+++ master/const.go
@@ -248,7 +248,7 @@ const (
 	defaultDataNodeHttpPort                            = "17320"
 	defaultSSDZoneTaskLimit                            = 20
 	defaultFollReadHostWeight                          = 0
-	defaultChildFileMaxCount                           = 1000000
+	defaultChildFileMaxCount                           = 2000 * 10000    //default 2kw
 )
 
 const (
