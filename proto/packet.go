944e4e15ade0e090ce35837f49c67011d7401fc7
Patrick Wu <wuchunhuan@oppo.com>
2023-01-09 15:17:51 +0800

    fix: remove temp inode when create dentry failed for quota exceeded
    Signed-off-by: Patrick Wu <wuchunhuan@oppo.com>

--- proto/packet.go
+++ proto/packet.go
@@ -451,6 +451,8 @@ func (p *Packet) GetResultMsg() (m string) {
 		m = "NotPerm"
 	case OpNotEmpty:
 		m = "DirNotEmpty"
+	case OpDirQuota:
+		m = "OpDirQuota"
 	default:
 		return fmt.Sprintf("Unknown ResultCode(%v)", p.ResultCode)
 	}
