829ba165632a94bf576e3168eee547522373aae9
true1064 <tangjingyu@oppo.com>
2023-03-27 18:49:04 +0800

    fix: the calculation of file count differ between replicas in 'cfs-cli datapartition check'
    Signed-off-by: true1064 <tangjingyu@oppo.com>

--- master/cluster.go
+++ master/cluster.go
@@ -992,7 +992,7 @@ func (c *Cluster) checkReplicaOfDataPartitions() (lackReplicaDPs []*DataPartitio
 
 				tempFileCountDiff := replica.FileCount - repFileCountSentry
 				if tempFileCountDiff > repFileCountDiff {
-					repFileCountSentry = tempFileCountDiff
+					repFileCountDiff = tempFileCountDiff
 				}
 			}
 
