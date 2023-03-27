49f6478c0be20d60f165e300f1c6c58f306dc3b6
bjguoweilong <guoweilong@jd.com>
2023-03-27 11:24:01 +0800

    change to CubeFS  Author
    Signed-off-by: bjguoweilong <guoweilong@jd.com>

--- util/iputil/addrselect.go
+++ util/iputil/addrselect.go
@@ -1,6 +1,6 @@
 // Copyright 2015 The Go Authors. All rights reserved.
 //
-// Modified by 2020 The Chubao Authors.
+// Modified by 2020 The CubeFS Authors.
 //
 // Use of this source code is governed by a BSD-style
 // license that can be found in the LICENSE file.
