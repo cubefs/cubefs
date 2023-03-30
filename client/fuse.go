4ccd46d001e122a00dbdadc6e9b7049cb2f41dc4
leonrayang <chl696@sina.com>
2023-03-30 19:51:33 +0800

    Fix: cold volume create replicaNum check failed
    Signed-off-by: leonrayang <chl696@sina.com>

--- client/fuse.go
+++ client/fuse.go
@@ -40,10 +40,9 @@ import (
 	"syscall"
 	"time"
 
-	"github.com/cubefs/cubefs/util/buf"
-
 	"github.com/cubefs/cubefs/sdk/master"
 	"github.com/cubefs/cubefs/util"
+	"github.com/cubefs/cubefs/util/buf"
 
 	sysutil "github.com/cubefs/cubefs/util/sys"
 
