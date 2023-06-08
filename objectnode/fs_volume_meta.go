ab2ad55ce57c79d6c9542011c310671121041b1e
tangdeyi <deyitang94@gmail.com>
2023-06-08 19:30:02 +0800

    fix(objectnode): bucket metadata cache synchronization
    Signed-off-by: tangdeyi <deyitang94@gmail.com>

--- objectnode/fs_volume_meta.go
+++ objectnode/fs_volume_meta.go
@@ -14,15 +14,21 @@
 
 package objectnode
 
-import "sync"
+import (
+	"sync"
+	"sync/atomic"
+
+	"golang.org/x/sync/singleflight"
+)
 
 type ossMetaLoader interface {
 	loadPolicy() (p *Policy, err error)
 	loadACL() (p *AccessControlPolicy, err error)
-	loadCors() (cors *CORSConfiguration, err error)
+	loadCORS() (cors *CORSConfiguration, err error)
 	storePolicy(p *Policy)
 	storeACL(p *AccessControlPolicy)
-	storeCors(cors *CORSConfiguration)
+	storeCORS(cors *CORSConfiguration)
+	setSynced()
 }
 
 type strictMetaLoader struct {
@@ -30,7 +36,10 @@ type strictMetaLoader struct {
 }
 
 type cacheMetaLoader struct {
-	om *OSSMeta
+	om     *OSSMeta
+	sml    *strictMetaLoader
+	sf     singleflight.Group
+	synced *int32
 }
 
 // OSSMeta is bucket policy and ACL metadata.
@@ -47,6 +56,17 @@ func (c *cacheMetaLoader) loadPolicy() (p *Policy, err error) {
 	c.om.policyLock.RLock()
 	p = c.om.policy
 	c.om.policyLock.RUnlock()
+	if p == nil && atomic.LoadInt32(c.synced) == 0 {
+		ret, err, _ := c.sf.Do(XAttrKeyOSSPolicy, func() (interface{}, error) {
+			p, err := c.sml.loadPolicy()
+			return p, err
+		})
+		if err != nil {
+			return nil, err
+		}
+		p = ret.(*Policy)
+		c.storePolicy(p)
+	}
 	return
 }
 
@@ -61,6 +81,17 @@ func (c *cacheMetaLoader) loadACL() (p *AccessControlPolicy, err error) {
 	c.om.aclLock.RLock()
 	p = c.om.acl
 	c.om.aclLock.RUnlock()
+	if p == nil && atomic.LoadInt32(c.synced) == 0 {
+		ret, err, _ := c.sf.Do(XAttrKeyOSSACL, func() (interface{}, error) {
+			a, err := c.sml.loadACL()
+			return a, err
+		})
+		if err != nil {
+			return nil, err
+		}
+		p = ret.(*AccessControlPolicy)
+		c.storeACL(p)
+	}
 	return
 }
 
@@ -71,18 +102,33 @@ func (c *cacheMetaLoader) storeACL(p *AccessControlPolicy) {
 	return
 }
 
-func (c *cacheMetaLoader) loadCors() (cors *CORSConfiguration, err error) {
+func (c *cacheMetaLoader) loadCORS() (cors *CORSConfiguration, err error) {
 	c.om.corsLock.RLock()
 	cors = c.om.corsConfig
 	c.om.corsLock.RUnlock()
+	if cors == nil && atomic.LoadInt32(c.synced) == 0 {
+		ret, err, _ := c.sf.Do(XAttrKeyOSSCORS, func() (interface{}, error) {
+			c, err := c.sml.loadCORS()
+			return c, err
+		})
+		if err != nil {
+			return nil, err
+		}
+		cors = ret.(*CORSConfiguration)
+		c.storeCORS(cors)
+	}
 	return
 }
 
-func (c *cacheMetaLoader) storeCors(cors *CORSConfiguration) {
+func (c *cacheMetaLoader) storeCORS(cors *CORSConfiguration) {
 	c.om.corsLock.Lock()
 	c.om.corsConfig = cors
 	c.om.corsLock.Unlock()
 	return
+}
+
+func (c *cacheMetaLoader) setSynced() {
+	atomic.StoreInt32(c.synced, 1)
 }
 
 func (s *strictMetaLoader) loadPolicy() (p *Policy, err error) {
@@ -97,8 +143,10 @@ func (s *strictMetaLoader) loadACL() (acp *AccessControlPolicy, err error) {
 
 func (s *strictMetaLoader) storeACL(p *AccessControlPolicy) {}
 
-func (s *strictMetaLoader) loadCors() (cors *CORSConfiguration, err error) {
+func (s *strictMetaLoader) loadCORS() (cors *CORSConfiguration, err error) {
 	return s.v.loadBucketCors()
 }
 
-func (s *strictMetaLoader) storeCors(cors *CORSConfiguration) {}
+func (s *strictMetaLoader) storeCORS(cors *CORSConfiguration) {}
+
+func (s *strictMetaLoader) setSynced() {}
