ad065dc8088dc203da1c718cf04b46f343aea7bd
NaturalSelect <2145973003@qq.com>
2023-06-12 15:10:13 +0800

    feat(datanode): datanode report cpu util and disk util in heartbeat response
    Signed-off-by: NaturalSelect <2145973003@qq.com>

--- datanode/server.go
+++ datanode/server.go
@@ -38,8 +38,10 @@ import (
 	"github.com/cubefs/cubefs/repl"
 	masterSDK "github.com/cubefs/cubefs/sdk/master"
 	"github.com/cubefs/cubefs/util"
+	"github.com/cubefs/cubefs/util/atomicutil"
 	"github.com/cubefs/cubefs/util/config"
 	"github.com/cubefs/cubefs/util/exporter"
+	"github.com/cubefs/cubefs/util/loadutil"
 	"github.com/cubefs/cubefs/util/log"
 
 	"github.com/xtaci/smux"
@@ -110,6 +112,8 @@ const (
 	ConfigServiceIDKey = "serviceIDKey"
 )
 
+const cpuSampleDuration = 1 * time.Second
+
 // DataNode defines the structure of a data node.
 type DataNode struct {
 	space           *SpaceManager
@@ -156,6 +160,8 @@ type DataNode struct {
 	clusterUuid             string
 	clusterUuidEnable       bool
 	serviceIDKey            string
+	cpuUtil        atomicutil.Float64
+	cpuSamplerDone chan struct{}
 }
 
 func NewServer() *DataNode {
@@ -239,6 +245,8 @@ func doStart(server common.Server, cfg *config.Config) (err error) {
 	// start metrics (LackDpCount, etc.)
 	s.startMetrics()
 
+	// start cpu sampler
+	s.startCpuSample()
 	return
 }
 
@@ -256,6 +264,8 @@ func doShutdown(server common.Server) {
 	s.stopSmuxService()
 	s.closeSmuxConnPool()
 	MasterClient.Stop()
+	// stop cpu sample
+	close(s.cpuSamplerDone)
 }
 
 func (s *DataNode) parseConfig(cfg *config.Config) (err error) {
@@ -402,6 +412,8 @@ func (s *DataNode) startSpaceManager(cfg *config.Config) (err error) {
 	}
 
 	wg.Wait()
+	// start async sample
+	s.space.StartDiskSample()
 	return nil
 }
 
@@ -833,6 +845,22 @@ func (s *DataNode) shallDegrade() bool {
 func (s *DataNode) scheduleTask() {
 	go s.startUpdateNodeInfo()
 	s.scheduleToCheckLackPartitions()
+}
+
+func (s *DataNode) startCpuSample() {
+	s.cpuSamplerDone = make(chan struct{})
+	go func() {
+		for {
+			select {
+			case <-s.cpuSamplerDone:
+				return
+			default:
+				// this function will sleep cpuSampleDuration
+				used := loadutil.GetCpuUtilPercent(cpuSampleDuration)
+				s.cpuUtil.Store(used)
+			}
+		}
+	}()
 }
 
 func (s *DataNode) scheduleToCheckLackPartitions() {
