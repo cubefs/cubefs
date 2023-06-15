d4adf5e25b122497ce554a9a5a474b040a4b2bb4
leonrayang <chl696@sina.com>
2023-06-15 20:25:26 +0800

    fix: need lock while update data of followerReadManager
    Signed-off-by: leonrayang <chl696@sina.com>

--- master/cluster.go
+++ master/cluster.go
@@ -141,8 +141,10 @@ func (mgr *followerReadManager) getVolumeDpView() {
 
 	for _, vv := range volViews {
 		if vv.Status == markDelete {
+			mgr.rwMutex.Lock()
 			mgr.lastUpdateTick[vv.Name] = time.Now()
 			mgr.status[vv.Name] = false
+			mgr.rwMutex.Unlock()
 			continue
 		}
 
