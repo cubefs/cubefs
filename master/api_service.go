23f7630a98a3edfe326efb8faec7d79a1ea00804
leonrayang <chl696@sina.com>
2023-08-07 00:54:12 +0800

    enhance(master):qos.Enhance the initialization of the client and take into account the requests, otherwise the client's requirements cannot be met.
    close:#2437
    
    Signed-off-by: leonrayang <chl696@sina.com>

--- master/api_service.go
+++ master/api_service.go
@@ -2111,16 +2111,18 @@ func (m *Server) qosUpload(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 	// qos upload may called by client init,thus use qosEnable param to identify it weather need to calc by master
-
+	var clientInfo *proto.ClientReportLimitInfo
 	if qosEnable, _ := strconv.ParseBool(qosEnableStr); qosEnable {
-		if clientInfo, err := parseQosInfo(r); err == nil {
+		if clientInfo, err = parseQosInfo(r); err == nil {
 			log.LogDebugf("action[qosUpload] cliInfoMgrMap [%v],clientInfo id[%v] clientInfo.Host %v, enable %v", clientInfo.ID, clientInfo.Host, r.RemoteAddr, qosEnable)
 			if clientInfo.ID == 0 {
 				if limit, err = vol.qosManager.init(m.cluster, clientInfo.Host); err != nil {
 					sendErrReply(w, r, newErrHTTPReply(err))
 					return
 				}
-			} else if limit, err = vol.qosManager.HandleClientQosReq(clientInfo, clientInfo.ID); err != nil {
+				clientInfo.ID = limit.ID
+			}
+			if limit, err = vol.qosManager.HandleClientQosReq(clientInfo, clientInfo.ID); err != nil {
 				sendErrReply(w, r, newErrHTTPReply(err))
 				return
 			}
