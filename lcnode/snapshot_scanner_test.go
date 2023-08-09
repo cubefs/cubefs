20ad1d2abd6382656d816ae89dad0aaf86dcca88
zhaochenyang <zhaochenyang@oppo.com>
2023-08-09 18:49:27 +0800

    refactor(lcnode): refactor some code in lcnode
    1)refactor some code about snapshot
    2)fix some bugs
    3)add some ut
    
    Signed-off-by: zhaochenyang <zhaochenyang@oppo.com>

---
+++ lcnode/snapshot_scanner_test.go
@@ -0,0 +1,54 @@
+// Copyright 2023 The CubeFS Authors.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or
+// implied. See the License for the specific language governing
+// permissions and limitations under the License.
+
+package lcnode
+
+import (
+	"testing"
+	"time"
+
+	"github.com/cubefs/cubefs/proto"
+	"github.com/cubefs/cubefs/util/routinepool"
+	"github.com/cubefs/cubefs/util/unboundedchan"
+	"github.com/stretchr/testify/require"
+)
+
+func TestSnapshotScanner(t *testing.T) {
+	snapshotRoutineNumPerTask = 1
+	scanCheckInterval = 1
+	scanner := &SnapshotScanner{
+		ID:     "test_id",
+		Volume: "test_vol",
+		mw:     NewMockMetaWrapper(),
+		lcnode: &LcNode{},
+		adminTask: &proto.AdminTask{
+			Response: &proto.SnapshotVerDelTaskResponse{},
+		},
+		verDelReq: &proto.SnapshotVerDelTaskRequest{
+			Task: &proto.SnapshotVerDelTask{
+				VerInfo: proto.VerInfo{
+					VolName: "test_vol",
+					VerSeq:  1,
+				},
+			},
+		},
+		inodeChan:   unboundedchan.NewUnboundedChan(10),
+		rPoll:       routinepool.NewRoutinePool(snapshotRoutineNumPerTask),
+		currentStat: &proto.SnapshotStatistics{},
+		stopC:       make(chan bool, 0),
+	}
+	go scanner.Start()
+	time.Sleep(time.Second * 5)
+	require.Equal(t, true, scanner.DoneScanning())
+}
