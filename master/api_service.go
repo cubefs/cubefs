dac218a2075c2bbb66f4ce2c618ece83079c5906
true1064 <tangjingyu@oppo.com>
2023-08-09 17:48:18 +0800

    feat(master/data/cli): [hybrid cloud] add datanode's mediaType for module master and datanode; the cmd 'cfs-cli datanode info' can show datanode's mediaType
    Signed-off-by: true1064 <tangjingyu@oppo.com>

--- master/api_service.go
+++ master/api_service.go
@@ -2529,6 +2529,7 @@ func (m *Server) addDataNode(w http.ResponseWriter, r *http.Request) {
 	var (
 		nodeAddr  string
 		zoneName  string
+		mediaType uint32
 		id        uint64
 		err       error
 		nodesetId uint64
@@ -2538,7 +2539,7 @@ func (m *Server) addDataNode(w http.ResponseWriter, r *http.Request) {
 		doStatAndMetric(proto.AddDataNode, metric, err, nil)
 	}()
 
-	if nodeAddr, zoneName, err = parseRequestForAddNode(r); err != nil {
+	if nodeAddr, zoneName, mediaType, err = parseRequestForAddNode(r); err != nil {
 		sendErrReply(w, r, &proto.HTTPReply{Code: proto.ErrCodeParamError, Msg: err.Error()})
 		return
 	}
@@ -2555,7 +2556,7 @@ func (m *Server) addDataNode(w http.ResponseWriter, r *http.Request) {
 			return
 		}
 	}
-	if id, err = m.cluster.addDataNode(nodeAddr, zoneName, nodesetId); err != nil {
+	if id, err = m.cluster.addDataNode(nodeAddr, zoneName, nodesetId, mediaType); err != nil {
 		sendErrReply(w, r, newErrHTTPReply(err))
 		return
 	}
@@ -2608,6 +2609,7 @@ func (m *Server) getDataNode(w http.ResponseWriter, r *http.Request) {
 		MaxDpCntLimit:             dataNode.GetDpCntLimit(),
 		CpuUtil:                   dataNode.CpuUtil.Load(),
 		IoUtils:                   dataNode.GetIoUtils(),
+		MediaType:                 dataNode.MediaType,
 	}
 
 	sendOkReply(w, r, newSuccessHTTPReply(dataNodeInfo))
@@ -4037,7 +4039,7 @@ func (m *Server) addMetaNode(w http.ResponseWriter, r *http.Request) {
 		doStatAndMetric(proto.AddMetaNode, metric, err, nil)
 	}()
 
-	if nodeAddr, zoneName, err = parseRequestForAddNode(r); err != nil {
+	if nodeAddr, zoneName, _, err = parseRequestForAddNode(r); err != nil {
 		sendErrReply(w, r, &proto.HTTPReply{Code: proto.ErrCodeParamError, Msg: err.Error()})
 		return
 	}
