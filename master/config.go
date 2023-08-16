b6272eb96d0bc84c20b4ddda6d1d481464856137
tangdeyi <tangdeyi@oppo.com>
2023-08-16 15:03:23 +0800

    perf(master): Optimize MP split algorithm
    Signed-off-by: tangdeyi <tangdeyi@oppo.com>

--- master/config.go
+++ master/config.go
@@ -87,6 +87,8 @@ const (
 	defaultIntervalToScanS3Expiration                  = 12 * 3600
 	defaultMaxConcurrentLcNodes                        = 3
 	defaultIntervalToCheckDelVerTaskExpiration         = 3
+	metaPartitionInodeUsageThreshold           float64 = 0.75 // inode usage threshold on a meta partition
+	lowerLimitRWMetaPartition                          = 2    // lower limit of RW meta partition
 )
 
 // AddrDatabase is a map that stores the address of a given host (e.g., the leader)
