a679018192238d38d5b85ddf3170ba1d05c3e8c7
slasher <shenjie1@oppo.com>
2023-09-18 11:45:01 +0800

    build(storage): format code with go generate
    Signed-off-by: slasher <shenjie1@oppo.com>

--- docker/script/run_format.sh
+++ docker/script/run_format.sh
@@ -3,8 +3,16 @@ CurrentPath=$(cd $(dirname ${BASH_SOURCE[0]}); pwd)
 pushd ${CurrentPath}/../../
 find . -type f -name "*.go" | grep -v 'vendor' |grep -v 'depends'| xargs gofmt -l > gofmt_results.txt
 cat gofmt_results.txt
-if [ "$(cat gofmt_results.txt|wc -l)" -gt 0  ]; then 
+if [ "$(cat gofmt_results.txt|wc -l)" -gt 0  ]; then
     popd
-    exit 1; 
+    exit 1;
+fi
+popd
+
+pushd ${CurrentPath}/../../storage
+export PATH=$PATH:/go/bin
+go generate ./...
+if [[ $? -ne 0 ]]; then
+    exit 1
 fi
 popd
