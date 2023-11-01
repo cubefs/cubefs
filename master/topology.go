6508a5ddee04282681926456ff420b66353cf175
true1064 <tangjingyu@oppo.com>
2023-11-02 05:25:48 +0800

    feat(master): hybrid cloud add mediaType property to zone
    	(1) zone mediaType is set as the first add-in datanode's mediaType
    	(2) once zone mediaType is set, all datanodes in the zone must be the same mediaType.
    	(3) the cmd 'cfs-cli zone info' shows zone mediaType.
    
    Signed-off-by: true1064 <tangjingyu@oppo.com>

--- master/topology.go
+++ master/topology.go
@@ -1390,6 +1390,7 @@ type Zone struct {
 	QosIopsWLimit           uint64
 	QosFlowRLimit           uint64
 	QosFlowWLimit           uint64
+	mediaType               uint32
 	sync.RWMutex
 }
 
@@ -1401,9 +1402,10 @@ type zoneValue struct {
 	QosFlowWLimit       uint64
 	DataNodesetSelector string
 	MetaNodesetSelector string
+	MediaType           uint32
 }
 
-func newZone(name string) (zone *Zone) {
+func newZone(name string, mediaType uint32) (zone *Zone) {
 	zone = &Zone{name: name}
 	zone.setStatus(normalZone)
 	zone.dataNodes = new(sync.Map)
@@ -1411,6 +1413,7 @@ func newZone(name string) (zone *Zone) {
 	zone.nodeSetMap = make(map[uint64]*nodeSet)
 	zone.dataNodesetSelector = NewNodesetSelector(DefaultNodesetSelectorName, DataNodeType)
 	zone.metaNodesetSelector = NewNodesetSelector(DefaultNodesetSelectorName, MetaNodeType)
+	zone.SetMediaType(mediaType)
 	return
 }
 
@@ -1460,6 +1463,7 @@ func (zone *Zone) getFsmValue() *zoneValue {
 		QosFlowWLimit:       zone.QosFlowWLimit,
 		DataNodesetSelector: zone.GetDataNodesetSelector(),
 		MetaNodesetSelector: zone.GetMetaNodesetSelector(),
+		MediaType:           zone.GetMediaType(),
 	}
 }
 
@@ -1484,7 +1488,7 @@ func (zone *Zone) getNodeSet(setID uint64) (ns *nodeSet, err error) {
 	defer zone.nsLock.RUnlock()
 	ns, ok := zone.nodeSetMap[setID]
 	if !ok {
-		return nil, errors.NewErrorf("set %v not found", setID)
+		return nil, errors.NewErrorf("nodeset %v not found", setID)
 	}
 	return
 }
@@ -1942,6 +1946,18 @@ func (zone *Zone) startDecommissionListTraverse(c *Cluster) (err error) {
 	}
 	log.LogInfof("action[startDecommissionListTraverse] All nodeset from %v start decommission schedule", zone.name)
 	return
+}
+
+func (zone *Zone) GetMediaType() uint32 {
+	return atomic.LoadUint32(&zone.mediaType)
+}
+
+func (zone *Zone) GetMediaTypeString() string {
+	return proto.MediaTypeString(atomic.LoadUint32(&zone.mediaType))
+}
+
+func (zone *Zone) SetMediaType(newMediaType uint32) {
+	atomic.StoreUint32(&zone.mediaType, newMediaType)
 }
 
 type DecommissionDataPartitionList struct {
