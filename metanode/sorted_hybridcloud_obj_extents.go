284ca0d2115b493c6ee750f5384b247fd31744b7
chihe <chi.he@oppo.com>
2023-11-12 20:41:52 -0800

    feat(metanodeclient): hybrid cloud support hybridcloud data migration
    Signed-off-by: chihe <chi.he@oppo.com>

--- metanode/sorted_hybridcloud_obj_extents.go
+++ metanode/sorted_hybridcloud_obj_extents.go
@@ -1,9 +1,20 @@
 package metanode
 
+import "github.com/cubefs/cubefs/proto"
+
 type SortedHybridCloudExtents struct {
 	sortedEks interface{}
 }
 
 func NewSortedHybridCloudExtents() *SortedHybridCloudExtents {
 	return &SortedHybridCloudExtents{}
+}
+
+type SortedHybridCloudExtentsMigration struct {
+	sortedEks    interface{}
+	storageClass uint32
+}
+
+func NewSortedHybridCloudExtentsMigration() *SortedHybridCloudExtentsMigration {
+	return &SortedHybridCloudExtentsMigration{storageClass: proto.MediaType_Unspecified}
 }
