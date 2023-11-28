b5c20be9bb395ec891d64cf0fe1a80b687d511ab
true1064 <tangjingyu@oppo.com>
2023-11-28 11:32:36 +0800

    feat(master, cli): [hybrid cloud] vol.volStorageClass can be changed by "cfs-cli volume update"
    Signed-off-by: true1064 <tangjingyu@oppo.com>

--- master/vol.go
+++ master/vol.go
@@ -49,6 +49,7 @@ type VolVarargs struct {
 	txConflictRetryNum      int64
 	txConflictRetryInterval int64
 	txOpLimit               int
+	volStorageClass         uint32
 }
 
 // Vol represents a set of meta partitionMap and data partitionMap
@@ -1541,6 +1542,7 @@ func setVolFromArgs(args *VolVarargs, vol *Vol) {
 
 	vol.dpSelectorName = args.dpSelectorName
 	vol.dpSelectorParm = args.dpSelectorParm
+	vol.volStorageClass = args.volStorageClass
 }
 
 func getVolVarargs(vol *Vol) *VolVarargs {
@@ -1576,6 +1578,7 @@ func getVolVarargs(vol *Vol) *VolVarargs {
 		txOpLimit:               vol.txOpLimit,
 		coldArgs:                args,
 		dpReadOnlyWhenVolFull:   vol.DpReadOnlyWhenVolFull,
+		volStorageClass:         vol.volStorageClass,
 	}
 }
 
