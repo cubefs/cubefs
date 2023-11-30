11a55f5b795ae3141983ce3f6c8a9724f3214cda
slasher <shenjie1@oppo.com>
2023-11-30 16:02:09 +0800

    feat(build): build with goreleaser
    Signed-off-by: slasher <shenjie1@oppo.com>

--- build/build.sh
+++ build/build.sh
@@ -10,7 +10,7 @@ VendorPath=${RootPath}/vendor
 DependsPath=${RootPath}/depends
 use_clang=$(echo ${CC} | grep "clang" | grep -v "grep")
 cgo_ldflags="-L${BuildDependsLibPath} -lrocksdb -lz -lbz2 -lsnappy -llz4 -lzstd -lstdc++"
-if [ "${use_clang}" != "" ]; then 
+if [ "${use_clang}" != "" ]; then
     cgo_ldflags="-L${BuildDependsLibPath} -lrocksdb -lz -lbz2 -lsnappy -llz4 -lzstd -lc++"
 fi
 cgo_cflags="-I${BuildDependsIncludePath}"
@@ -356,6 +356,21 @@ build_snapshot() {
     popd >/dev/null
 }
 
+build_libsdkpre() {
+    case `uname` in
+        Linux)
+            TargetFile=${1:-${BuildBinPath}/libcfs.so}
+            ;;
+        *)
+            echo "Unsupported platform"
+            exit 1
+            ;;
+    esac
+    pushd $SrcPath > /dev/null
+    echo -n "build libsdk: libcfs.so"
+    CGO_ENABLED=1 go build $MODFLAGS -gcflags=all=-trimpath=${SrcPath} -asmflags=all=-trimpath=${SrcPath} -ldflags="${LDFlags}" -buildmode c-shared -o ${TargetFile} ${SrcPath}/libsdk/*.go && echo "success" || echo "failed"
+    popd > /dev/null
+}
 
 build_libsdk() {
     case `uname` in
@@ -487,6 +502,9 @@ case "$cmd" in
         ;;
     "snapshot")
         build_snapshot
+        ;;
+    "libsdkpre")
+        build_libsdkpre
         ;;
     "libsdk")
         build_libsdk
