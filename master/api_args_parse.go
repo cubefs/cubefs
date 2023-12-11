e8149e33640dd8239620b729bfe7b64047da096d
true1064 <tangjingyu@oppo.com>
2023-12-11 20:13:53 +0800

    feat(master): [hybrid cloud] sort AllowedStorageClass[] while creating volume
    Signed-off-by: true1064 <tangjingyu@oppo.com>

--- master/api_args_parse.go
+++ master/api_args_parse.go
@@ -22,6 +22,7 @@ import (
 	"io"
 	"math"
 	"net/http"
+	"sort"
 	"strconv"
 	"strings"
 	"time"
@@ -812,6 +813,11 @@ func parseAllowedStorageClass(r *http.Request) (allowedStorageClass []uint32, er
 			log.LogDebugf("[parseAllowedStorageClass] pick allowedStorageClass(%v)", proto.StorageClassString(ascUint32))
 		}
 	}
+
+	sort.Slice(allowedStorageClass, func(i, j int) bool {
+		return allowedStorageClass[i] < allowedStorageClass[j]
+	})
+
 	return
 }
 
