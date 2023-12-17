f6691a7b747fcbebb8cc6516bb5e650dd4e796c9
leonrayang <chl696@sina.com>
2023-12-17 21:41:28 +0800

    enhance(datanode):Empty extents are also repaired, and the data partition check considers the count of empty extents as a factor
    Signed-off-by: leonrayang <chl696@sina.com>

--- storage/extent_store.go
+++ storage/extent_store.go
@@ -89,7 +89,7 @@ var (
 	NormalExtentFilter = func() ExtentFilter {
 		now := time.Now()
 		return func(ei *ExtentInfo) bool {
-			return !IsTinyExtent(ei.FileID) && now.Unix()-ei.ModifyTime > RepairInterval && !ei.IsDeleted && ei.TotalSize() > 0
+			return !IsTinyExtent(ei.FileID) && now.Unix()-ei.ModifyTime > RepairInterval && !ei.IsDeleted
 		}
 	}
 
