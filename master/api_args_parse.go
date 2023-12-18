07d763808a90779a1f6b8f6bca79ec46ffb58ddf
true1064 <tangjingyu@oppo.com>
2023-12-18 14:25:37 +0800

    fix(master): [hybrid cloud] In create volume procedure, sort allowedStorageClass after check if append volStorageClass info it.
    Signed-off-by: true1064 <tangjingyu@oppo.com>

--- master/api_args_parse.go
+++ master/api_args_parse.go
@@ -22,7 +22,6 @@ import (
 	"io"
 	"math"
 	"net/http"
-	"sort"
 	"strconv"
 	"strings"
 	"time"
@@ -844,10 +843,6 @@ func parseAllowedStorageClass(r *http.Request) (allowedStorageClass []uint32, er
 			log.LogDebugf("[parseAllowedStorageClass] pick allowedStorageClass(%v)", proto.StorageClassString(ascUint32))
 		}
 	}
-
-	sort.Slice(allowedStorageClass, func(i, j int) bool {
-		return allowedStorageClass[i] < allowedStorageClass[j]
-	})
 
 	return
 }
