443199e0b08ad41a5cefa8f19bec82f707b63b11
chihe <chi.he@oppo.com>
2023-12-18 22:31:46 -0800

    fix(client): hybrid cloud only clear dp when volume storage class is blobstore
    Signed-off-by: chihe <chi.he@oppo.com>

--- sdk/data/wrapper/wrapper.go
+++ sdk/data/wrapper/wrapper.go
@@ -78,19 +78,18 @@ type Wrapper struct {
 	preload     bool
 	LocalIp     string
 
-	minWritableDataPartitionCnt int
-	verConfReadSeq              uint64
-	verReadSeq                  uint64
-	SimpleClient                SimpleClientInfo
-
+	verConfReadSeq    uint64
+	verReadSeq        uint64
+	SimpleClient      SimpleClientInfo
 	IsSnapshotEnabled bool
 
-	allowedStorageClass []uint32
+	volStorageClass        uint32
+	volAllowedStorageClass []uint32
 }
 
 // NewDataPartitionWrapper returns a new data partition wrapper.
 func NewDataPartitionWrapper(client SimpleClientInfo, volName string, masters []string, preload bool,
-	minWriteAbleDataPartitionCnt int, verReadSeq uint64, allowedStorageClass []uint32) (w *Wrapper, err error) {
+	minWriteAbleDataPartitionCnt int, verReadSeq uint64, volStorageClass uint32) (w *Wrapper, err error) {
 	log.LogInfof("action[NewDataPartitionWrapper] verReadSeq %v", verReadSeq)
 
 	w = new(Wrapper)
@@ -101,12 +100,7 @@ func NewDataPartitionWrapper(client SimpleClientInfo, volName string, masters []
 	w.partitions = make(map[uint64]*DataPartition)
 	w.HostsStatus = make(map[string]bool)
 	w.preload = preload
-
-	w.minWritableDataPartitionCnt = minWritableDataPartitionCnt
-	if w.minWritableDataPartitionCnt < 0 {
-		w.minWritableDataPartitionCnt = DefaultMinWritableDataPartitionCnt
-	}
-	w.allowedStorageClass = allowedStorageClass
+	w.volStorageClass = volStorageClass
 
 	if w.LocalIp, err = ump.GetLocalIpAddr(); err != nil {
 		err = errors.Trace(err, "NewDataPartitionWrapper:")
@@ -373,7 +367,7 @@ func (w *Wrapper) updateDataPartitionByRsp(forceUpdate bool, refreshPolicy Refre
 			ClientWrapper:         w,
 		}
 	}
-	if proto.IsCold(w.volType) || proto.VolSupportsBlobStore(w.allowedStorageClass) {
+	if proto.IsStorageClassBlobStore(w.volStorageClass) {
 		w.clearPartitions()
 	}
 	rwPartitionGroups := make([]*DataPartition, 0)
@@ -390,7 +384,7 @@ func (w *Wrapper) updateDataPartitionByRsp(forceUpdate bool, refreshPolicy Refre
 		w.replaceOrInsertPartition(dp)
 
 		// do not insert preload dp in cold vol
-		if (proto.IsCold(w.volType) || proto.VolSupportsBlobStore(w.allowedStorageClass)) && proto.IsPreLoadDp(dp.PartitionType) {
+		if (proto.IsCold(w.volType) || proto.VolSupportsBlobStore(w.volAllowedStorageClass)) && proto.IsPreLoadDp(dp.PartitionType) {
 			continue
 		}
 		if dp.Status == proto.ReadWrite {
@@ -412,7 +406,7 @@ func (w *Wrapper) updateDataPartitionByRsp(forceUpdate bool, refreshPolicy Refre
 			w.volName, len(rwPartitionGroups), len(DataPartitions), forceUpdate)
 	}
 
-	log.LogInfof("updateDataPartition: finish")
+	log.LogInfof("updateDataPartition: finissdk/data/wrapper/wrapper.goh")
 	return err
 }
 
@@ -496,7 +490,7 @@ func (w *Wrapper) AllocatePreLoadDataPartition(volName string, count int, capaci
 	rwPartitionGroups := make([]*DataPartition, 0)
 	for _, partition := range dpv.DataPartitions {
 		dp := convert(partition)
-		if (proto.IsCold(w.volType) || proto.VolSupportsBlobStore(w.allowedStorageClass)) && !proto.IsPreLoadDp(dp.PartitionType) {
+		if (proto.IsStorageClassBlobStore(w.volStorageClass)) && !proto.IsPreLoadDp(dp.PartitionType) {
 			continue
 		}
 		log.LogInfof("updateDataPartition: dp(%v)", dp)
@@ -538,7 +532,7 @@ func (w *Wrapper) replaceOrInsertPartition(dp *DataPartition) {
 // GetDataPartition returns the data partition based on the given partition ID.
 func (w *Wrapper) GetDataPartition(partitionID uint64) (*DataPartition, error) {
 	dp, ok := w.tryGetPartition(partitionID)
-	if !ok && (!proto.IsCold(w.volType) || !proto.VolSupportsBlobStore(w.allowedStorageClass)) { // cache miss && hot volume
+	if !ok && (!proto.IsCold(w.volType) || !proto.IsStorageClassReplica(w.volStorageClass)) { // cache miss && hot volume
 		err := w.getDataPartitionFromMaster(partitionID)
 		if err == nil {
 			dp, ok = w.tryGetPartition(partitionID)
