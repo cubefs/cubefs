e8f790a2d94705fe86b36771d6207e0d0e11e3bf
chihe <chi.he@oppo.com>
2023-12-18 22:31:46 -0800

    fix(client): [hybrid cloud] only clear dp when volume storage class is blobstore
    Signed-off-by: chihe <chi.he@oppo.com>

--- libsdk/libsdk.go
+++ libsdk/libsdk.go
@@ -240,6 +240,7 @@ type client struct {
 	cluster             string
 	dirChildrenNumLimit uint32
 	enableAudit         bool
+	volStorageClass     uint32
 
 	// runtime context
 	cwd    string // current working directory
@@ -1442,6 +1443,7 @@ func (c *client) start() (err error) {
 		OnCacheBcache:     c.bc.Put,
 		OnEvictBcache:     c.bc.Evict,
 		DisableMetaCache:  true,
+		VolStorageClass:   c.volStorageClass,
 	}); err != nil {
 		log.LogErrorf("newClient NewExtentClient failed(%v)", err)
 		return
@@ -1711,6 +1713,7 @@ func (c *client) loadConfFromMaster(masters []string) (err error) {
 	c.cacheAction = volumeInfo.CacheAction
 	c.cacheRuleKey = volumeInfo.CacheRule
 	c.cacheThreshold = volumeInfo.CacheThreshold
+	c.volStorageClass = volumeInfo.VolStorageClass
 
 	var clusterInfo *proto.ClusterInfo
 	clusterInfo, err = mc.AdminAPI().GetClusterInfo()
