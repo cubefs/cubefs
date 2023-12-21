0cf940f65d5fbd6c49f6595a825fbd71a92249dc
slasher <mcq.sejust@gmail.com>
2023-12-21 16:41:15 +0800

    feat(flashnode): make slot with user defined
    . #22200518
    
    Signed-off-by: slasher <mcq.sejust@gmail.com>

--- master/metadata_fsm.go
+++ master/metadata_fsm.go
@@ -149,7 +149,7 @@ func (mf *MetadataFsm) Apply(command []byte, index uint64) (resp interface{}, er
 	switch cmd.Op {
 	case opSyncDeleteDataNode, opSyncDeleteMetaNode, opSyncDeleteVol, opSyncDeleteDataPartition, opSyncDeleteMetaPartition,
 		opSyncDeleteUserInfo, opSyncDeleteAKUser, opSyncDeleteVolUser, opSyncDeleteQuota, opSyncDeleteLcNode,
-		opSyncDeleteLcConf, opSyncS3QosDelete, opSyncDeleteDecommissionDisk:
+		opSyncDeleteLcConf, opSyncS3QosDelete, opSyncDeleteDecommissionDisk, opSyncDeleteFlashNode, opSyncDeleteFlashGroup:
 		if err = mf.delKeyAndPutIndex(cmd.K, cmdMap); err != nil {
 			panic(err)
 		}
