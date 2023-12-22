3e8770796c9647ce8abc3449b3d9c22178e45c8f
true1064 <tangjingyu@oppo.com>
2023-12-22 18:47:58 +0800

    fix(master): [hybrid cloud] in create vol procedure, update datapartition view cache right after the creation of datapartitions
    Signed-off-by: true1064 <tangjingyu@oppo.com>

--- master/cluster.go
+++ master/cluster.go
@@ -3732,6 +3732,7 @@ func (c *Cluster) createVol(req *createVolReq) (vol *Vol, err error) {
 			req.name, readWriteDataPartitions, proto.MediaTypeString(chosenMediaType))
 	}
 
+	vol.dataPartitions.updateResponseCache(true, 0, vol.VolType)
 	vol.updateViewCache(c)
 	// NOTE: update dp view cache
 	vol.dataPartitions.updateResponseCache(true, 0, vol)
