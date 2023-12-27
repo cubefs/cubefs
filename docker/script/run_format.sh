8f3e31faa8beb1e3cde6a149b02c9d3bfeca0755
slasher <shenjie1@oppo.com>
2023-12-27 09:54:56 +0800

    feat(ci): show diffs of gofmt in ci format
    Signed-off-by: slasher <shenjie1@oppo.com>

--- docker/script/run_format.sh
+++ docker/script/run_format.sh
@@ -1,26 +1,23 @@
 #!/bin/bash
 CurrentPath=$(cd $(dirname ${BASH_SOURCE[0]}); pwd)
 pushd ${CurrentPath}/../../
-find . -type f -name "*.go" | grep -v 'vendor' |grep -v 'depends'| xargs gofmt -l > gofmt_results.txt
+find . -type f -name "*.go" | grep -v 'vendor' |grep -v 'depends'| xargs gofmt -l -d > gofmt_results.txt
 cat gofmt_results.txt
 if [ "$(cat gofmt_results.txt|wc -l)" -gt 0  ]; then
     popd
     exit 1;
 fi
+rm -f gofmt_results.txt
 popd
 
 export PATH=$PATH:/go/bin
 
-pushd ${CurrentPath}/../../storage
-go generate ./...
-if [[ $? -ne 0 ]]; then
-    exit 1
-fi
-popd
-
-pushd ${CurrentPath}/../../cli
-go generate ./...
-if [[ $? -ne 0 ]]; then
-    exit 1
-fi
-popd
+for subdir in storage cli
+do
+    pushd ${CurrentPath}/../../${subdir}
+    go generate ./...
+    if [[ $? -ne 0 ]]; then
+        exit 1
+    fi
+    popd
+done
