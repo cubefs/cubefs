d1ee3b10bfc674e36be15d67018f494ec4dbe75f
true1064 <tangjingyu@oppo.com>
2024-01-10 20:16:08 +0800

    feat(meta): handle compatibility when old client send create request without field StorageClass
    Signed-off-by: true1064 <tangjingyu@oppo.com>

--- metanode/const.go
+++ metanode/const.go
@@ -236,7 +236,7 @@ const (
 	metaNodeDeleteBatchCountKey = "batchCount"
 	configNameResolveInterval   = "nameResolveInterval" // int
 
-	cfgDefaultMediaType = "defaultMediaType"
+	cfgLegacyStorageClass = "legacyReplicaStorageClass"
 )
 
 const (
