c50dcf5a5a4e7582064e01173ec1f4597d8d1e1b
baihailong <baihailong@oppo.com>
2024-01-12 01:27:10 -0800

    fix(sdk): not check result of InodeGet_ll leadto panic in Truncate.
    Signed-off-by: baihailong <baihailong@oppo.com>

--- sdk/data/stream/extent_client.go
+++ sdk/data/stream/extent_client.go
@@ -473,6 +473,10 @@ func (client *ExtentClient) Truncate(mw *meta.MetaWrapper, parentIno uint64, ino
 	var oldSize uint64
 	if mw.EnableSummary {
 		info, err = mw.InodeGet_ll(inode)
+		if err != nil || info == nil {
+			log.LogErrorf("Truncate: InodeGet failed, inode(%d) err(%v)\n", inode, err)
+			return err
+		}
 		oldSize = info.Size
 	}
 	err = s.IssueTruncRequest(size)
