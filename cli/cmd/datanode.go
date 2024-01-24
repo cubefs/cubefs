b7a8b4237c03719a3affc52fded6cf256e02fde3
leonrayang <chl696@sina.com>
2024-01-24 15:47:10 +0800

    Revert "[Enhancement]: Support reporting the total size of writable space in the cluster."
    This reverts commit 37bdadfc776e6710fcf799cc5dcf399594980dd2.
    
    Signed-off-by: leonrayang <chl696@sina.com>

--- cli/cmd/datanode.go
+++ cli/cmd/datanode.go
@@ -67,7 +67,7 @@ func newDataNodeListCmd(client *master.MasterClient) *cobra.Command {
 			stdoutln(formatNodeViewTableHeader())
 			for _, node := range view.DataNodes {
 				if optFilterStatus != "" &&
-					!strings.Contains(formatNodeStatus(node.IsActive), optFilterStatus) {
+					!strings.Contains(formatNodeStatus(node.Status), optFilterStatus) {
 					continue
 				}
 				if optFilterWritable != "" &&
