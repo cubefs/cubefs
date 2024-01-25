ab37171d70f8e3249900575c36af914dd42b44ce
shuqiang-zheng <zhengshuqiang@oppo.com>
2024-01-25 11:08:27 +0800

    enhance(cli):Modifying Volume Deletion Related Error Messages.
    Signed-off-by: shuqiang-zheng <zhengshuqiang@oppo.com>

--- master/api_service_test.go
+++ master/api_service_test.go
@@ -126,7 +126,6 @@ func createDefaultMasterServerForTest() *Server {
 	}`
 
 	testServer, err := createMasterServer(cfgJSON)
-	testServer.cluster.cfg.volForceDeletion = true
 
 	if err != nil {
 		panic(err)
