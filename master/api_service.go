bd80a4e115cb233247c3cce796102bfbf372aa5c
chihe <chihe@oppo.com>
2024-01-26 15:00:34 +0800

    feature(master): support specify count for datanode decomission
    Signed-off-by: chihe <chihe@oppo.com>

--- master/api_service.go
+++ master/api_service.go
@@ -2553,6 +2553,7 @@ func (m *Server) decommissionDataNode(w http.ResponseWriter, r *http.Request) {
 		rstMsg      string
 		offLineAddr string
 		raftForce   bool
+		limit       int
 		err         error
 	)
 	metric := exporter.NewTPCnt(apiToMetricsName(proto.DecommissionDataNode))
@@ -2560,7 +2561,7 @@ func (m *Server) decommissionDataNode(w http.ResponseWriter, r *http.Request) {
 		doStatAndMetric(proto.DecommissionDataNode, metric, err, nil)
 	}()
 
-	if offLineAddr, err = parseDecomDataNodeReq(r); err != nil {
+	if offLineAddr, limit, err = parseDecomDataNodeReq(r); err != nil {
 		sendErrReply(w, r, &proto.HTTPReply{Code: proto.ErrCodeParamError, Msg: err.Error()})
 		return
 	}
@@ -2575,7 +2576,7 @@ func (m *Server) decommissionDataNode(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
-	if err = m.cluster.migrateDataNode(offLineAddr, "", raftForce, 0); err != nil {
+	if err = m.cluster.migrateDataNode(offLineAddr, "", raftForce, limit); err != nil {
 		sendErrReply(w, r, newErrHTTPReply(err))
 		return
 	}
