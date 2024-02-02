43363ac12ce0aea01c8e858562a9fde9dc3ae8bb
Victor1319 <834863182@qq.com>
2024-02-02 17:37:41 +0800

    fix(data): cancel extent count limit when create extent.
     Cancel the limit of "maxCount + 10" extents to avoid no available data partitions.
    
    Signed-off-by: Victor1319 <834863182@qq.com>

--- datanode/wrap_operator.go
+++ datanode/wrap_operator.go
@@ -218,13 +218,6 @@ func (s *DataNode) handlePacketToCreateExtent(p *repl.Packet) {
 		return
 	}
 
-	// in case too many extents
-	if partition.GetExtentCount() >= storage.MaxExtentCount+10 {
-		log.LogWarnf("[handlePacketToCreateExtent] dp(%v) not enough space, too many extents(%v)", partition.partitionID, partition.GetExtentCount())
-		err = storage.NoSpaceError
-		return
-	}
-
 	partition.disk.allocCheckLimit(proto.IopsWriteType, 1)
 	partition.disk.limitWrite.Run(0, func() {
 		err = partition.ExtentStore().Create(p.ExtentID)
