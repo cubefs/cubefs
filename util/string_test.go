36c01e9c167578c2c3f19d777176cb3f7add577a
slasher <mcq.sejust@gmail.com>
2024-02-07 18:38:08 +0800

    style(util): convert any value to string in util
    util.Any2String
    util.String2Any
    
    BenchmarkStrFmt2String
    BenchmarkStrFmt2String-4    20373084     57.41 ns/op    2 B/op    1 allocs/op
    BenchmarkStrAny2String
    BenchmarkStrAny2String-4    226842036     5.39 ns/op    0 B/op    0 allocs/op
    BenchmarkStrString2Any
    BenchmarkStrString2Any-4    47629057     24.11 ns/op    0 B/op    0 allocs/op
    
    Signed-off-by: slasher <mcq.sejust@gmail.com>

--- util/string_test.go
+++ util/string_test.go
@@ -15,6 +15,7 @@
 package util_test
 
 import (
+	"fmt"
 	"testing"
 
 	"github.com/cubefs/cubefs/util"
@@ -29,5 +30,108 @@ func TestRandomString(t *testing.T) {
 
 func TestSubStr(t *testing.T) {
 	str := "abcd"
-	require.Equal(t, util.SubString(str, 1, 2), "b")
+	require.Equal(t, "b", util.SubString(str, 1, 2))
+	require.Equal(t, str, util.SubString(str, -1, 100))
+}
+
+func TestStringConvert(t *testing.T) {
+	const s77 = "77"
+	const s99 = "99"
+	const v77 = 77
+	const v99 = 99
+	var (
+		val                      = struct{}{}
+		valStr        string     = s77
+		valBool       bool       = true
+		valInt        int        = v77
+		valInt8       int8       = v77
+		valInt16      int16      = v77
+		valInt32      int32      = v77
+		valInt64      int64      = v77
+		valUint       uint       = v77
+		valUint8      uint8      = v77
+		valUint16     uint16     = v77
+		valUint32     uint32     = v77
+		valUint64     uint64     = v77
+		valFloat32    float32    = 77.77
+		valFloat64    float64    = 77.77
+		valComplex64  complex64  = complex(77, 77)
+		valComplex128 complex128 = complex(77, 77)
+	)
+
+	for _, cs := range []interface{}{valBool, &valBool} {
+		require.Equal(t, "true", util.Any2String(cs))
+	}
+	require.NoError(t, util.String2Any("false", &valBool))
+	require.False(t, valBool)
+
+	for _, cs := range []interface{}{
+		valStr, &valStr,
+		valInt, &valInt, valInt8, &valInt8, valInt16, &valInt16,
+		valInt32, &valInt32, valInt64, &valInt64,
+		valUint, &valUint, valUint8, &valUint8, valUint16, &valUint16,
+		valUint32, &valUint32, valUint64, &valUint64,
+	} {
+		require.Equal(t, s77, util.Any2String(cs))
+	}
+	require.NoError(t, util.String2Any(s99, &valStr))
+	require.NoError(t, util.String2Any(s99, &valInt))
+	require.NoError(t, util.String2Any(s99, &valInt8))
+	require.NoError(t, util.String2Any(s99, &valInt16))
+	require.NoError(t, util.String2Any(s99, &valInt32))
+	require.NoError(t, util.String2Any(s99, &valInt64))
+	require.NoError(t, util.String2Any(s99, &valUint))
+	require.NoError(t, util.String2Any(s99, &valUint8))
+	require.NoError(t, util.String2Any(s99, &valUint16))
+	require.NoError(t, util.String2Any(s99, &valUint32))
+	require.NoError(t, util.String2Any(s99, &valUint64))
+	require.Equal(t, s99, valStr)
+	require.Equal(t, int(v99), valInt)
+	require.Equal(t, int8(v99), valInt8)
+	require.Equal(t, int16(v99), valInt16)
+	require.Equal(t, int32(v99), valInt32)
+	require.Equal(t, int64(v99), valInt64)
+	require.Equal(t, uint(v99), valUint)
+	require.Equal(t, uint8(v99), valUint8)
+	require.Equal(t, uint16(v99), valUint16)
+	require.Equal(t, uint32(v99), valUint32)
+	require.Equal(t, uint64(v99), valUint64)
+
+	for _, cs := range []interface{}{
+		valFloat32, &valFloat32, valFloat64, &valFloat64,
+		valComplex64, &valComplex64, valComplex128, &valComplex128,
+	} {
+		t.Logf(util.Any2String(cs))
+	}
+	require.NoError(t, util.String2Any("99.99", &valFloat32))
+	require.NoError(t, util.String2Any("99.99", &valFloat64))
+	require.NoError(t, util.String2Any("(99+99i)", &valComplex64))
+	require.NoError(t, util.String2Any("(99+99i)", &valComplex128))
+	t.Log(valFloat32, valFloat64, valComplex64, valComplex128)
+
+	t.Log("unknown type", util.Any2String(val))
+	require.Error(t, util.String2Any(s99, &val))
+	require.Error(t, util.String2Any("not-bool", &valBool))
+}
+
+func BenchmarkStrFmt2String(b *testing.B) {
+	var val int64 = 77
+	for ii := 0; ii < b.N; ii++ {
+		_ = fmt.Sprintf("%v", val)
+	}
+}
+
+func BenchmarkStrAny2String(b *testing.B) {
+	var val int64 = 77
+	for ii := 0; ii < b.N; ii++ {
+		_ = util.Any2String(val)
+	}
+}
+
+func BenchmarkStrString2Any(b *testing.B) {
+	const s = "77.77"
+	var valFloat64 float64
+	for ii := 0; ii < b.N; ii++ {
+		_ = util.String2Any(s, &valFloat64)
+	}
 }
