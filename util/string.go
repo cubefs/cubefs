0633f94f5a27f451d03ccff995d1416f71b6f9fa
slasher <mcq.sejust@gmail.com>
2024-02-07 18:38:08 +0800

    style(util): convert any value to string in util
    util.Any2String
    util.String2Any
    
    BenchmarkStrFmt2String
    BenchmarkStrFmt2String-4    20373084     57.41 ns/op    2 B/op    1 allocs/op
    BenchmarkStrAny2String
    BenchmarkStrAny2String-4    226842036     5.39 ns/op    0 B/op    0 allocs/op
    BenchmarkStrString2Any
    BenchmarkStrString2Any-4    47629057     24.11 ns/op    0 B/op    0 allocs/op
    
    Signed-off-by: slasher <mcq.sejust@gmail.com>

--- util/string.go
+++ util/string.go
@@ -16,7 +16,9 @@ package util
 
 import (
 	"crypto/rand"
+	"fmt"
 	"math/big"
+	"strconv"
 	"strings"
 )
 
@@ -64,4 +66,171 @@ func RandomString(length int, seed RandomSeed) string {
 		result += string(runs[randNumber.Uint64()])
 	}
 	return result
+}
+
+// Any2String format any value to string.
+func Any2String(value interface{}) string {
+	var val string
+	switch v := value.(type) {
+	case string:
+		val = v
+	case *string:
+		val = *v
+	case bool:
+		val = strconv.FormatBool(v)
+	case *bool:
+		val = strconv.FormatBool(*v)
+
+	case int:
+		val = strconv.FormatInt(int64(v), 10)
+	case int8:
+		val = strconv.FormatInt(int64(v), 10)
+	case int16:
+		val = strconv.FormatInt(int64(v), 10)
+	case int32:
+		val = strconv.FormatInt(int64(v), 10)
+	case int64:
+		val = strconv.FormatInt(int64(v), 10)
+	case *int:
+		val = strconv.FormatInt(int64(*v), 10)
+	case *int8:
+		val = strconv.FormatInt(int64(*v), 10)
+	case *int16:
+		val = strconv.FormatInt(int64(*v), 10)
+	case *int32:
+		val = strconv.FormatInt(int64(*v), 10)
+	case *int64:
+		val = strconv.FormatInt(int64(*v), 10)
+
+	case uint:
+		val = strconv.FormatUint(uint64(v), 10)
+	case uint8:
+		val = strconv.FormatUint(uint64(v), 10)
+	case uint16:
+		val = strconv.FormatUint(uint64(v), 10)
+	case uint32:
+		val = strconv.FormatUint(uint64(v), 10)
+	case uint64:
+		val = strconv.FormatUint(uint64(v), 10)
+	case *uint:
+		val = strconv.FormatUint(uint64(*v), 10)
+	case *uint8:
+		val = strconv.FormatUint(uint64(*v), 10)
+	case *uint16:
+		val = strconv.FormatUint(uint64(*v), 10)
+	case *uint32:
+		val = strconv.FormatUint(uint64(*v), 10)
+	case *uint64:
+		val = strconv.FormatUint(uint64(*v), 10)
+
+	case float32:
+		val = strconv.FormatFloat(float64(v), 'f', 6, 64)
+	case float64:
+		val = strconv.FormatFloat(float64(v), 'f', 6, 64)
+	case *float32:
+		val = strconv.FormatFloat(float64(*v), 'f', 6, 64)
+	case *float64:
+		val = strconv.FormatFloat(float64(*v), 'f', 6, 64)
+	case complex64:
+		val = strconv.FormatComplex(complex128(v), 'f', 6, 64)
+	case complex128:
+		val = strconv.FormatComplex(complex128(v), 'f', 6, 64)
+	case *complex64:
+		val = strconv.FormatComplex(complex128(*v), 'f', 6, 64)
+	case *complex128:
+		val = strconv.FormatComplex(complex128(*v), 'f', 6, 64)
+
+	default:
+		val = fmt.Sprintf("%v", value)
+	}
+	return val
+}
+
+// Any2String parse string to pointer of value.
+func String2Any(str string, pvalue interface{}) error {
+	var val interface{}
+	var err error
+
+	switch v := pvalue.(type) {
+	case *string:
+		val = str
+	case *bool:
+		val, err = strconv.ParseBool(str)
+
+	case *int:
+		val, err = strconv.ParseInt(str, 10, 0)
+	case *int8:
+		val, err = strconv.ParseInt(str, 10, 8)
+	case *int16:
+		val, err = strconv.ParseInt(str, 10, 16)
+	case *int32:
+		val, err = strconv.ParseInt(str, 10, 32)
+	case *int64:
+		val, err = strconv.ParseInt(str, 10, 64)
+
+	case *uint:
+		val, err = strconv.ParseUint(str, 10, 0)
+	case *uint8:
+		val, err = strconv.ParseUint(str, 10, 8)
+	case *uint16:
+		val, err = strconv.ParseUint(str, 10, 16)
+	case *uint32:
+		val, err = strconv.ParseUint(str, 10, 32)
+	case *uint64:
+		val, err = strconv.ParseUint(str, 10, 64)
+
+	case *float32:
+		val, err = strconv.ParseFloat(str, 32)
+	case *float64:
+		val, err = strconv.ParseFloat(str, 64)
+	case *complex64:
+		val, err = strconv.ParseComplex(str, 64)
+	case *complex128:
+		val, err = strconv.ParseComplex(str, 128)
+
+	default:
+		return fmt.Errorf("unknown type %v of %s %v", v, str, pvalue)
+	}
+	if err != nil {
+		return err
+	}
+
+	switch v := pvalue.(type) {
+	case *string:
+		*v = val.(string)
+	case *bool:
+		*v = val.(bool)
+
+	case *int:
+		*v = int(val.(int64))
+	case *int8:
+		*v = int8(val.(int64))
+	case *int16:
+		*v = int16(val.(int64))
+	case *int32:
+		*v = int32(val.(int64))
+	case *int64:
+		*v = int64(val.(int64))
+
+	case *uint:
+		*v = uint(val.(uint64))
+	case *uint8:
+		*v = uint8(val.(uint64))
+	case *uint16:
+		*v = uint16(val.(uint64))
+	case *uint32:
+		*v = uint32(val.(uint64))
+	case *uint64:
+		*v = uint64(val.(uint64))
+
+	case *float32:
+		*v = float32(val.(float64))
+	case *float64:
+		*v = float64(val.(float64))
+	case *complex64:
+		*v = complex64(val.(complex128))
+	case *complex128:
+		*v = complex128(val.(complex128))
+	}
+	return nil
 }
