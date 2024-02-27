8ac3bc6e6031fc7a1d0b7a3c0c44679a068cffe8
slasher <mcq.sejust@gmail.com>
2024-02-27 11:18:34 +0800

    chore(format): format code with gofumpt
    Signed-off-by: slasher <mcq.sejust@gmail.com>

--- docker/script/run_format.sh
+++ docker/script/run_format.sh
@@ -1,10 +1,14 @@
 #!/bin/bash
 CurrentPath=$(cd $(dirname ${BASH_SOURCE[0]}); pwd)
+gofmtFile=gofmt_results.diff
 pushd ${CurrentPath}/../../
-find . -type f -name "*.go" | grep -v 'vendor' |grep -v 'depends'| xargs gofmt -l > gofmt_results.txt
-cat gofmt_results.txt
-if [ "$(cat gofmt_results.txt|wc -l)" -gt 0  ]; then 
+find . -type f -name "*.go" | \
+    grep -v 'vendor'|grep -v 'depends'|grep -v ./sdk/graphql/client/ | \
+    xargs gofmt -l -d > ${gofmtFile}
+cat ${gofmtFile}
+if [ "$(cat ${gofmtFile}|wc -l)" -gt 0  ]; then
     popd
-    exit 1; 
+    exit 1;
 fi
+rm -f ${gofmtFile}
 popd
