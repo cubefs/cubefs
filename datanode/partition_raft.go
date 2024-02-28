0cc6a3631d58f50ab582fdf6f5da69f5539b8906
true1064 <true1063@163.com>
2024-02-28 19:06:57 +0800

    fix(data): when traversing the replicas slice, avoid panics caused by changes in the slice length
    Signed-off-by: true1064 <true1063@163.com>

--- datanode/partition_raft.go
+++ datanode/partition_raft.go
@@ -732,19 +732,23 @@ func (dp *DataPartition) getMemberExtentIDAndPartitionSize() (maxExtentID, Parti
 }
 
 func (dp *DataPartition) broadcastMinAppliedID(minAppliedID uint64) (err error) {
-	for i := 0; i < dp.getReplicaLen(); i++ {
-		p := NewPacketToBroadcastMinAppliedID(dp.partitionID, minAppliedID)
-		replicaHostParts := strings.Split(dp.getReplicaAddr(i), ":")
+	allReplica := dp.getReplicaCopy()
+
+	for i := 0; i < len(allReplica); i++ {
+		targetReplica := allReplica[i]
+
+		replicaHostParts := strings.Split(targetReplica, ":")
 		replicaHost := strings.TrimSpace(replicaHostParts[0])
 		if LocalIP == replicaHost {
-			log.LogDebugf("partition(%v) local no send msg. localIP(%v) replicaHost(%v) appliedId(%v)",
+			log.LogDebugf("[broadcastMinAppliedID] partition(%v) local no send msg. localIP(%v) replicaHost(%v) appliedId(%v)",
 				dp.partitionID, LocalIP, replicaHost, dp.appliedID)
 			dp.minAppliedID = minAppliedID
 			continue
 		}
-		target := dp.getReplicaAddr(i)
+
+		p := NewPacketToBroadcastMinAppliedID(dp.partitionID, minAppliedID)
 		var conn *net.TCPConn
-		conn, err = gConnPool.GetConnect(target)
+		conn, err = gConnPool.GetConnect(targetReplica)
 		if err != nil {
 			return
 		}
@@ -759,7 +763,8 @@ func (dp *DataPartition) broadcastMinAppliedID(minAppliedID uint64) (err error) 
 			return
 		}
 		gConnPool.PutConnect(conn, false)
-		log.LogDebugf("partition(%v) minAppliedID(%v)", dp.partitionID, minAppliedID)
+		log.LogDebugf("[broadcastMinAppliedID] partition(%v) minAppliedID(%v) has sent to replica(%v)",
+			dp.partitionID, minAppliedID, targetReplica)
 	}
 
 	return
@@ -784,7 +789,7 @@ func (dp *DataPartition) getAllReplicaAppliedID() (allAppliedID []uint64, replyN
 		target := replicas[i]
 		appliedID, err := dp.getRemoteAppliedID(target, p)
 		if err != nil {
-			log.LogErrorf("partition(%v) getRemoteAppliedID Failed(%v).", dp.partitionID, err)
+			log.LogErrorf("partition(%v) getRemoteAppliedID from replica(%v) Failed(%v).", dp.partitionID, targetReplica, err)
 			continue
 		}
 		if appliedID == 0 {
