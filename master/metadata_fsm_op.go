ca861f401701318b78725058218989c2132837ce
zhaochenyang <zhaochenyang@oppo.com>
2024-02-28 17:18:23 +0800

    fix(lcnode): fix lcnode panic and reduce task allocation time
    #21938894
    Signed-off-by: zhaochenyang <zhaochenyang@oppo.com>

--- master/metadata_fsm_op.go
+++ master/metadata_fsm_op.go
@@ -541,6 +541,10 @@ func (m *RaftCmd) setOpType() {
 		m.Op = opSyncAddAKUser
 	case volUserAcronym:
 		m.Op = opSyncAddVolUser
+	case lcNodeAcronym:
+		m.Op = opSyncAddLcNode
+	case lcConfigurationAcronym:
+		m.Op = opSyncAddLcConf
 	default:
 		log.LogWarnf("action[setOpType] unknown opCode[%v]", keyArr[1])
 	}
