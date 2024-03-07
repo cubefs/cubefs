0a530c9cbbaf763446ec04c0cf4d6f8410d11e80
chihe <chihe@oppo.com>
2024-03-07 15:43:49 +0800

    fix(client): hybrid cloud if file is stored in ebs, do not fetch extentkey list from metanode in lookup operation
    Signed-off-by: chihe <chihe@oppo.com>

--- proto/packet.go
+++ proto/packet.go
@@ -650,6 +650,8 @@ func (p *Packet) GetOpMsg() (m string) {
 		m = "OpBatchUnlockNormalExtent"
 	case OpMetaUpdateExtentKeyAfterMigration:
 		m = "OpMetaUpdateExtentKeyAfterMigration"
+	case OpMetaRenewalForbiddenMigration:
+		m = "OpMetaRenewalForbiddenMigration"
 	default:
 		m = fmt.Sprintf("op:%v not found", p.Opcode)
 	}
