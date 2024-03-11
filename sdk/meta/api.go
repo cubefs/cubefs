14b7156f4eaa2218fa6aad55e1816aef6b82ba06
sunjiapeng <782615313@qq.com>
2024-03-11 14:54:34 +0800

    fix(meta): create dentry concurrency cause orphan inodes
    Signed-off-by: sunjiapeng <782615313@qq.com>

--- sdk/meta/api.go
+++ sdk/meta/api.go
@@ -311,10 +311,8 @@ create_dentry:
 		}
 		return nil, statusToErrno(status)
 	} else if status != statusOK {
-		if status != statusExist {
-			mw.iunlink(mp, info.Inode, mw.Client.GetLatestVer(), 0, fullPath)
-			mw.ievict(mp, info.Inode, fullPath)
-		}
+		mw.iunlink(mp, info.Inode, mw.Client.GetLatestVer(), 0, fullPath)
+		mw.ievict(mp, info.Inode, fullPath)
 		return nil, statusToErrno(status)
 	}
 	if mw.EnableSummary {
@@ -1645,9 +1643,7 @@ func (mw *MetaWrapper) link(parentID uint64, name string, ino uint64, fullPath s
 	if err != nil {
 		return nil, statusToErrno(status)
 	} else if status != statusOK {
-		if status != statusExist {
-			mw.iunlink(mp, ino, mw.Client.GetLatestVer(), 0, fullPath)
-		}
+		mw.iunlink(mp, ino, mw.Client.GetLatestVer(), 0, fullPath)
 		return nil, statusToErrno(status)
 	}
 	return info, nil
