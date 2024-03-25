a0012889551a750a3340f761747962f516c84b16
true1064 <tangjingyu@oppo.com>
2024-03-25 19:32:46 +0800

    fix(meta): [hybrid cloud] set migrate storageClass in function fsmInternalDeleteMigrationExtentKey close:#22029476 Signed-off-by: tangjingyu <tangjingyu@oppo.com>

--- metanode/sorted_hybridcloud_obj_extents.go
+++ metanode/sorted_hybridcloud_obj_extents.go
@@ -1,6 +1,10 @@
 package metanode
 
-import "github.com/cubefs/cubefs/proto"
+import (
+	"bytes"
+	"fmt"
+	"github.com/cubefs/cubefs/proto"
+)
 
 type SortedHybridCloudExtents struct {
 	sortedEks interface{}
@@ -34,4 +38,28 @@ func (sem *SortedHybridCloudExtentsMigration) GetExpiredTime() int64 {
 
 func NewSortedHybridCloudExtentsMigration() *SortedHybridCloudExtentsMigration {
 	return &SortedHybridCloudExtentsMigration{storageClass: proto.StorageClass_Unspecified}
+}
+
+func (sem *SortedHybridCloudExtentsMigration) String() string {
+	buff := bytes.NewBuffer(nil)
+	buff.Grow(128)
+	buff.WriteString("{")
+	buff.WriteString(fmt.Sprintf("\"StorageClass\":%d,", sem.storageClass))
+	buff.WriteString(fmt.Sprintf("\"expiredTime\"%d,", sem.expiredTime))
+
+	if sem.sortedEks == nil {
+		buff.WriteString(fmt.Sprint("\"ExtentsMigration\"[nil]"))
+		buff.WriteString("}")
+		return buff.String()
+	}
+
+	if proto.IsStorageClassReplica(sem.storageClass) {
+		buff.WriteString(fmt.Sprintf("\"ExtentsMigration\"[%s]", sem.sortedEks.(*SortedExtents)))
+	} else if proto.IsStorageClassBlobStore(sem.storageClass) {
+		buff.WriteString(fmt.Sprintf("\"ExtentsMigration\"[%s]", sem.sortedEks.(*SortedObjExtents)))
+	} else {
+		buff.WriteString(fmt.Sprint("\"ExtentsMigration\"[unknown type]"))
+	}
+	buff.WriteString("}")
+	return buff.String()
 }
