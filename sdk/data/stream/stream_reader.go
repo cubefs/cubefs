2f7d76b2f2f47d3fbd1fe770f8cd3475d72070a4
baihailong <baihailong@oppo.com>
2024-03-28 00:18:16 -0700

    fix(sdk): one streamer has multi server maybe leadto bad file descriptor.#21989411
    Signed-off-by: baihailong <baihailong@oppo.com>

--- sdk/data/stream/stream_reader.go
+++ sdk/data/stream/stream_reader.go
@@ -79,6 +79,7 @@ func NewStreamer(client *ExtentClient, inode uint64) *Streamer {
 	s.dirtylist = NewDirtyExtentList()
 	s.isOpen = true
 	s.pendingCache = make(chan bcacheKey, 1)
+	log.LogDebugf("NewStreamer: streamer(%v)", s)
 	go s.server()
 	go s.asyncBlockCache()
 	return s
@@ -90,7 +91,7 @@ func (s *Streamer) SetParentInode(inode uint64) {
 
 // String returns the string format of the streamer.
 func (s *Streamer) String() string {
-	return fmt.Sprintf("Streamer{ino(%v)}", s.inode)
+	return fmt.Sprintf("Streamer{ino(%v), refcnt(%v), isOpen(%v), inflight(%v), addr(%p)}", s.inode, s.refcnt, s.isOpen, len(s.request), s)
 }
 
 // TODO should we call it RefreshExtents instead?
@@ -302,7 +303,7 @@ func (s *Streamer) asyncBlockCache() {
 			}
 		case <-t.C:
 			if s.refcnt <= 0 {
-				s.isOpen = false
+				//s.isOpen = false
 				return
 			}
 		}
