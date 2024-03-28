14c91ab07ab67a5b142cb88aec75ff5163292a33
NaturalSelect <huangzhibin1@oppo.com>
2024-03-28 16:19:55 +0800

    feat(data): limit rw dp decrease count at once
    close: #21996578
    
    Signed-off-by: NaturalSelect <huangzhibin1@oppo.com>

--- sdk/data/wrapper/k_faster_random_selector.go
+++ sdk/data/wrapper/k_faster_random_selector.go
@@ -142,7 +142,20 @@ func (s *KFasterRandomSelector) RemoveDP(partitionID uint64) {
 	newRwPartition = append(newRwPartition, partitions[i+1:]...)
 
 	s.Refresh(newRwPartition)
+}
 
+func (s *KFasterRandomSelector) GetAllDp() (dps []*DataPartition) {
+	s.RLock()
+	defer s.RUnlock()
+	dps = make([]*DataPartition, len(s.partitions))
+	copy(dps, s.partitions)
+	return
+}
+
+func (s *KFasterRandomSelector) GetDpCount() (count int) {
+	s.RLock()
+	defer s.RUnlock()
+	count = len(s.partitions)
 	return
 }
 
