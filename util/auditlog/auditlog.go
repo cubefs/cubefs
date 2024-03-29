f7f012de3dc4720d9d673074f7f8f7026a4b7958
NaturalSelect <huangzhibin1@oppo.com>
2024-03-29 09:48:06 +0800

    feat(meta): audit log print millisecond
    Signed-off-by: NaturalSelect <huangzhibin1@oppo.com>

--- util/auditlog/auditlog.go
+++ util/auditlog/auditlog.go
@@ -293,6 +293,9 @@ func (a *Audit) formatMetaAudit(ipAddr, hostName, op, src, dst string, err error
 	} else {
 		errStr = "nil"
 	}
+	curTime := time.Now()
+	curTimeStr := curTime.Format("2006-01-02 15:04:05.000000")
+	timeZone, _ := curTime.Zone()
 	latencyStr := strconv.FormatInt(latency, 10) + " us"
 	srcInodeStr := strconv.FormatUint(srcInode, 10)
 	dstInodeStr := strconv.FormatUint(dstInode, 10)
