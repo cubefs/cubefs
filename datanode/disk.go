b7112d4aa8f252b2bab45033cf90c70981ac6547
NaturalSelect <huangzhibin1@oppo.com>
2024-04-02 10:34:06 +0800

    feat(data): flush extent cache in timer
    Signed-off-by: NaturalSelect <huangzhibin1@oppo.com>

--- datanode/disk.go
+++ datanode/disk.go
@@ -686,6 +686,9 @@ func (d *Disk) RestorePartition(visitor PartitionVisitor, allowDelay bool) (err 
 		}
 	}
 	close(loadCh)
+	// NOTE: wait for load dp goroutines
+	// avoid delete expired dp competing for io
+	wg.Wait()
 
 	if len(toDeleteExpiredPartitionNames) > 0 {
 		go func(toDeleteExpiredPartitions []string) {
@@ -698,7 +701,6 @@ func (d *Disk) RestorePartition(visitor PartitionVisitor, allowDelay bool) (err 
 			log.LogInfof("action[RestorePartition] delete expiredPartitions automatically finish")
 		}(toDeleteExpiredPartitionNames)
 	}
-	wg.Wait()
 	return err
 }
 
