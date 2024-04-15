e47d54f539c8ed0386d2d2ed0bcddd8e0c630d35
W9068822 <v-lijianrong1@oppo.com>
2024-04-15 10:42:13 +0800

    feat(metrics): report version of client, metanode, datanode, authnode, lcnode, objectnode
    Signed-off-by: W9068822 <v-lijianrong1@oppo.com>

---
+++ util/exporter/version.go
@@ -0,0 +1,57 @@
+package exporter
+
+import (
+	"time"
+
+	"github.com/cubefs/cubefs/proto"
+	"github.com/cubefs/cubefs/util/log"
+)
+
+const StatPeriod = time.Minute * time.Duration(1)
+
+type VersionMetrics struct {
+	stopC      chan struct{}
+	Metric     *Counter
+	moduleName string
+	statPeriod time.Duration
+}
+
+func NewVersionMetrics(module string) *VersionMetrics {
+	return &VersionMetrics{
+		moduleName: module,
+		Metric:     NewCounter(Version),
+		stopC:      make(chan struct{}),
+		statPeriod: StatPeriod,
+	}
+}
+
+func (m *VersionMetrics) SetStatPeriod(period time.Duration) {
+	m.statPeriod = period
+}
+
+func (m *VersionMetrics) Start() {
+	ticker := time.NewTicker(m.statPeriod)
+	for {
+		select {
+		case <-m.stopC:
+			ticker.Stop()
+			log.LogInfof("stop version metrics ticker")
+			return
+		case <-ticker.C:
+			m.doStat()
+		}
+	}
+}
+
+func (m *VersionMetrics) Stop() {
+	close(m.stopC)
+	log.LogInfof("stop version metrics")
+}
+
+func (m *VersionMetrics) doStat() {
+	m.setVersionMetrics()
+}
+
+func (m *VersionMetrics) setVersionMetrics() {
+	m.Metric.AddWithLabels(1, proto.GetVersion(m.moduleName).ToMap())
+}
