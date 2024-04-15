6d70385c0d18d9b1f96364c05875ccb9d4ee73f1
true1064 <tangjingyu@oppo.com>
2024-04-15 17:47:12 +0800

    fix(meta): [hybrid cloud] If the inode is a directory, it is not allowed to set migrate extents in the UpdateExtentKeyAfterMigration operation close:#22076233 Signed-off-by: tangjingyu <tangjingyu@oppo.com>

--- metanode/inode.go
+++ metanode/inode.go
@@ -2474,4 +2474,5 @@ func (i *Inode) UpdateHybridCloudParams(paramIno *Inode) {
 	i.HybridCouldExtentsMigration.storageClass = paramIno.HybridCouldExtentsMigration.storageClass
 	i.HybridCouldExtentsMigration.sortedEks = paramIno.HybridCouldExtentsMigration.sortedEks
 	i.HybridCouldExtentsMigration.expiredTime = paramIno.HybridCouldExtentsMigration.expiredTime
+	i.Type = paramIno.Type
 }
