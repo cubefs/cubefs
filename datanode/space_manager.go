5021ef5ee0836675ce9e46c97070a98db1ed0e40
Victor1319 <zengxuewei@oppo.com>
2024-04-15 20:08:12 +0800

    fix(data): Use usedCap as totalCap when usedCap bigger than totalCap. #22082582
    Signed-off-by: Victor1319 <zengxuewei@oppo.com>

--- datanode/space_manager.go
+++ datanode/space_manager.go
@@ -278,7 +278,12 @@ func (manager *SpaceManager) updateMetrics() {
 			continue
 		}
 
-		total += d.Total
+		if d.Used > d.Total {
+			total += d.Used
+		} else {
+			total += d.Total
+		}
+
 		used += d.Used
 		available += d.Available
 		totalPartitionSize += d.Allocated
