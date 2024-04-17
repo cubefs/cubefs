0fc7a908e0b15c217656ff0554ef0a1373591531
baihailong <baihailong@oppo.com>
2024-04-17 00:23:50 -0700

    fix(sdk): hybrid cloud waitForFlush blocked leadto deadlock.
    Signed-off-by: baihailong <baihailong@oppo.com>

--- sdk/data/stream/extent_client.go
+++ sdk/data/stream/extent_client.go
@@ -481,7 +481,7 @@ func (client *ExtentClient) CloseStream(inode uint64) error {
 	if !ok {
 		return nil
 	}
-
+	log.LogDebugf("CloseStream begin: streamer(%v)", s)
 	s.refcnt--
 	s.closeOpenHandler()
 	err := s.flush()
@@ -490,7 +490,7 @@ func (client *ExtentClient) CloseStream(inode uint64) error {
 		log.LogDebugf("CloseStream failed, streamer(%v), err(%v)", s, err)
 		return err
 	}
-	log.LogDebugf("CloseStream: streamer(%v)", s)
+	log.LogDebugf("CloseStream end: streamer(%v)", s)
 
 	return nil
 }
