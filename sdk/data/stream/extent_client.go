2059928dd05724c0e55e0d49b098a9380f49e9aa
baihailong <baihailong@oppo.com>
2024-04-16 19:46:13 -0700

    fix(sdk): ExtentHandler waitForFlush blocked leadto deadlock.#22088191
    Signed-off-by: baihailong <baihailong@oppo.com>

--- sdk/data/stream/extent_client.go
+++ sdk/data/stream/extent_client.go
@@ -361,6 +361,7 @@ func (client *ExtentClient) CloseStream(inode uint64) error {
 		return nil
 	}
 
+	log.LogDebugf("CloseStream begin: streamer(%v)", s)
 	s.refcnt--
 	s.closeOpenHandler()
 	err := s.flush()
@@ -369,7 +370,7 @@ func (client *ExtentClient) CloseStream(inode uint64) error {
 		log.LogDebugf("CloseStream failed, streamer(%v), err(%v)", s, err)
 		return err
 	}
-	log.LogDebugf("CloseStream: streamer(%v)", s)
+	log.LogDebugf("CloseStream end: streamer(%v)", s)
 
 	return nil
 }
