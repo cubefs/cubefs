aeb6366acfa01247522c763ce5223834d8327b34
leonrayang <changliang@oppo.com>
2024-04-19 18:42:03 +0800

    fix(master): go routine for qos leak while raft change but forget close
    close: #22098958
    
    Signed-off-by: leonrayang <changliang@oppo.com>

--- master/cluster.go
+++ master/cluster.go
@@ -3586,6 +3586,12 @@ func (c *Cluster) setForbidMpDecommission(isForbid bool) (err error) {
 func (c *Cluster) clearVols() {
 	c.volMutex.Lock()
 	defer c.volMutex.Unlock()
+	vols := c.vols
+	go func() {
+		for _, vol := range vols {
+			vol.qosManager.stop()
+		}
+	}()
 	c.vols = make(map[string]*Vol, 0)
 }
 
