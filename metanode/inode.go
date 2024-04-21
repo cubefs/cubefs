2944cbce15ffb00367290f6ad1406ba961b8e186
chihe <chihe@oppo.com>
2024-04-21 09:45:21 +0800

    fix(metanode): hybrid cloud when ek is nil but mek is not nil, no replacement occurs.
    close:#22037699
    
    Signed-off-by: chihe <chihe@oppo.com>

--- metanode/inode.go
+++ metanode/inode.go
@@ -2317,11 +2317,6 @@ func (i *Inode) SetDeleteMark() {
 	i.Unlock()
 }
 
-func (i *Inode) SetDeleteMigrationExtentKey() {
-	i.Lock()
-	i.Flag |= DeleteMigrationExtentKeyFlag
-	i.Unlock()
-}
 func (i *Inode) SetDeleteMigrationExtentKeyImmediately() {
 	i.Lock()
 	i.Flag |= DeleteMigrationExtentKeyFlag
