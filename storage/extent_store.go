5cf640bd90fa36d9c66425efdfc4a5dde1bfa8c8
NaturalSelect <huangzhibin1@oppo.com>
2024-05-13 09:49:06 +0800

    fix(data): avoid panic during apply rand write after store close
    Signed-off-by: NaturalSelect <huangzhibin1@oppo.com>

--- storage/extent_store.go
+++ storage/extent_store.go
@@ -136,7 +136,7 @@ type ExtentStore struct {
 	metadataFp             *os.File // metadata file pointer?
 	tinyExtentDeleteFp     *os.File
 	normalExtentDeleteFp   *os.File
-	closed                 bool
+	closed                 int32
 	availableTinyExtentC   chan uint64 // available tinyExtent channel
 	availableTinyExtentMap sync.Map
 	brokenTinyExtentC      chan uint64 // broken tinyExtent channel
@@ -231,7 +231,7 @@ func NewExtentStore(dataDir string, partitionID uint64, storeSize, dpType int, i
 	}
 	s.hasAllocSpaceExtentIDOnVerfiyFile = s.GetPreAllocSpaceExtentIDOnVerifyFile()
 	s.storeSize = storeSize
-	s.closed = false
+	s.closed = 0
 	err = s.initTinyExtent()
 	if err != nil {
 		return
@@ -315,11 +315,24 @@ func (s *ExtentStore) startFlushCache() {
 	}
 }
 
+func (s *ExtentStore) IsClosed() (closed bool) {
+	closed = atomic.LoadInt32(&s.closed) == 1
+	return
+}
+
+func (s *ExtentStore) setClosed(v bool) {
+	closed := int32(0)
+	if v {
+		closed = 1
+	}
+	atomic.StoreInt32(&s.closed, closed)
+}
+
 // Create creates an extent.
 func (s *ExtentStore) Create(extentID uint64) (err error) {
 	s.stopMutex.RLock()
 	defer s.stopMutex.RUnlock()
-	if s.closed {
+	if s.IsClosed() {
 		err = ErrStoreAlreadyClosed
 		log.LogErrorf("[Create] store(%v) failed to create extent(%v), err(%v)", s.dataPath, extentID, err)
 		return
@@ -585,7 +598,7 @@ func (s *ExtentStore) initBaseFileID() error {
 func (s *ExtentStore) Write(extentID uint64, offset, size int64, data []byte, crc uint32, writeType int, isSync bool, isHole bool, isRepair, isBackupWrite bool) (status uint8, err error) {
 	s.stopMutex.RLock()
 	defer s.stopMutex.RUnlock()
-	if s.closed {
+	if s.IsClosed() {
 		err = ErrStoreAlreadyClosed
 		log.LogErrorf("[Write] store(%v) failed to write extent(%v), err(%v)", s.dataPath, extentID, err)
 		return
@@ -789,7 +802,7 @@ func (s *ExtentStore) GetGcFlag(extId uint64) proto.GcFlag {
 func (s *ExtentStore) MarkDelete(extentID uint64, offset, size int64) (err error) {
 	s.stopMutex.RLock()
 	defer s.stopMutex.RUnlock()
-	if s.closed {
+	if s.IsClosed() {
 		err = ErrStoreAlreadyClosed
 		log.LogErrorf("[MarkDelete] store(%v) failed to mark delete extent(%v), err(%v)", s.dataPath, extentID, err)
 		return
@@ -881,7 +894,7 @@ func (s *ExtentStore) Flush() {
 	}()
 	s.mutex.Lock()
 	defer s.mutex.Unlock()
-	if s.closed {
+	if s.IsClosed() {
 		return
 	}
 	s.cache.Flush()
@@ -895,7 +908,7 @@ func (s *ExtentStore) Close() {
 	}()
 	s.mutex.Lock()
 	defer s.mutex.Unlock()
-	if s.closed {
+	if s.IsClosed() {
 		return
 	}
 	close(s.stopC)
@@ -918,7 +931,7 @@ func (s *ExtentStore) Close() {
 
 	s.stopMutex.Lock()
 	defer s.stopMutex.Unlock()
-	s.closed = true
+	s.setClosed(true)
 	if err := s.writeReadDirHint(); err != nil {
 		log.LogErrorf("[Close] store(%v) failed to write extent hint, err(%v)", s.dataPath, err)
 	}
