f0f0a63f67b46af0dbbc7e09f8c22b5370ba1150
chihe <chihe@oppo.com>
2024-05-15 19:48:21 +0800

    fix(datanode): remove root dir for disk error dp not loaded
    close:#22185410
    
    Signed-off-by: chihe <chihe@oppo.com>

--- master/topology.go
+++ master/topology.go
@@ -2405,6 +2405,7 @@ func (l *DecommissionDiskList) PopMarkDecommissionDisk(limit int) (count int, co
 	}
 	return count, collection
 }
+
 func (l *DecommissionDataPartitionList) Has(id uint64) bool {
 	l.mu.Lock()
 	_, ok := l.cacheMap[id]
