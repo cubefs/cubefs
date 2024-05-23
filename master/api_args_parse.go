f8ecd7cafd5a3725bae2d17f171f8b628c1b28fb
NaturalSelect <huangzhibin1@oppo.com>
2024-05-23 10:20:09 +0800

    feat(master): support reset dp restore status
    Signed-off-by: NaturalSelect <huangzhibin1@oppo.com>

--- master/api_args_parse.go
+++ master/api_args_parse.go
@@ -1982,3 +1982,11 @@ func extractDiskBrokenThreshold(r *http.Request) (ratio float64, err error) {
 	}
 	return strconv.ParseFloat(value, 64)
 }
+
+func parseRequestToResetDpRestoreStatus(r *http.Request) (dpId uint64, err error) {
+	if err = r.ParseForm(); err != nil {
+		return
+	}
+	dpId, err = extractDataPartitionID(r)
+	return
+}
