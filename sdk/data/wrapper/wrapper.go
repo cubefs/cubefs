31c164682b24f43f98706ffb6d1bb3ede33884d9
alingse <alingse@foxmail.com>
2024-05-26 23:21:04 +0800

    fix: fix miss makezero bug
    Signed-off-by: alingse <alingse@foxmail.com>

--- sdk/data/wrapper/wrapper.go
+++ sdk/data/wrapper/wrapper.go
@@ -446,7 +446,7 @@ func (w *Wrapper) getDataPartitionFromMaster(isInit bool, dpId uint64) (err erro
 	dpr.IsRecover = dpInfo.IsRecover
 	dpr.IsDiscard = dpInfo.IsDiscard
 
-	DataPartitions := make([]*proto.DataPartitionResponse, 1)
+	DataPartitions := make([]*proto.DataPartitionResponse, 0, 1)
 	DataPartitions = append(DataPartitions, dpr)
 	return w.updateDataPartitionByRsp(isInit, DataPartitions)
 }
