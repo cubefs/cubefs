e287f3a9ce812651555e72b55a2647ff83abe733
chihe <chihe@oppo.com>
2024-06-03 09:40:19 +0800

    fix(master): redundant replica not participate when decommissioning mutiple replicas of dp
    Signed-off-by: chihe <chihe@oppo.com>

--- master/cluster.go
+++ master/cluster.go
@@ -4407,6 +4407,12 @@ func (c *Cluster) TryDecommissionDisk(disk *DecommissionDisk) {
 				ns.AddToDecommissionDataPartitionList(dp, c)
 				ignoreIDs = append(ignoreIDs, dp.PartitionID)
 				continue
+			} else if strings.Contains(err.Error(), proto.ErrPerformingDecommission.Error()) {
+				disk.DecommissionDpTotal -= 1
+				ignoreIDs = append(ignoreIDs, dp.PartitionID)
+				log.LogWarnf("action[TryDecommissionDisk] disk(%v) dp(%v) is decommissioning",
+					disk.decommissionInfo(), dp.PartitionID)
+				continue
 			} else {
 				// mark as failed and set decommission src, make sure it can be included in the calculation of progress
 				dp.DecommissionSrcAddr = node.Addr
