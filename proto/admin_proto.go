84e70ec172c2992a9bf74067802c036f2883b3a9
chihe <chihe@oppo.com>
2024-06-20 10:03:38 +0800

    fix(datanode): check disk path for replica with master when executing attachPartition
    Signed-off-by: chihe <chihe@oppo.com>

--- proto/admin_proto.go
+++ proto/admin_proto.go
@@ -548,7 +548,6 @@ type DeleteDataPartitionRequest struct {
 	PartitionSize     int
 	Force             bool
 	DecommissionType  uint32
-	IsSpecialReplica  bool
 }
 
 // DeleteDataPartitionResponse defines the response to the request of deleting a data partition.
