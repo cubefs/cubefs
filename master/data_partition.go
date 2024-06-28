9eec4a82cd7874f207fc1d0f41f2fe512e54793a
chihe <chihe@oppo.com>
2024-06-28 14:35:59 +0800

    fix(master): mark disk as disabled when decommissioning disk
    Signed-off-by: chihe <chihe@oppo.com>

--- master/data_partition.go
+++ master/data_partition.go
@@ -1283,12 +1283,9 @@ directly:
 	// forbidden dp to restore meta for replica
 	for {
 		if !partition.setRestoreReplicaForbidden() && migrateType != AutoAddReplica {
-			partition.DecommissionRetry++
-			if partition.DecommissionRetry >= defaultDecommissionRetryLimit {
-				return errors.NewErrorf("set RestoreReplicaMetaForbidden failed")
-			}
-			// wait for checkReplicaMeta ended
-			time.Sleep(3 * time.Second)
+			log.LogWarnf("action[MarkDecommissionStatus] dp [%d]wait for setting restore replica forbidden",
+				partition.PartitionID)
+			time.Sleep(1 * time.Second)
 		}
 		break
 	}
