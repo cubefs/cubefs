da18d34fa8805b06861528fda95017a7aec8cf11
zhaochenyang <zhaochenyang@oppo.com>
2024-07-01 16:38:32 +0800

    fix(lcnode): [hybrid cloud] optimize lcnode auditlog #22295845
    Signed-off-by: zhaochenyang <zhaochenyang@oppo.com>

--- util/auditlog/auditlog.go
+++ util/auditlog/auditlog.go
@@ -395,7 +395,7 @@ func (a *Audit) LogMigrationOp(clientAddr, volume, op, fullPath string, err erro
 	a.AddLog(entry)
 }
 
-func (a *Audit) LogLcNodeOp(op, name, path string, pid, inode, size, writeGen uint64, hasMek bool, from, to uint32, latency int64, err error) {
+func (a *Audit) LogLcNodeOp(op, vol, name, path string, pid, inode, size, writeGen uint64, hasMek bool, from, to uint32, latency int64, err error) {
 	var errStr string
 	if err != nil {
 		errStr = err.Error()
@@ -407,8 +407,8 @@ func (a *Audit) LogLcNodeOp(op, name, path string, pid, inode, size, writeGen ui
 	timeZone, _ := curTime.Zone()
 	latencyStr := strconv.FormatInt(latency, 10) + " ms"
 
-	entry := fmt.Sprintf("%s %s, op: %s, %s, %s, pid: %v, ino: %v, size: %v, %v, %v, from: %v, to: %v, err: %v, %v",
-		curTimeStr, timeZone, op, name, path, pid, inode, size, writeGen, hasMek, from, to, errStr, latencyStr)
+	entry := fmt.Sprintf("%s %s, op: %s, vol: %s, %s, %s, pid: %v, ino: %v, size: %v, %v, %v, from: %v, to: %v, err: %v, %v",
+		curTimeStr, timeZone, op, vol, name, path, pid, inode, size, writeGen, hasMek, from, to, errStr, latencyStr)
 	if a.prefix != nil {
 		entry = fmt.Sprintf("%s%s", a.prefix.String(), entry)
 	}
@@ -509,13 +509,13 @@ func LogMigrationOp(clientAddr, volume, op, fullPath string, err error, latency 
 	gAdt.LogMigrationOp(clientAddr, volume, op, fullPath, err, latency, ino, from, to)
 }
 
-func LogLcNodeOp(op, name, path string, pid, inode, size, writeGen uint64, hasMek bool, from, to uint32, latency int64, err error) {
+func LogLcNodeOp(op, vol, name, path string, pid, inode, size, writeGen uint64, hasMek bool, from, to uint32, latency int64, err error) {
 	gAdtMutex.RLock()
 	defer gAdtMutex.RUnlock()
 	if gAdt == nil {
 		return
 	}
-	gAdt.LogLcNodeOp(op, name, path, pid, inode, size, writeGen, hasMek, from, to, latency, err)
+	gAdt.LogLcNodeOp(op, vol, name, path, pid, inode, size, writeGen, hasMek, from, to, latency, err)
 }
 
 func ResetWriterBufferSize(size int) {
