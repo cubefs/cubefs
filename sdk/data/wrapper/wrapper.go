114fa4f0d6d1ecc93fec4d66d07c95c60c8fd692
true1064 <tangjingyu@oppo.com>
2024-07-03 19:37:00 +0800

    fix(master): close chan stopc only once, to avoid panic
    Signed-off-by: true1064 <tangjingyu@oppo.com>

--- sdk/data/wrapper/wrapper.go
+++ sdk/data/wrapper/wrapper.go
@@ -604,7 +604,7 @@ func (w *Wrapper) WarningMsg() string {
 
 func (w *Wrapper) updateDataNodeStatus() (err error) {
 	var cv *proto.ClusterView
-	cv, err = w.mc.AdminAPI().GetCluster()
+	cv, err = w.mc.AdminAPI().GetCluster(false)
 	if err != nil {
 		log.LogErrorf("updateDataNodeStatus: get cluster fail: err(%v)", err)
 		return
