2eb491fea2dabe7f88b24b625d543d07eb431fb7
leonrayang <chl696@sina.com>
2024-07-08 15:09:47 +0800

    fix(master): Simplify the process of FolloweRead jugement and resolve issues #22365254
    Signed-off-by: leonrayang <chl696@sina.com>

--- master/cluster.go
+++ master/cluster.go
@@ -542,8 +542,7 @@ func (c *Cluster) scheduleToCheckFollowerReadCache() {
 	go func() {
 		for {
 			if !c.cfg.EnableFollowerCache {
-				time.Sleep(time.Second * 5)
-				continue
+				return
 			}
 			begin := time.Now()
 			if !c.partition.IsRaftLeader() {
