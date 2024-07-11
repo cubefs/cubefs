9a25dd5460130ad61e4a18da6486f92aadf78271
Cloudstriff <chenjiongwendao@qq.com>
2024-07-11 18:40:48 +0800

    feat(api): add shard and shard constant definition
    with #22357426
    
    Signed-off-by: Cloudstriff <chenjiongwendao@qq.com>

--- blobstore/blobstore.go
+++ blobstore/blobstore.go
@@ -35,7 +35,7 @@ package blobstore
 // protoc -I /usr/local/include/ -I ${current}/vendor/github.com/gogo/protobuf/ -I ${current}../  --proto_path=. --gogo_out=Mcubefs/blobstore/common/sharding/range.proto=github.com/cubefs/cubefs/blobstore/common/sharding,plugins=grpc:. *.proto
 // generate api/shardnode
 // cd blobstore/api/shardnode
-// protoc -I ${current}/../ -I /usr/local/include/ -I ${current}/vendor/github.com/gogo/protobuf/ --proto_path=. --gogo_out=Mcubefs/blobstore/common/sharding/range.proto=github.com/cubefs/cubefs/blobstore/common/sharding,plugins=grpc:. *.proto
+// protoc -I /home/project/cubefs/../ -I /usr/local/include/ -I /home/project/cubefs/vendor/github.com/gogo/protobuf/ --proto_path=. --gogo_out=Mcubefs/blobstore/common/sharding/range.proto=github.com/cubefs/cubefs/blobstore/common/sharding,Mcubefs/blobstore/api/clustermgr/shard.proto=github.com/cubefs/cubefs/blobstore/api/clustermgr,plugins=grpc:. *.proto
 // generate api/clustermgr
 // cd blobstore/api/clustermgr
 // protoc -I /home/project/cubefs/../ -I /usr/local/include/ -I /home/project/cubefs/vendor/github.com/gogo/protobuf/ --proto_path=. --gogo_out=Mcubefs/blobstore/common/sharding/range.proto=github.com/cubefs/cubefs/blobstore/common/sharding,plugins=grpc:. *.proto
