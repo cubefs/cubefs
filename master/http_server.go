eb14e94355cbc37902e10aa69a90aa6c7a2ffa4c
chihe <chihe@oppo.com>
2024-07-15 14:52:26 +0800

    feat(master): api for setting accesstime vaild interval by vol
    Signed-off-by: chihe <chihe@oppo.com>

--- master/http_server.go
+++ master/http_server.go
@@ -346,6 +346,9 @@ func (m *Server) registerAPIRoutes(router *mux.Router) {
 	router.NewRoute().Methods(http.MethodGet, http.MethodPost).
 		Path(proto.AdminGenerateClusterUuid).
 		HandlerFunc(m.generateClusterUuid)
+	router.NewRoute().Methods(http.MethodGet, http.MethodPost).
+		Path(proto.AdminSetVolAccessTimeValidInterval).
+		HandlerFunc(m.setVolAccessTimeValidInterval)
 
 	router.NewRoute().Methods(http.MethodGet, http.MethodPost).
 		Path(proto.AdminGetClusterValue).
