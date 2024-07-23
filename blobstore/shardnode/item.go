2b2fd21f8345fe0ed02a32bc1d053c8d2b797432
xiejian <xiejian3@oppo.com>
2024-07-23 15:19:49 +0800

    fix(shardnode): add item interface implementation, fix code merge problems
    with: #22409856 of #22357426
    
    Signed-off-by: xiejian <xiejian3@oppo.com>

---
+++ blobstore/shardnode/item.go
@@ -0,0 +1,58 @@
+package shardnode
+
+import (
+	"context"
+
+	"github.com/cubefs/cubefs/blobstore/api/shardnode"
+)
+
+func (s *service) InsertItem(ctx context.Context, req *shardnode.InsertItemRequest) error {
+	sid := req.Header.SpaceID
+	space, err := s.catalog.GetSpace(ctx, sid)
+	if err != nil {
+		return err
+	}
+	return space.InsertItem(ctx, req.GetHeader(), req.GetItem())
+}
+
+func (s *service) UpdateItem(ctx context.Context, req *shardnode.UpdateItemRequest) error {
+	sid := req.Header.SpaceID
+	space, err := s.catalog.GetSpace(ctx, sid)
+	if err != nil {
+		return err
+	}
+	return space.UpdateItem(ctx, req.GetHeader(), req.GetItem())
+}
+
+func (s *service) DeleteItem(ctx context.Context, req *shardnode.DeleteItemRequest) error {
+	sid := req.Header.SpaceID
+	space, err := s.catalog.GetSpace(ctx, sid)
+	if err != nil {
+		return err
+	}
+	return space.DeleteItem(ctx, req.GetHeader(), req.GetID())
+}
+
+func (s *service) GetItem(ctx context.Context, req *shardnode.GetItemRequest) (item shardnode.Item, err error) {
+	sid := req.Header.SpaceID
+	space, err := s.catalog.GetSpace(ctx, sid)
+	if err != nil {
+		return
+	}
+	item, err = space.GetItem(ctx, req.GetHeader(), req.GetID())
+	return
+}
+
+func (s *service) ListItem(ctx context.Context, req *shardnode.ListItemRequest) (resp shardnode.ListItemResponse, err error) {
+	sid := req.Header.SpaceID
+	space, err := s.catalog.GetSpace(ctx, sid)
+	if err != nil {
+		return
+	}
+	items, err := space.ListItem(ctx, req.GetHeader(), req.GetPrefix(), req.GetMarker(), req.GetCount())
+	if err != nil {
+		return
+	}
+	resp.Items = items
+	return
+}
