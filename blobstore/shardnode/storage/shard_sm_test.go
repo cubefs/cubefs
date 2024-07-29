cbb12711018b4d0d5489670e6123b104e15cd19c
Cloudstriff <chenjiongwendao@qq.com>
2024-07-29 20:14:29 +0800

    style(shardnode): code static check and style refactor
    with #22357426
    
    Signed-off-by: Cloudstriff <chenjiongwendao@qq.com>
    Signed-off-by: xiejian <xiejian3@oppo.com>

--- blobstore/shardnode/storage/shard_sm_test.go
+++ blobstore/shardnode/storage/shard_sm_test.go
@@ -1,3 +1,17 @@
+// Copyright 2022 The CubeFS Authors.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or
+// implied. See the License for the specific language governing
+// permissions and limitations under the License.
+
 package storage
 
 import (
@@ -84,7 +98,8 @@ func TestServerShardSM_Item(t *testing.T) {
 		items[i] = protoItem
 	}
 	rets, marker, err := mockShard.shard.ListItem(ctx, OpHeader{
-		ShardKeys: [][]byte{items[0].ID}}, nil, items[0].ID, uint64(n-1))
+		ShardKeys: [][]byte{items[0].ID},
+	}, nil, items[0].ID, uint64(n-1))
 	require.Nil(t, err)
 	require.Equal(t, items[n-1].ID, marker)
 
@@ -93,7 +108,8 @@ func TestServerShardSM_Item(t *testing.T) {
 	}
 
 	_, marker, err = mockShard.shard.ListItem(ctx, OpHeader{
-		ShardKeys: [][]byte{items[0].ID}}, nil, items[0].ID, uint64(n))
+		ShardKeys: [][]byte{items[0].ID},
+	}, nil, items[0].ID, uint64(n))
 	require.Nil(t, err)
 	require.Nil(t, marker)
 }
