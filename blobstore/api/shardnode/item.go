8c065b5bf6950f6e49c95c0f694f1075fbb1b956
xiejian <xiejian3@oppo.com>
2024-08-02 16:06:54 +0800

    feat(shardnode): volume manager and shardnode service
    1.init volume and bid manager for allocate blob slices
    2.complete shardnode service init
    3. init shardnode client
    
    with #22357426
    
    Signed-off-by: xiejian <xiejian3@oppo.com>

---
+++ blobstore/api/shardnode/item.go
@@ -0,0 +1,41 @@
+// Copyright 2024 The CubeFS Authors.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or
+// implied. See the License for the specific language governing
+// permissions and limitations under the License.
+
+package shardnode
+
+import "context"
+
+func (c *Client) AddItem(ctx context.Context, args InsertItemRequest) error {
+	return nil
+}
+
+func (c *Client) UpdateItem(ctx context.Context, args UpdateItemRequest) error {
+	return nil
+}
+
+func (c *Client) DeleteItem(ctx context.Context, args DeleteItemRequest) error {
+	return nil
+}
+
+func (c *Client) GetItem(ctx context.Context, args GetItemRequest) (Item, error) {
+	return Item{}, nil
+}
+
+func (c *Client) ListItem(ctx context.Context, args ListItemRequest) (ListItemResponse, error) {
+	return ListItemResponse{}, nil
+}
+
+func (i *Item) Read(data []byte) (int, error) {
+	return i.MarshalToSizedBuffer(data)
+}
