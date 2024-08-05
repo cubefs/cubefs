fb4f2ee7625ec1683daf3eeb4c0ad18699e59aa8
chihe <chihe@oppo.com>
2024-08-05 14:48:04 +0800

    fix(cli): use macro value instead of hard code
    Signed-off-by: chihe <chihe@oppo.com>

--- master/http_server.go
+++ master/http_server.go
@@ -354,12 +354,6 @@ func (m *Server) registerAPIRoutes(router *mux.Router) {
 	router.NewRoute().Methods(http.MethodGet, http.MethodPost).
 		Path(proto.AdminGenerateClusterUuid).
 		HandlerFunc(m.generateClusterUuid)
-	router.NewRoute().Methods(http.MethodGet, http.MethodPost).
-		Path(proto.AdminSetVolAccessTimeValidInterval).
-		HandlerFunc(m.setVolAccessTimeValidInterval)
-	router.NewRoute().Methods(http.MethodGet, http.MethodPost).
-		Path(proto.AdminEnablePersistAccessTime).
-		HandlerFunc(m.enablePersistVolAccessTime)
 
 	router.NewRoute().Methods(http.MethodGet, http.MethodPost).
 		Path(proto.AdminGetClusterValue).
