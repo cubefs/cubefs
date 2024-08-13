96d65045c9a7dedcfa1eb680a721fde46448f805
Victor1319 <zengxuewei@oppo.com>
2024-08-13 17:19:02 +0800

    fix(master): reset bad dp report info after dp recover.
    Signed-off-by: Victor1319 <zengxuewei@oppo.com>

--- master/monitor_metrics.go
+++ master/monitor_metrics.go
@@ -599,6 +599,11 @@ func (mm *monitorMetrics) setMpAndDpMetrics() {
 	dpMissingLeaderMap := make(map[uint64]int)
 	dpMissingReplicaMap := make(map[uint64]int)
 
+	for num := range mm.replicaCntMap {
+		dpMissingLeaderMap[num] = 0
+		dpMissingReplicaMap[num] = 0
+	}
+
 	mpMissingLeaderCount := 0
 	mpMissingReplicaCount := 0
 
