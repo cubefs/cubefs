c697fc85ddebfd1d2f0aec4fe8a28724dd3dc977
xiejian <xiejian3@oppo.com>
2024-09-06 17:33:52 +0800

    fix(shardnode): fix raft related problems
    with #22357426
    
    Signed-off-by: xiejian <xiejian3@oppo.com>

--- build/build.sh
+++ build/build.sh
@@ -315,14 +315,20 @@ build_blobstore_cli() {
     popd >/dev/null
 }
 
+build_shardnode() {
+    pushd $SrcPath/blobstore/cmd/shardnode >/dev/null
+    CGO_ENABLED=1 go build ${MODFLAGS} -gcflags=all=-trimpath=${BlobPath} -asmflags=all=-trimpath=${BlobPath} -ldflags="${LDFlags}" -o ${BuildBinPath}/blobstore .
+    popd >/dev/null
+}
+
 build_blobstore_dialtest_bin() {
-    CGO_ENABLED=0 go build ${MODFLAGS} -gcflags=all=-trimpath=${SrcPath} -asmflags=all=-trimpath=${SrcPath} -ldflags="${LDFlags}" -o ${BuildBinPath}/blobstore/blobstore-dialtest ${SrcPath}/blobstore/testing/dial/main
+    CGO_ENABLED=0 go build ${MODFLAGS} -gcflags=all=-trimpath=${BlobPath} -asmflags=all=-trimpath=${BlobPath} -ldflags="${LDFlags}" -o ${BuildBinPath}/blobstore/blobstore-dialtest ${SrcPath}/blobstore/testing/dial/main
 }
 
 build_blobstore() {
     pushd $SrcPath >/dev/null
     echo -n "build blobstore    "
-    build_clustermgr && build_blobnode && build_access && build_scheduler && build_proxy && build_blobstore_cli && echo "success" || echo "failed"
+    build_clustermgr && build_blobnode && build_access && build_scheduler && build_proxy && build_blobstore_cli && build_shardnode && echo "success" || echo "failed"
     popd >/dev/null
 }
 
