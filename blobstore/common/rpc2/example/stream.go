3059127900a94cc5b0f75e52e7043c6cbdb68ace
slasher <shenjie1@oppo.com>
2024-09-09 10:17:09 +0800

    feat(rpc2): add any codec for any value
    . #22573386
    
    Signed-off-by: slasher <shenjie1@oppo.com>

--- blobstore/common/rpc2/example/stream.go
+++ blobstore/common/rpc2/example/stream.go
@@ -10,37 +10,24 @@ import (
 	"github.com/cubefs/cubefs/blobstore/util/log"
 )
 
-type streamReq struct{ str string }
-
-func (s *streamReq) Size() int                       { return len(s.str) }
-func (s *streamReq) Marshal() ([]byte, error)        { return []byte(s.str), nil }
-func (s *streamReq) MarshalTo(b []byte) (int, error) { return copy(b, []byte(s.str)), nil }
-func (s *streamReq) Unmarshal(b []byte) error        { s.str = string(b); return nil }
-
-type streamResp struct{ str string }
-
-func (s *streamResp) Size() int                       { return len(s.str) }
-func (s *streamResp) Marshal() ([]byte, error)        { return []byte(s.str), nil }
-func (s *streamResp) MarshalTo(b []byte) (int, error) { return copy(b, []byte(s.str)), nil }
-func (s *streamResp) Unmarshal(b []byte) error        { s.str = string(b); return nil }
-
-var (
-	_ rpc2.Codec = (*streamReq)(nil)
-	_ rpc2.Codec = (*streamResp)(nil)
+type (
+	streamReq  rpc2.AnyCodec[string]
+	streamResp rpc2.AnyCodec[string]
 )
 
 func runStream() {
 	client := rpc2.Client{ConnectorConfig: rpc2.ConnectorConfig{Network: "tcp"}}
 	streamCli := rpc2.StreamClient[streamReq, streamResp]{Client: &client}
 
-	para := pingPara{I: 11, S: "stream string"}
+	var para paraCodec
+	para.Value = message{I: 11, S: "stream string"}
 	req, err := rpc2.NewStreamRequest(context.Background(),
 		listenon[int(time.Now().UnixNano())%len(listenon)], "/stream", &para)
 	if err != nil {
 		panic(err)
 	}
 
-	var ret pingPara
+	var ret paraCodec
 	cli, err := streamCli.Streaming(req, &ret)
 	if err != nil {
 		panic(err)
@@ -61,12 +48,13 @@ func runStream() {
 			if errx != nil {
 				panic(errx)
 			}
-			log.Info("recv:", resp.str)
+			log.Info("recv:", resp.Value)
 		}
 	}()
 	for idx := range [10]struct{}{} {
-		req := streamReq{str: fmt.Sprintf("request-%d", idx)}
-		log.Info("send:", req.str)
+		var req streamReq
+		req.Value = fmt.Sprintf("request-%d", idx)
+		log.Info("send:", req.Value)
 		if err = cli.Send(&req); err != nil {
 			panic(err)
 		}
