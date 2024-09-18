8fb92d613d01936400da87c4ce794f40f1aa5d51
leonrayang <chl696@sina.com>
2024-09-18 09:30:56 +0800

    feat(master): Improve the exit process in multiple goroutines to ensure proper resource cleanup and graceful shutdown.
    close:#22458839
    
    Signed-off-by: leonrayang <chl696@sina.com>

--- master/metadata_fsm_op.go
+++ master/metadata_fsm_op.go
@@ -1659,7 +1659,7 @@ func (c *Cluster) loadDataPartitions() (err error) {
 		}
 		vol, err1 := c.getVol(dpv.VolName)
 		if err1 != nil {
-			log.LogErrorf("action[loadDataPartitions] err:%v %v", dpv.VolName, err1.Error())
+			log.LogErrorf("action[loadDataPartitions] dp:%v err:%v %v", dpv.PartitionID, dpv.VolName, err1.Error())
 			continue
 		}
 		if vol.ID != dpv.VolID {
