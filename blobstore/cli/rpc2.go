be573307c22cdd23a38da941a43de3e4c6d29e1d
slasher <shenjie1@oppo.com>
2024-09-18 18:32:26 +0800

    feat(rpc2): audit log on error handler
    . #22617142
    
    Signed-off-by: slasher <shenjie1@oppo.com>

--- blobstore/cli/rpc2.go
+++ blobstore/cli/rpc2.go
@@ -15,61 +15,94 @@
 package cli
 
 import (
-	"os"
+	"reflect"
 
 	"github.com/desertbit/grumble"
 
+	"github.com/cubefs/cubefs/blobstore/api/shardnode"
 	"github.com/cubefs/cubefs/blobstore/cli/common"
 	"github.com/cubefs/cubefs/blobstore/cli/common/fmt"
 	"github.com/cubefs/cubefs/blobstore/cli/config"
 	"github.com/cubefs/cubefs/blobstore/common/rpc2"
 )
 
-type (
-	sCodec = rpc2.AnyCodec[struct{ val string }]
-	bCodec = rpc2.AnyCodec[struct{ val []byte }]
-)
+var types = map[string]func() rpc2.Codec{
+	"shardnode.GetBlobArgs": func() rpc2.Codec { return new(shardnode.GetBlobArgs) },
+	"shardnode.GetBlobRet":  func() rpc2.Codec { return new(shardnode.GetBlobRet) },
+
+	"nil":              func() rpc2.Codec { return nil },
+	"rpc2.NoParameter": func() rpc2.Codec { return rpc2.NoParameter },
+}
+
+func helpType(t any) string {
+	if t == nil {
+		return "nil"
+	}
+	typ := reflect.TypeOf(t)
+	if typ.Kind() == reflect.Ptr {
+		typ = typ.Elem()
+	}
+	if typ.Kind() != reflect.Struct {
+		panic(typ.Name())
+	}
+	return typ.PkgPath() + "." + typ.Name()
+}
+
+func helps() (s string) {
+	for n, ft := range types {
+		s += "\n" + n + " -> " + helpType(ft())
+	}
+	return
+}
 
 func cmdRpc2Request(c *grumble.Context) error {
 	cli := config.Rpc2Client
-	addr, path := c.Args.String("addr"), c.Args.String("path")
-	paraVal, rstVal := c.Flags.String("parameter"), c.Flags.String("result")
+	addr, path, request := c.Args.String("addr"), c.Args.String("path"), c.Args.String("request")
+	paraType, rstType := c.Flags.String("parameter"), c.Flags.String("result")
 
-	if c.Flags.Bool("readable") {
-		var para, rst sCodec
-		para.Value.val = paraVal
-		if err := cli.Request(common.CmdContext(), addr, path, &para, &rst); err != nil {
+	paraf, exist := types[paraType]
+	if !exist {
+		return fmt.Errorf("not found parameter(%s), types:%s", paraType, helps())
+	}
+	para := paraf()
+	if para != nil && para != rpc2.NoParameter {
+		if err := common.Unmarshal([]byte(request), para); err != nil {
 			return err
 		}
-		fmt.Println("result:", rst.Value.val)
-		return nil
+	}
+	if s, ok := para.(interface{ GoString() string }); ok {
+		fmt.Println("parameter: " + s.GoString())
+	} else if s, ok := para.(interface{ String() string }); ok {
+		fmt.Println("parameter: " + s.String())
+	} else {
+		fmt.Println("parameter: " + common.Readable(para))
 	}
 
-	val, err := os.ReadFile(paraVal)
-	if err != nil {
+	rstf, exist := types[rstType]
+	if !exist {
+		return fmt.Errorf("not found result(%s), types:%s", rstType, helps())
+	}
+	rst := rstf()
+	if err := cli.Request(common.CmdContext(), addr, path, para, rst); err != nil {
 		return err
 	}
-	var para, rst bCodec
-	para.Value.val = val
-	if err := cli.Request(common.CmdContext(), addr, path, &para, &rst); err != nil {
-		return err
-	}
-	return os.WriteFile(rstVal, rst.Value.val, 0o644)
+	fmt.Println("result: ", common.Readable(rst))
+	return nil
 }
 
 func registerRpc2(app *grumble.App) {
 	rpc2Command := &grumble.Command{
 		Name:     "rpc2",
 		Help:     "simple client of rpc2",
-		LongHelp: "rpc2 simple client struct request and response",
+		LongHelp: "rpc2 simple client struct request and response, types:\n" + helps(),
 		Args: func(a *grumble.Args) {
 			a.String("addr", "request address")
 			a.String("path", "request path")
+			a.String("request", "request parameter json value")
 		},
 		Flags: func(f *grumble.Flags) {
-			f.BoolL("readable", false, "para and args is readable")
-			f.StringL("parameter", "", "request parameter")
-			f.StringL("result", "", "response result")
+			f.StringL("parameter", "", "request parameter name")
+			f.StringL("result", "", "response result name")
 		},
 		Run: cmdRpc2Request,
 	}
