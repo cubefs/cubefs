532d35a5f1663bbf6e65391e412fd01ce77db0e6
Victor1319 <zengxuewei@oppo.com>
2024-09-26 20:04:17 +0800

    fix(sdk): set innerReq when init metawrapper. #22645132
    Signed-off-by: Victor1319 <zengxuewei@oppo.com>

--- proto/fs_proto.go
+++ proto/fs_proto.go
@@ -147,8 +147,8 @@ func (info *InodeInfo) SetExpiration(e int64) {
 
 // String returns the string format of the inode.
 func (info *InodeInfo) String() string {
-	return fmt.Sprintf("Inode(%v) Mode(%v) OsMode(%v) Nlink(%v) Size(%v) Uid(%v) Gid(%v) Gen(%v) QuotaIds(%v) StorageClass(%v)",
-		info.Inode, info.Mode, OsMode(info.Mode), info.Nlink, info.Size, info.Uid, info.Gid, info.Generation, info.QuotaInfos, info.StorageClass)
+	return fmt.Sprintf("Inode(%v) Mode(%v) OsMode(%v) Nlink(%v) Size(%v) Uid(%v) Gid(%v) Gen(%v) QuotaIds(%v) StorageClass(%v), Atime(%v), Mtime(%v), Ctime(%v)",
+		info.Inode, info.Mode, OsMode(info.Mode), info.Nlink, info.Size, info.Uid, info.Gid, info.Generation, info.QuotaInfos, info.StorageClass, info.AccessTime, info.ModifyTime, info.CreateTime)
 }
 
 type XAttrInfo struct {
