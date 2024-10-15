5dbd5ba05ea337ecf0d4bcf6086dd48a7717f3b5
chihe <chihe@oppo.com>
2024-10-15 17:21:07 +0800

    fix(master): compatible with 3.3.2 metanode(used for memory)
    Signed-off-by: chihe <chihe@oppo.com>

--- proto/admin_proto.go
+++ proto/admin_proto.go
@@ -857,7 +857,7 @@ type MetaPartitionReport struct {
 type MetaNodeHeartbeatResponse struct {
 	ZoneName             string
 	Total                uint64
-	MemUsed              uint64
+	Used                 uint64
 	MetaPartitionReports []*MetaPartitionReport
 	Status               uint8
 	Result               string
