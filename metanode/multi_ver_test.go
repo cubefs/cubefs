96527a6ed918a632dded60e2801e7d7ce797c0c8
leonrayang <chl696@sina.com>
2024-11-14 00:56:56 +0800

    feat(metanode): optimize quota infomation storage to reduce usage of memory
    close:#22788628
    
    Signed-off-by: leonrayang <chl696@sina.com>

--- metanode/multi_ver_test.go
+++ metanode/multi_ver_test.go
@@ -1435,7 +1435,7 @@ func TestExtendSerialization(t *testing.T) {
 	}
 
 	checkFunc()
-	mv.multiSnap = &ExtendMultiSnap{}
+	mv.multiSnap = &ExtendMultiSnap{verSeq: 101}
 	mv.multiSnap.multiVers = []*Extend{
 		{
 			inode:     789,
