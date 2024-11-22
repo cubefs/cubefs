7b8801af4eb7daa2e91452432c2dd06459bf8e40
Victor1319 <zengxuewei@oppo.com>
2024-11-22 09:35:30 +0800

    test(docker): support run in hybrid way for docker mod. #22816675
    Signed-off-by: Victor1319 <zengxuewei@oppo.com>

--- docker/run_docker.sh
+++ docker/run_docker.sh
@@ -30,7 +30,8 @@ EOF
     exit 0
 }
 
-compose="docker compose --env-file ${RootPath}/docker/run_docker.env -f ${RootPath}/docker/docker-compose.yml"
+#compose="docker-compose --env-file ${RootPath}/docker/run_docker.env -f ${RootPath}/docker/docker-compose-hy.yml"
+compose="docker-compose --env-file ${RootPath}/docker/run_docker.env -f ${RootPath}/docker/docker-compose.yml"
 
 has_go() {
     if command -v go &> /dev/null
