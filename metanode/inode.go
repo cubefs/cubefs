6442a4fd2c834d69912ccced6561b9e99f762576
leonrayang <chl696@sina.com>
2024-12-02 23:27:59 +0800

    feat(metanode): Disable the process related to snapshots if snapshots are switched off
    Signed-off-by: leonrayang <chl696@sina.com>

--- metanode/inode.go
+++ metanode/inode.go
@@ -143,6 +143,9 @@ func (i *Inode) setVer(seq uint64) {
 }
 
 func (i *Inode) insertEkRefMap(mpId uint64, ek *proto.ExtentKey) {
+	if !clusterEnableSnapshot {
+		return
+	}
 	if i.multiSnap == nil {
 		i.multiSnap = NewMultiSnap(i.getVer())
 	}
@@ -1492,6 +1495,9 @@ func (i *Inode) CreateUnlinkVer(mpVer uint64, nVer uint64) {
 }
 
 func (i *Inode) CreateVer(ver uint64) {
+	if !clusterEnableSnapshot {
+		return
+	}
 	// inode copy not include multi ver array
 	ino := i.CopyDirectly().(*Inode)
 	ino.Extents = NewSortedExtents()
@@ -1511,6 +1517,9 @@ func (i *Inode) CreateVer(ver uint64) {
 }
 
 func (i *Inode) buildMultiSnap() {
+	if !clusterEnableSnapshot {
+		return
+	}
 	if i.multiSnap == nil {
 		i.multiSnap = &InodeMultiSnap{}
 	}
@@ -1559,6 +1568,9 @@ func (i *Inode) SplitExtentWithCheck(param *AppendExtParam) (delExtents []proto.
 
 // try to create version between curVer and seq of multiSnap.multiVersions[0] in verList
 func (i *Inode) CreateLowerVersion(curVer uint64, verlist *proto.VolVersionInfoList) (err error) {
+	if !clusterEnableSnapshot {
+		return
+	}
 	verlist.RWLock.RLock()
 	defer verlist.RWLock.RUnlock()
 
@@ -1629,7 +1641,7 @@ func (i *Inode) AppendExtentWithCheck(param *AppendExtParam) (delExtents []proto
 	}
 
 	// multi version take effect
-	if i.getVer() > 0 && len(delExtents) > 0 {
+	if clusterEnableSnapshot && i.getVer() > 0 && len(delExtents) > 0 {
 		var err error
 		if err = i.CreateLowerVersion(i.getVer(), param.multiVersionList); err != nil {
 			return
@@ -1690,6 +1702,9 @@ func (i *Inode) DecNLinkByVer(verSeq uint64) {
 }
 
 func (i *Inode) DecSplitExts(mpId uint64, delExtents interface{}) {
+	if !clusterEnableSnapshot {
+		return
+	}
 	log.LogDebugf("[DecSplitExts] mpId [%v] inode[%v]", mpId, i.Inode)
 	cnt := len(delExtents.([]proto.ExtentKey))
 	for id := 0; id < cnt; id++ {
