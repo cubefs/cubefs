f13cc3a1b6f74e8dec3181121311fcbe4a778750
Wu Huocheng <wuhuocheng@oppo.com>
2025-01-06 14:40:20 +0800

    feat(master): Add migrate request meta partition apis.#22834621
    Signed-off-by: Wu Huocheng <wuhuocheng@oppo.com>

--- master/const.go
+++ master/const.go
@@ -243,6 +243,10 @@ const (
 	maxMpCreationCount                            = 10
 	defaultVolForbidWriteOpOfProtoVersion0        = true
 	DefaultRemoteCacheMaxFileSizeGB               = 128
+	defaultMetaNodeMemHighPer                     = 0.75
+	defaultMetaNodeMemLowPer                      = 0.3
+	metaNodeReserveMemorySize                     = 3 * 1024 * 1024 * 1024
+	metaNodeMemoryRatio                           = 2
 )
 
 const (
@@ -341,6 +345,9 @@ const (
 	opSyncAddFlashGroup    uint32 = 0x6D
 	opSyncDeleteFlashGroup uint32 = 0x6E
 	opSyncUpdateFlashGroup uint32 = 0x6F
+
+	opSyncAddBalanceTask    uint32 = 0x70
+	opSyncUpdateBalanceTask uint32 = 0x71
 )
 
 func init() {
@@ -477,6 +484,8 @@ const (
 
 	flashNodePrefix  = keySeparator + "fn" + keySeparator
 	flashGroupPrefix = keySeparator + "fg" + keySeparator
+
+	balanceTaskPrefix = keySeparator + "balanceTask" + keySeparator
 )
 
 // selector enum
@@ -503,4 +512,9 @@ const (
 	CleanTaskFreezed   = "freezeDone"
 	CleanTaskBackuping = "backuping"
 	CleanTaskBackuped  = "backupDone"
+	PlanTaskInit       = "init"
+	PlanTaskRun        = "running"
+	PlanTaskError      = "error"
+	PlanTaskStop       = "stop"
+	PlanTaskDone       = "done"
 )
