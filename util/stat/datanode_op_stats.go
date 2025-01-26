faa537e14757387567054a85f685d0d4580cb1a7
shuqiang-zheng <zhengshuqiang@oppo.com>
2025-01-26 14:34:14 +0800

    fix(datanode): fixes for read/write requests still being processed and log files not being found after service stopped.
    Signed-off-by: shuqiang-zheng <zhengshuqiang@oppo.com>

--- util/stat/datanode_op_stats.go
+++ util/stat/datanode_op_stats.go
@@ -20,6 +20,7 @@ import (
 	"fmt"
 	"os"
 	"path"
+	"path/filepath"
 	"sort"
 	"strings"
 	"sync"
@@ -88,13 +89,19 @@ func NewOpLogger(dir, filename string, maxOps int, duration time.Duration, leftS
 
 	logLeftSpace := int64(float64((fs.Blocks * uint64(fs.Bsize))) * leftSpaceRatio)
 
+	filePath := path.Join(dir, filename)
+	filePath, err = filepath.Abs(filePath)
+	if err != nil {
+		return nil, errors.New("get absolute file path failed, " + err.Error())
+	}
+
 	_ = os.Chmod(dir, 0o755)
 	logger := &OpLogger{
 		opCounts:       map[string]*int32{},
 		opCountsMaster: map[string]*int32{},
 		opCountsPrev:   map[string]*int32{},
 		maxOps:         maxOps,
-		logFile:        path.Join(dir, filename),
+		logFile:        filePath,
 		ticker:         time.NewTicker(duration),
 		done:           make(chan bool),
 		fileSize:       DefaultStatLogSize,
