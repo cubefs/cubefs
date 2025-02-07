82ffd69b6bb89a8b6e4e4d6acbc9eede7e6b7781
clinx <chenlin1@oppo.com>
2025-02-07 15:03:34 +0800

    fix(object): stop update volume meta info when vol is deleted #22861848
    Signed-off-by: clinx <chenlin1@oppo.com>

--- objectnode/fs_volume.go
+++ objectnode/fs_volume.go
@@ -162,7 +162,9 @@ func (v *Volume) syncOSSMeta() {
 	for {
 		select {
 		case <-v.ticker.C:
-			v.loadOSSMeta()
+			if v.loadOSSMeta() {
+				return
+			}
 		case <-v.closeCh:
 			return
 		}
@@ -170,8 +172,9 @@ func (v *Volume) syncOSSMeta() {
 }
 
 // update Volume meta info
-func (v *Volume) loadOSSMeta() {
+func (v *Volume) loadOSSMeta() (deleted bool) {
 	var err error
+
 	defer func() {
 		if err != nil {
 			v.onAsyncTaskError.OnError(err)
@@ -184,8 +187,9 @@ func (v *Volume) loadOSSMeta() {
 		return
 	}
 	if volumeInfo.Status == 1 {
-		log.LogWarnf("loadOSSMeta: volume has been deleted: volume(%s) status(%d)", v.name, volumeInfo.Status)
+		deleted = true
 		err = syscall.ENOENT
+		log.LogWarnf("loadOSSMeta: volume has been deleted: volume(%s) status(%d)", v.name, volumeInfo.Status)
 		return
 	}
 
@@ -213,6 +217,7 @@ func (v *Volume) loadOSSMeta() {
 	}
 	v.metaLoader.storeObjectLock(objectlock)
 	v.metaLoader.setSynced()
+	return
 }
 
 func (v *Volume) Name() string {
