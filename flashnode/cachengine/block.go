b72f80a8eda464ca7ed12e4b3d1690c5f1a56a7c
clinx <chenlin1@oppo.com>
2025-03-07 17:11:04 +0800

    fix(flashnode): avoid unnecessary audit log printing
    Signed-off-by: clinx <chenlin1@oppo.com>

--- flashnode/cachengine/block.go
+++ flashnode/cachengine/block.go
@@ -357,9 +357,13 @@ func (cb *CacheBlock) initFilePath(isLoad bool) (err error) {
 		cb.notifyReady()
 	}
 	_, err = os.Stat(cb.filePath)
-	msg := fmt.Sprintf("init cache block(%s) to local: err %v", cb.filePath, err)
-	log.LogDebugf("%v", msg)
-	auditlog.LogFlashNodeOp("BlockInit", msg, err)
+	if log.EnableDebug() || err != nil {
+		msg := fmt.Sprintf("init cache block(%s) to local: err %v", cb.filePath, err)
+		log.LogDebugf("%v", msg)
+		if !isLoad {
+			auditlog.LogFlashNodeOp("BlockInit", msg, err)
+		}
+	}
 	return
 }
 
