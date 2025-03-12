0cb374c1af948af3d07c6e1d43f77b709f594cd0
Victor1319 <zengxuewei@oppo.com>
2025-03-12 16:55:33 +0800

    docs(docs): add faq upgrade file to sidbar.
    Signed-off-by: Victor1319 <zengxuewei@oppo.com>

--- docs-zh/source/sidebarConfig.js
+++ docs-zh/source/sidebarConfig.js
@@ -168,6 +168,7 @@ module.exports = [
             'faq/kafka.md',
             'faq/troubleshoot/strategy.md',
             'faq/troubleshoot/case.md',
+            'faq/upgrade/3.5.0.md',
         ]
     },
     {
