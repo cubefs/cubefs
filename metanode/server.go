a92c8452cf77acaa3bf2a4eb172463e721562de4
leonrayang <chl696@sina.com>
2025-03-21 15:20:19 +0800

    fix(revert): align and reuse the Packet struct to reduce allocated memory
    This reverts commit f4c3f8aa40dbfe971075a7bd3ad2ee98e29f7183.
    
    Signed-off-by: leonrayang <chl696@sina.com>

--- metanode/server.go
+++ metanode/server.go
@@ -81,14 +81,13 @@ func (m *MetaNode) serveConn(conn net.Conn, stopC chan uint8) {
 	c.SetKeepAlive(true)
 	c.SetNoDelay(true)
 	remoteAddr := conn.RemoteAddr().String()
-	p := &Packet{}
 	for {
 		select {
 		case <-stopC:
 			return
 		default:
 		}
-		ClearPacket(p)
+		p := &Packet{}
 		if err := p.ReadFromConnWithVer(conn, proto.NoReadDeadlineTime); err != nil {
 			if err != io.EOF {
 				log.LogError("serve MetaNode: ", err.Error())
