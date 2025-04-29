e4ff320f75dc48ba6de09df01fb3ced0dae26fba
slasher <shenjie1@oppo.com>
2025-04-29 16:41:08 +0800

    feat(ci): add docker for go 1.18
    @formatter:off
    
    close: #3782
    
    Signed-off-by: slasher <shenjie1@oppo.com>

--- docker/script/run_format.sh
+++ docker/script/run_format.sh
@@ -3,7 +3,10 @@ CurrentPath=$(cd $(dirname ${BASH_SOURCE[0]}); pwd)
 gofmtFile=gofmt_results.diff
 pushd ${CurrentPath}/../../
 find . -type f -name "*.go" | \
-    grep -v 'vendor'|grep -v 'depends'|grep -v ./sdk/graphql/client/ | \
+    grep -v -E '^./vendor/' | \
+    grep -v -E '^./depends/' | \
+    grep -v -E '^./sdk/graphql/client/' | \
+    grep -v -E '\.pb\.go$' | \
     xargs gofumpt -l -d > ${gofmtFile}
 cat ${gofmtFile}
 if [ "$(cat ${gofmtFile}|wc -l)" -gt 0  ]; then
@@ -27,7 +30,11 @@ done
 
 golintFile=golint.diff
 pushd ${CurrentPath}/../..
+oldldflags="$(go env CGO_LDFLAGS)"
+lpath=$(echo -n ${oldldflags} | awk '{print $1}')
+go env -w CGO_LDFLAGS="${lpath} -ldl -ltcmalloc -lm -lrocksdb -lz -lbz2 -lsnappy -llz4 -lzstd -lstdc++"
 go generate . > ${golintFile}
+go env -w CGO_LDFLAGS="${oldldflags}"
 cat ${golintFile}
 if [[ $? -ne 0 ]]; then
     exit 1
