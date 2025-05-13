c296a62206396ed7aa288904ceab898d1b6b2f80
Victor1319 <zengxuewei@oppo.com>
2025-05-13 14:28:22 +0800

    feat(meta): add testcase for inode and dentry marshal. #1000114612
    Signed-off-by: Victor1319 <zengxuewei@oppo.com>

--- proto/extent_key_test.go
+++ proto/extent_key_test.go
@@ -15,10 +15,13 @@
 package proto_test
 
 import (
+	"bytes"
 	"encoding/json"
+	"reflect"
 	"testing"
 
 	"github.com/cubefs/cubefs/proto"
+	"github.com/cubefs/cubefs/util/buf"
 	"github.com/stretchr/testify/require"
 )
 
@@ -51,4 +54,62 @@ func TestDelExtentParam(t *testing.T) {
 	require.EqualValues(t, 1, len(eks))
 	require.EqualValues(t, 1, eks[0].PartitionId)
 	require.EqualValues(t, 1, eks[0].ExtentId)
+}
+
+func TestEkMarshalBinary(t *testing.T) {
+	ek := &proto.ExtentKey{
+		PartitionId: 1,
+		ExtentId:    1,
+		Size:        1024,
+		CRC:         1001,
+		SnapInfo: &proto.ExtSnapInfo{
+			IsSplit: true,
+		},
+	}
+
+	tmpBuf := buf.NewByteBufEx(1024)
+	ek.MarshalBinary(tmpBuf, true)
+
+	ek2 := &proto.ExtentKey{}
+	buf2 := buf.NewReadByteBuf()
+	buf2.SetData(tmpBuf.Bytes())
+	err := ek2.UnmarshalBinary(buf2, true)
+	if err != nil || !reflect.DeepEqual(ek, ek2) {
+		t.Fail()
+	}
+}
+
+func BenchmarkExtentMarshalBinary(b *testing.B) {
+	ek := &proto.ExtentKey{
+		PartitionId: 1,
+		ExtentId:    1,
+	}
+	b.ReportAllocs()
+
+	buf := buf.NewByteBufEx(1024)
+	for i := 0; i < b.N; i++ {
+		ek.MarshalBinary(buf, false)
+	}
+}
+
+func TestExtentKeyCompitable(t *testing.T) {
+	// data marshal by version 3.5.0
+	oldData := []byte{0, 0, 0, 0, 0, 0, 8, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 4, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 4, 0, 1}
+
+	ek := &proto.ExtentKey{
+		PartitionId: 1,
+		ExtentId:    1,
+		Size:        1024,
+		FileOffset:  2048,
+		SnapInfo: &proto.ExtSnapInfo{
+			IsSplit: true,
+			VerSeq:  1024,
+		},
+	}
+
+	buff := buf.NewByteBufEx(40)
+	err := ek.MarshalBinary(buff, true)
+	if err != nil || !bytes.Equal(oldData, buff.Bytes()) {
+		t.Fail()
+	}
 }
