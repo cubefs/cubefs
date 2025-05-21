60baf1ff409c5a2ce120743055f43b73abf00acf
Victor1319 <zengxuewei@oppo.com>
2025-05-21 20:26:41 +0800

    fix(master): not return discard dp for datanode get api. #1000135139
    Signed-off-by: Victor1319 <zengxuewei@oppo.com>

--- master/cluster.go
+++ master/cluster.go
@@ -2366,6 +2366,9 @@ func (c *Cluster) getAllDataPartitionIDByDatanode(addr string) (partitionIDs []u
 	safeVols := c.allVols()
 	for _, vol := range safeVols {
 		for _, dp := range vol.dataPartitions.partitions {
+			if dp.IsDiscard {
+				continue
+			}
 			for _, host := range dp.Hosts {
 				if host == addr {
 					partitionIDs = append(partitionIDs, dp.PartitionID)
