c8b4efe0848af2cee5461e126d097fcd29a0ccd0
Wu Huocheng <wuhuocheng@oppo.com>
2025-06-12 14:20:48 +0800

    chore(master): remove unnecessary message.#1000173254
    Signed-off-by: Wu Huocheng <wuhuocheng@oppo.com>

--- master/api_service_balance.go
+++ master/api_service_balance.go
@@ -549,7 +549,6 @@ func (m *Server) offlineMetaNode(w http.ResponseWriter, r *http.Request) {
 	}
 
 	if plan, err = m.cluster.CreateOfflineMetaNodePlan(offLineAddr); err != nil {
-		err = fmt.Errorf("Create plan metanode(%s) err: %s, msg: %s", offLineAddr, err.Error(), plan.Msg)
 		log.LogErrorf(err.Error())
 		sendErrReply(w, r, newErrHTTPReply(err))
 		return
