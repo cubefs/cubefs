e0c53eb03581461d26681af09821afe07d1d358c
chihe <chihe@oppo.com>
2025-06-30 09:38:23 +0800

    fix(client): do not remove node cache when dir is released
    close:#21990545
    
    Signed-off-by: chihe <chihe@oppo.com>

--- client/fs/dir.go
+++ client/fs/dir.go
@@ -164,6 +164,9 @@ func (d *Dir) Release(ctx context.Context, req *fuse.ReleaseRequest) (err error)
 		log.LogDebugf("TRACE Release exit: ino(%v) name(%v)", d.info.Inode, d.name)
 	}()
 	d.dctx.Clear()
+	d.dcache.Clear()
+	ino := d.info.Inode
+	d.super.ic.Delete(ino)
 
 	return nil
 }
