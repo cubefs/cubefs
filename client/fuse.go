08895b35ab069325ccd3c049fe52a7fa41851aa6
clinx <chenlin1@oppo.com>
2025-11-27 15:43:55 +0800

    fix(client): set FuseServeThreads default value to 64
    with: #1000451308
    
    Signed-off-by: clinx <chenlin1@oppo.com>
    (cherry picked from commit 37bce4a4247e0d3ac93798a432db680125953d05)

--- client/fuse.go
+++ client/fuse.go
@@ -1046,6 +1046,7 @@ func parseMountOption(cfg *config.Config) (*proto.MountOptions, error) {
 	opt.MetaCacheAcceleration = GlobalMountOptions[proto.MetaCacheAcceleration].GetBool()
 	opt.MinimumNlinkReadDir = GlobalMountOptions[proto.MinimumNlinkReadDir].GetInt64()
 	opt.InodeLruLimit = GlobalMountOptions[proto.InodeLruLimit].GetInt64()
+	opt.FuseServeThreads = GlobalMountOptions[proto.FuseServeThreads].GetInt64()
 
 	if opt.MountPoint == "" || opt.Volname == "" || opt.Owner == "" || opt.Master == "" {
 		return nil, errors.New(fmt.Sprintf("invalid config file: lack of mandatory fields, mountPoint(%v), volName(%v), owner(%v), masterAddr(%v)", opt.MountPoint, opt.Volname, opt.Owner, opt.Master))
