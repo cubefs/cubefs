0424ada0fcfe184365b94705d72cb25183abeb5f
Victor1319 <zengxuewei@oppo.com>
2025-12-23 15:50:01 +0800

    fix(fuse): revert maxWrite constant to 128kB.
    close:#1000511705
    
    Signed-off-by: Victor1319 <zengxuewei@oppo.com>

--- depends/bazil.org/fuse/fuse_linux.go
+++ depends/bazil.org/fuse/fuse_linux.go
@@ -4,5 +4,6 @@ package fuse
 //
 // Linux 4.2.0 has been observed to cap this value at 128kB
 // (FUSE_MAX_PAGES_PER_REQ=32, 4kB pages).
-// const maxWrite = 128 * 1024
-const maxWrite = 128 * 1024 * 2
+const maxWrite = 128 * 1024
+
+// const maxWrite = 128 * 1024 * 2
